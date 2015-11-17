e5a56a004877ae8a3d6aa9f0cd4c25fd584a1579
David Cormier <david.cormier@erudit.org>
2015-11-17 14:00:05 -0500

    editor: name the urls

--- erudit/editor/urls.py
+++ erudit/editor/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from editor.views import JournalSubmissionCreate
+from editor.views import JournalSubmissionCreate, JournalSubmissionUpdate, DashboardView
 
 urlpatterns = [
-    url(r'^add/', JournalSubmissionCreate.as_view(), name='add'),
+    url(r'^numero/ajout', JournalSubmissionCreate.as_view(), name='add'),
+    url(r'^numero/(?P<pk>[0-9]+)/$', JournalSubmissionUpdate.as_view(), name='update'),
+    url(r'', DashboardView.as_view(), name='dashboard')
 ]
