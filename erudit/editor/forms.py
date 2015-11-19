67995ff227198b12b7159417586c691c7fafd3ce
sophyo <sophy.ouch@erudit.org>
2015-11-19 15:57:48 -0500

    change to vertical forms and remove bootstrap classes

--- erudit/editor/forms.py
+++ erudit/editor/forms.py
@@ -42,9 +42,9 @@ class IssueSubmissionForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
 
         self.helper = FormHelper()
-        self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-lg-2'
-        self.helper.field_class = 'col-lg-8'
+        self.helper.form_class = 'form-vertical'
+        # self.helper.label_class = 'col-xs-12'
+        # self.helper.field_class = 'col-xs-12'
         self.helper.form_method = 'post'
 
         self.helper.add_input(
