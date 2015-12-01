0517ba36c11122fa6dc136a052329c3660248ce0
David Cormier <david.cormier@erudit.org>
2015-12-01 13:09:33 -0500

    Add management commands
    Import publisher and users from Edinum and Mandragore

---
+++ erudit/editor/management/commands/import_publisher_accounts.py
@@ -0,0 +1,42 @@
+import logging
+
+from editor.edinum import (
+    fetch_publishers_from_edinum,
+    create_or_update_publisher,
+    create_or_update_journal
+)
+
+from django.core.management.base import BaseCommand
+
+log = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+
+    help = """Import publisher accounts from a csv file"""
+
+    def handle(self, *args, **options):
+        self.created_or_updated_publishers = []
+        self.created_or_updated_journals = []
+
+        for publisher_row in fetch_publishers_from_edinum():
+            (person_id, publisher_name, series_id, journal_id,
+             journal_name, journal_subtitle) = publisher_row
+
+            if person_id not in self.created_or_updated_publishers:
+                publisher = create_or_update_publisher(
+                    person_id,
+                    publisher_name
+                )
+
+                self.created_or_updated_publishers.append(person_id)
+
+            if journal_id not in self.created_or_updated_journals:
+                create_or_update_journal(
+                    publisher,
+                    journal_id,
+                    journal_name,
+                    journal_subtitle
+                )
+
+                self.created_or_updated_journals.append(journal_id)
---
+++ erudit/editor/tests/test_commands.py
@@ -0,0 +1,190 @@
+# from datetime import datetime
+from django.contrib.auth.models import User
+
+from erudit.models import Publisher, Journal
+
+from editor.tests.base import BaseEditorTestCase
+from editor.mandragore import (
+    create_mandragore_profile_for_user,
+    user_coherent_with_mandragore,
+    can_create_mandragore_user,
+    get_list_as_sql,
+    create_or_update_mandragore_user,
+    MandragoreError,
+    get_mandragore_user
+)
+
+from editor.edinum import (
+    create_or_update_publisher,
+    create_or_update_journal
+)
+
+
+class TestCommands(BaseEditorTestCase):
+
+    def test_create_mandragore_profile_for_user(self):
+
+        mandragore_profile = create_mandragore_profile_for_user(
+            9001, self.user
+        )
+
+        self.assertTrue(
+            mandragore_profile.synced_with_mandragore
+        )
+
+    def test_user_coherent_with_mandragore(self):
+
+        # Create a fake mandragore user
+
+        mandragore_user = User(
+            first_name="Karl",
+            last_name="Marx",
+            username="kmarx",
+            email="kmarx@international.info",
+        )
+
+        mandragore_user.save()
+
+        mandragoreprofile = create_mandragore_profile_for_user(
+            '1234',
+            mandragore_user
+        )
+        mandragoreprofile.save()
+
+        self.assertTrue(
+            user_coherent_with_mandragore(
+                mandragore_user,
+                {
+                    'person_id': '1234',
+                    'username': 'kmarx',
+                    'email': 'kmarx@international.info',
+                }
+            )
+        )
+
+    def test_can_create_mandragore_user(self):
+
+        self.assertTrue(
+            can_create_mandragore_user(
+                "new username !!",
+                "dummy mandragore id!",
+            ),
+            "We should be able to create a mandragore user for a nonexistent username and nonexistent mandragore id"  # noqa
+        )
+
+        self.assertFalse(
+            can_create_mandragore_user(
+                self.user.username,
+                "dummy mandragore id!",
+            ),
+            "We should not be able to create a mandragore user for an existing username"
+        )
+
+        create_mandragore_profile_for_user("12345", self.user)
+
+        self.assertFalse(
+            can_create_mandragore_user(
+                "dummy user",
+                "12345"
+            ),
+            "We should not be able to create a mandragore user for an existing mandragore id"
+        )
+
+    def test_get_list_as_sql(self):
+        self.assertEquals(
+            get_list_as_sql([1, 2]),
+            '"1","2"'
+        )
+
+    def test_create_or_update_mandragore_user(self):
+
+        user = create_or_update_mandragore_user(
+            '1234', username="fengels", first_name="Friedrich", last_name="Engels"
+        )
+
+        self.assertIsNotNone(
+            user.mandragoreprofile
+        )
+
+        self.assertEquals(
+            user.first_name,
+            "Friedrich"
+        )
+
+        user = create_or_update_mandragore_user(
+            '1234', username="fengels", first_name="updated"
+        )
+
+        self.assertEquals(
+            user.first_name,
+            "updated"
+        )
+
+        with self.assertRaises(MandragoreError):
+            create_or_update_mandragore_user(
+                'bad id', username="fengels", last_name="updated"
+            )
+
+    def test_get_mandragore_user(self):
+
+        # A user with no profile should raise the error
+        with self.assertRaises(MandragoreError):
+            get_mandragore_user(self.user.username)
+
+        create_mandragore_profile_for_user("1234", self.user)
+
+        # Validating with a different person_id should raise the error
+        with self.assertRaises(MandragoreError):
+            get_mandragore_user(self.user.username, person_id="123456")
+
+        user = get_mandragore_user(self.user.username, person_id="1234")
+        self.assertEquals(
+            user.pk,
+            self.user.pk
+        )
+
+    def test_create_or_update_publisher(self):
+        publisher = create_or_update_publisher("123456", "dcormier")
+        self.assertEquals(publisher.name, "dcormier")
+        self.assertEquals(publisher.edinum_id, "123456")
+
+        publisher_2 = create_or_update_publisher("123456", "dcormier2")
+        self.assertEquals(publisher_2.pk, publisher.pk)
+        self.assertEquals(publisher_2.name, "dcormier2")
+
+        # create another publisher with the same edinum_id
+        Publisher.objects.create(name="bob", edinum_id="123456")
+        publisher = create_or_update_publisher("123456", "dcormier2")
+        self.assertIsNone(publisher)
+
+    def test_create_or_update_journal(self):
+        publisher = create_or_update_publisher("123456", "dcormier")
+        journal = create_or_update_journal(
+            publisher, "123", "Journal of journals", ""
+        )
+
+        self.assertEquals(
+            journal.edinum_id, "123"
+        )
+
+        journal_2 = create_or_update_journal(
+            publisher, "123", "Journal", ""
+        )
+
+        self.assertEquals(
+            journal.pk,
+            journal_2.pk
+        )
+
+        self.assertEquals(
+            journal_2.name,
+            "Journal"
+        )
+
+        # create another journal with the same edinum_id
+        Journal.objects.create(name="test", publisher=publisher, edinum_id="123")
+        journal = create_or_update_journal(
+            publisher, "123", "test", ""
+        )
+
+        self.assertIsNone(journal)
