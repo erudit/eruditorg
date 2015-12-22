171457a4cc5fa36fc8ca26b3f84574b8f951a3d8
Olivier Larchevêque <olivier.larcheveque@gmail.com>
2015-12-21 21:45:02 -0500

    add #331 basket case

--- erudit/individual_subscription/management/commands/individual_account_permissions.py
+++ erudit/individual_subscription/management/commands/individual_account_permissions.py
@@ -7,23 +7,8 @@ from ...models import OrganizationPolicy, IndividualAccountJournal
 
 class Command(BaseCommand):
 
-    def _add_permission(self, accounts, journals):
-        for account in accounts:
-            for journal in journals:
-                rule, created = IndividualAccountJournal.objects.get_or_create(
-                    account=account,
-                    journal=journal)
-                if created:
-                    log = '{} {}'.format(account, journal)
-                    if self.verbosity > 1:
-                        self.stdout.write(log)
-
     def handle(self, *args, **options):
         self.verbosity = int(options['verbosity'])
         organisations = OrganizationPolicy.objects.all()
         for organisation in organisations:
-            accounts = organisation.accounts.all()
-
-            if organisation.access_full:
-                journals = Journal.objects.all()
-                self._add_permission(accounts, journals)
+            organisation.generate_flat_access()
