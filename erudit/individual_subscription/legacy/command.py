d63cf6dedb6f8f45c03ad5b254066ce047cbde99
Olivier Larchevêque <olivier.larcheveque@w.illi.am>
2015-12-22 22:31:12 -0500

    #372 : doc + command link abonne / revue

--- erudit/individual_subscription/legacy/command.py
+++ erudit/individual_subscription/legacy/command.py
@@ -1,7 +1,12 @@
+import csv
+import os
+
 from django.core.management.base import BaseCommand
 
+from erudit.models import Journal, Organisation
+
 from .legacy_models import Abonneindividus
-from ..models import IndividualAccount
+from ..models import IndividualAccount, OrganizationPolicy
 from ..factories import OrganizationPolicyFactory
 
 
@@ -17,6 +22,7 @@ class Command(BaseCommand):
             self.stdout.write(self.args)
             return
 
+        self.args = args
         command = args[0]
         self.stdout.write(command)
         cmd = getattr(self, command)
@@ -48,3 +54,36 @@ class Command(BaseCommand):
                 lastname=old_abonne.nom,
                 password=old_abonne.password)
             new_account.save()
+
+    def link_abonnes(self):
+        # Create policy from filename if it does not exist
+        filename = self.args[1]
+        basename = os.path.basename(filename)
+        if '.' in basename:
+            basename = basename.split('.')[0]
+        organization = Organisation.objects.get(name__iexact=basename)
+        policy, created = OrganizationPolicy.objects.get_or_create(organization=organization)
+        print(policy)
+
+        with open(filename, 'r') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reader:
+                email = row[2]
+                journal_id = row[3]
+
+                # Assign the policy to the account
+                try:
+                    account = IndividualAccount.objects.\
+                        get(email__iexact=email)
+                    account.organization_policy = policy
+                    account.save()
+                except Exception:
+                    print('{} {}'.format('account', email))
+
+                # Define the journal in the policy
+
+                if isinstance(journal_id, int) and \
+                        Journal.objects.filter(id=journal_id).count() == 1:
+                    journal = Journal.objects.get(id=journal_id)
+                    policy.access_journal.add(journal)
+                    policy.save()
