d402488dc3f8cde168fca0791825e5816690997d
Olivier Larchevêque <olivier.larcheveque@gmail.com>
2016-01-07 22:28:35 -0500

    delete account

--- erudit/individual_subscription/urls.py
+++ erudit/individual_subscription/urls.py
@@ -4,11 +4,13 @@ from django.conf.urls import url
 from .views import (
     IndividualAccountCreate,
     IndividualAccountUpdate,
+    IndividualAccountDelete,
     IndividualAccountList
 )
 
 urlpatterns = [
     url(r'^acces/$', IndividualAccountList.as_view(), name='account_list'),
     url(r'^acces/ajout', IndividualAccountCreate.as_view(), name='account_add'),
+    url(r'^acces/supprimer/(?P<pk>[0-9]+)/$', IndividualAccountDelete.as_view(), name='account_delete'),
     url(r'^acces/(?P<pk>[0-9]+)/$', IndividualAccountUpdate.as_view(), name='account_update'),
 ]
