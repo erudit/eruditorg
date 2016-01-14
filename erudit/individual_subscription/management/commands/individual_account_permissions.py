e6a75ad5ece571c803635016b03883d9c14f0217
Olivier Larchevêque <olivier.larcheveque@gmail.com>
2016-01-13 23:15:16 -0500

    add revue policy

--- erudit/individual_subscription/management/commands/individual_account_permissions.py
+++ erudit/individual_subscription/management/commands/individual_account_permissions.py
@@ -1,14 +1,12 @@
-from erudit.models import Journal
-
 from django.core.management.base import BaseCommand
 
-from ...models import OrganizationPolicy, IndividualAccountJournal
+from ...models import Policy
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.verbosity = int(options['verbosity'])
-        organisations = OrganizationPolicy.objects.all()
-        for organisation in organisations:
-            organisation.generate_flat_access()
+        policies = Policy.objects.all()
+        for policy in policies:
+            policy.generate_flat_access()
