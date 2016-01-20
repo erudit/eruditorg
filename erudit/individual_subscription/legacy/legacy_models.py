500dd847db24bc26333667dbaed82f59b077a4ee
Olivier Larchevêque <olivier.larcheveque@gmail.com>
2016-01-19 23:35:13 -0500

    pep8 E501 fix

--- erudit/individual_subscription/legacy/legacy_models.py
+++ erudit/individual_subscription/legacy/legacy_models.py
@@ -2,7 +2,8 @@
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
 #   * Make sure each model has one field with primary_key=True
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify,
+#   and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 #
 # Also note: You'll have to insert the output of 'django-admin sqlcustom [app_label]'
