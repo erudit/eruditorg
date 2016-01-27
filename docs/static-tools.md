76a84ca05cb41178791abee0c864a17e3b97e608
Antoine Girard <thetoine@gmail.com>
2016-01-27 16:11:59 -0500

    NPM and Bower config file. Gulp-file for SASS compression. Installed some vendors

---
+++ docs/static-tools.md
@@ -0,0 +1,11 @@
+# Static tools
+
+TODO: how to use...
+
+### Install NPM package
+
+  sudo npm install --save-dev
+
+### start Gulp
+
+  ./node_modules/.bin/gulp --gulpfile ./tools/static/gulpfile.js watch
---
+++ tools/static/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp       = require('gulp'),
+    sass       = require('gulp-sass'),
+    path       = require('path');
+
+// display (or swallow?) an error in console
+// also prevent error to stop the gulp watch process unless restart
+function swallowError (error) {
+  console.log(error.toString());
+  this.emit('end');
+};
+
+gulp.task('sass', function() {
+  return gulp.src('../../erudit/erudit/static/sass/main.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('../../erudit/erudit/static/sass/'));
+});
+
+gulp.task('watch', function() {
+  // watch any less file /css directory, ** is for recursive mode
+  gulp.watch('../../erudit/erudit/static/sass/**/*.scss', ['sass']);
+});
