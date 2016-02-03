0b0aee993bae8cf79c0bd1d50ae18198712c60b2
Antoine Girard <thetoine@gmail.com>
2016-02-03 12:03:20 -0500

    space, not tab in context_processors for constants

--- erudit/base/context_processors.py
+++ erudit/base/context_processors.py
@@ -4,26 +4,24 @@ import socket
 from django.conf import settings
 
 try:
-	HOSTNAME = socket.gethostname()
+    HOSTNAME = socket.gethostname()
 except:
-	HOSTNAME = 'localhost'
+    HOSTNAME = 'localhost'
 
 
 def common_settings(request):
-	"""Passing custom CONSTANT in Settings into RequestContext."""
-	from django.contrib.sites.models import get_current_site
+    """Passing custom CONSTANT in Settings into RequestContext."""
+    from django.contrib.sites.models import get_current_site
+    COMMON_CONTEXT = {
+        "DEBUG": settings.DEBUG,
+        "HOSTNAME": HOSTNAME,
+        "CURRENT_DOMAIN": get_current_site(request).domain,
+    }
 
+    try:
+        # set EXTRA_CONTEXT in local settings
+        COMMON_CONTEXT.update(settings.EXTRA_CONTEXT)
+    except:
+        pass
 
-	COMMON_CONTEXT = {
-		"DEBUG": settings.DEBUG,
-		"HOSTNAME": HOSTNAME,
-		"CURRENT_DOMAIN": get_current_site(request).domain,
-	}
-
-	try:
-		# set EXTRA_CONTEXT in local settings
-		COMMON_CONTEXT.update(settings.EXTRA_CONTEXT)
-	except:
-		pass
-
-	return COMMON_CONTEXT
+    return COMMON_CONTEXT
