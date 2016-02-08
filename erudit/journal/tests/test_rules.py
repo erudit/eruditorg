48e8a0aa90f7aa336333230e070bb58eb0a97659
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-02-08 11:58:18 -0500

    Add missing tests

--- erudit/journal/tests/test_rules.py
+++ erudit/journal/tests/test_rules.py
@@ -24,4 +24,15 @@ class TestEditJournalRule(BaseEruditTestCase):
         self.assertTrue(user.has_perm('journal.edit_journal'))
         self.assertTrue(user.has_perm('journal.edit_journal', self.journal))
 
-    # TODO: add missing tests when the work on permissions is done
+    def test_knows_if_a_simple_user_can_edit_a_journal(self):
+        # Run & check
+        self.assertTrue(self.user.has_perm('journal.edit_journal'))
+        self.assertTrue(self.user.has_perm('journal.edit_journal', self.journal))
+
+    def test_knows_if_a_simple_user_cannot_edit_a_journal(self):
+        # Setup
+        user = User.objects.create_user(
+            username='staff', email='admin@xyz.com', password='top_secret')
+        # Run & check
+        self.assertFalse(user.has_perm('journal.edit_journal'))
+        self.assertFalse(user.has_perm('journal.edit_journal', self.journal))
