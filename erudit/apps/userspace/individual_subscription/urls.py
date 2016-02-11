19e51b0666ba15911598e2c937ba3cb8984f921e
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-02-11 13:11:44 -0500

    Add missing i18n URLs

--- erudit/apps/userspace/individual_subscription/urls.py
+++ erudit/apps/userspace/individual_subscription/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.utils.translation import ugettext_lazy as _
 
 
 from .views import (
@@ -10,11 +11,11 @@ from .views import (
 )
 
 urlpatterns = [
-    url(r'^acces/$', IndividualAccountList.as_view(), name='account_list'),
-    url(r'^acces/ajout', IndividualAccountCreate.as_view(), name='account_add'),
-    url(r'^acces/supprimer/(?P<pk>[0-9]+)/$', IndividualAccountDelete.as_view(),
+    url(_(r'^acces/$'), IndividualAccountList.as_view(), name='account_list'),
+    url(_(r'^acces/ajout'), IndividualAccountCreate.as_view(), name='account_add'),
+    url(_(r'^acces/supprimer/(?P<pk>[0-9]+)/$'), IndividualAccountDelete.as_view(),
         name='account_delete'),
-    url(r'^acces/(?P<pk>[0-9]+)/$', IndividualAccountUpdate.as_view(), name='account_update'),
-    url(r'^acces/nouveau-mdp/(?P<pk>[0-9]+)/$', IndividualAccountResetPwd.as_view(),
+    url(_(r'^acces/(?P<pk>[0-9]+)/$'), IndividualAccountUpdate.as_view(), name='account_update'),
+    url(_(r'^acces/nouveau-mdp/(?P<pk>[0-9]+)/$'), IndividualAccountResetPwd.as_view(),
         name='account_reset_pwd'),
 ]
--- erudit/apps/userspace/permissions/urls.py
+++ erudit/apps/userspace/permissions/urls.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 from . import views
 
 urlpatterns = [
-    url(_(r'^$'), views.PermissionsListView.as_view(), name='perm_list'),
+    url(r'^$', views.PermissionsListView.as_view(), name='perm_list'),
     url(_(r'^ajout/$'), views.PermissionsCreateView.as_view(),
         name='perm_create'),
     url(_(r'(?P<pk>[0-9]+)/supprimer/$'),
