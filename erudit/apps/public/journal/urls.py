0c69f30eec936e5be2805f2b1cf9c4e44ee349f8
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-02-11 17:17:06 -0500

    Start adding journal information to journal detail view

--- erudit/apps/public/journal/urls.py
+++ erudit/apps/public/journal/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
 
     # Compatibility URLs
     url('^', include(urls_compat.urlpatterns)),
+
+    # Journal catchall
+    url(r'^(?P<code>[\w-]+)/$', views.JournalDetailView.as_view(), name='journal-detail'),
 ]
--- erudit/apps/public/urls.py
+++ erudit/apps/public/urls.py
@@ -6,9 +6,9 @@ from django.utils.translation import ugettext_lazy as _
 
 
 urlpatterns = [
-    url(r'^', include('apps.public.journal.urls', namespace='journal')),
-
     url(_(r'^livre/'), include('apps.public.book.urls', namespace='book')),
     url(_(r'^recherche/'), include('apps.public.search.urls', namespace='search')),
     url(_(r'^these/'), include('apps.public.thesis.urls', namespace='thesis')),
+
+    url(r'^', include('apps.public.journal.urls', namespace='journal')),
 ]
