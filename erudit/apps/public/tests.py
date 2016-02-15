d2005105d6adffa6a20341016824f6d44cd6ef5a
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-02-15 14:51:20 -0500

    Add test

---
+++ erudit/apps/public/tests.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import datetime as dt
+
+from django.core.urlresolvers import reverse
+
+from erudit.factories import IssueFactory
+from erudit.tests import BaseEruditTestCase
+
+
+class TestHomeView(BaseEruditTestCase):
+    def test_embeds_the_latest_issues_into_the_context(self):
+        # Setup
+        issue_1 = IssueFactory.create(
+            journal=self.journal, date_published=dt.datetime.now() - dt.timedelta(days=1))
+        issue_2 = IssueFactory.create(journal=self.journal, date_published=dt.datetime.now())
+        url = reverse('public:home')
+        # Run
+        response = self.client.get(url)
+        # Check
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(list(response.context['latest_issues']), [issue_2, issue_1, ])
