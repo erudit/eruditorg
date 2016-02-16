332272c00eaea9f84da420189b1c8c7a25cc4f6c
Olivier Larchevêque <olivier.larcheveque@gmail.com>
2016-02-15 22:07:06 -0500

    refac to use predicate generator

--- erudit/core/userspace/rules.py
+++ erudit/core/userspace/rules.py
@@ -1,24 +1,12 @@
-from django.contrib.contenttypes.models import ContentType
 import rules
 from rules.predicates import is_staff, is_superuser
-from core.permissions.models import Rule
+
 from core.permissions.predicates import HasPermission
+from erudit.rules import is_journal_member
 
-
-@rules.predicate
-def can_manage_permissions(user, journal=None):
-    if journal is None:
-        return bool(Rule.objects.filter(
-            user=user,
-            permission='userspace.manage_permissions').count())
-    else:
-        ct = ContentType.objects.get(app_label="erudit", model="journal")
-        return bool(Rule.objects.filter(
-            user=user,
-            content_type=ct,
-            object_id=journal.id,
-            permission='userspace.manage_permissions').count())
-
-rules.add_perm('userspace.manage_permissions',
-               is_superuser | is_staff |
-               HasPermission("userspace.manage_permissions"))
+# This permission assume to use a 'Journal' object to perform the perm check
+rules.add_perm(
+    'userspace.manage_permissions',
+    is_superuser | is_staff |
+    is_journal_member & HasPermission("userspace.manage_permissions"),
+)
