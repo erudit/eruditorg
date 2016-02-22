b265a4feeecd6b954b576760e21ad0a8aa786e16
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-02-22 15:24:39 -0500

    Add ip_addresses property to InstitutionIPAddressRange

--- erudit/core/subscription/tests/test_models.py
+++ erudit/core/subscription/tests/test_models.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import ipaddress
+
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
@@ -98,5 +102,22 @@ class TestInstitutionIPAddressRange(TestCase):
         # Run & check
         with self.assertRaises(ValidationError):
             ip_range = InstitutionIPAddressRangeFactory.build(
+                institutional_account=self.institutional_account,
                 ip_start='192.168.1.3', ip_end='192.168.1.2')
             ip_range.clean()
+
+    def test_can_return_the_list_of_corresponding_ip_addresses(self):
+        # Setup
+        ip_range = InstitutionIPAddressRangeFactory.build(
+            institutional_account=self.institutional_account,
+            ip_start='192.168.1.3', ip_end='192.168.1.5')
+        # Run
+        ip_addresses = ip_range.ip_addresses
+        # Check
+        self.assertEqual(
+            ip_addresses, [
+                ipaddress.ip_address('192.168.1.3'),
+                ipaddress.ip_address('192.168.1.4'),
+                ipaddress.ip_address('192.168.1.5'),
+            ]
+        )
