14f8ab1d475fe29afc2d3444a12a61aacf81443d
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-02-24 17:29:13 -0500

    Fix #697 - Add video and video.js

--- erudit/base/settings/base.py
+++ erudit/base/settings/base.py
@@ -121,6 +121,15 @@ PIPELINE = {
                 'media': 'screen,projection',
             },
         },
+        'videojs': {
+            'source_filenames': (
+                'vendor/video.js/dist/video-js.min.css',
+            ),
+            'output_filename': 'css/videojs.css',
+            'extra_context': {
+                'media': 'screen,projection',
+            },
+        },
     },
     'JAVASCRIPT': {
         # main js file for erudit.org
@@ -148,6 +157,13 @@ PIPELINE = {
                 'vendor/pdfjs-build/generic/web/viewer.js',
             ),
             'output_filename': 'js/pdfjs.min.js',
+        },
+        'videojs': {
+            'source_filenames': (
+                'vendor/video.js/dist/video.min.js',
+                'vendor/videojs-vimeo/src/Vimeo.js',
+            ),
+            'output_filename': 'js/videojs.min.js',
         },
     }
 }
