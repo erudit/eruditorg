83add31caa2ae3888352a71d5ef1928724b405ba
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-03-04 10:17:09 -0500

    Add missing migrations

---
+++ erudit/core/subscription/migrations/0005_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('subscription', '0003_merge'),
+        ('subscription', '0004_auto_20160225_1441'),
+    ]
+
+    operations = [
+    ]
---
+++ erudit/erudit/migrations/0009_merge.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('erudit', '0008_event'),
+        ('erudit', '0005_author_suffix'),
+        ('erudit', '0008_auto_20160225_1108'),
+        ('erudit', '0008_journal_upcoming'),
+    ]
+
+    operations = [
+    ]
