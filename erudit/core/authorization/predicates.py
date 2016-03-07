f580501db07927b1ea2b26a4cb43db12bbce5799
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-03-07 12:32:40 -0500

    Update HasAuthorization predicate to allow checks on user groups (including tests)

--- erudit/core/authorization/predicates.py
+++ erudit/core/authorization/predicates.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
+from django.db.models import Q
 from rules import Predicate
 
 from .defaults import AuthorizationDef
@@ -18,17 +20,22 @@ class HasAuthorization(object):
             if isinstance(authorization, AuthorizationDef) else authorization
 
         def check(user, obj=None):
-            if obj is None:
-                return bool(Authorization.objects.filter(
-                    user=user, authorization_codename=authorization_codename).count())
-            else:
+            user_model = get_user_model()
+            user_groups_related_name = user_model.groups.field.related_query_name()
+
+            # Defines the main Authorization queryset
+            authorization_qs = Authorization.objects \
+                .filter(authorization_codename=authorization_codename) \
+                .filter(Q(**{'group__{}'.format(user_groups_related_name): user}) | Q(user=user))
+
+            if obj is not None:
                 if foreign_key:
                     obj = getattr(obj, foreign_key)
                 app_label = obj._meta.app_label
                 model_name = obj.__class__.__name__.lower()
                 ct = ContentType.objects.get(app_label=app_label, model=model_name)
-                return bool(Authorization.objects.filter(
-                    user=user, content_type=ct, object_id=obj.id,
-                    authorization_codename=authorization_codename).count())
+                authorization_qs = authorization_qs.filter(content_type=ct, object_id=obj.id)
+
+            return authorization_qs.exists()
 
         return Predicate(check)
