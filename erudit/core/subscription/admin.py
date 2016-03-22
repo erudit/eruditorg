40c5afcddcc7bda2c4dc1c364e7d103fae141d1c
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-03-22 15:18:03 -0400

    Add new subscription models

--- erudit/core/subscription/admin.py
+++ erudit/core/subscription/admin.py
@@ -2,26 +2,13 @@
 
 from django.contrib import admin
 
-from .models import InstitutionalAccount
 from .models import InstitutionIPAddressRange
+from .models import JournalAccessSubscription
+from .models import JournalManagementPlan
+from .models import JournalManagementSubscription
 
 
-class JournalAdmin(admin.ModelAdmin):
-    fields = ('name', )
-    readonly_fields = ('name', )
-
-    def has_add_permission(self, request):
-        """
-        Only provide policy settings for existing Journal.
-        """
-        return False
-
-    def has_delete_permission(self, request, pk=None):
-        """
-        Only provide policy settings for existing Journal.
-        """
-        return False
-
-
-admin.site.register(InstitutionalAccount)
 admin.site.register(InstitutionIPAddressRange)
+admin.site.register(JournalAccessSubscription)
+admin.site.register(JournalManagementPlan)
+admin.site.register(JournalManagementSubscription)
--- erudit/core/subscription/models.py
+++ erudit/core/subscription/models.py
@@ -3,31 +3,45 @@
 import ipaddress
 from functools import reduce
 
+from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.core.exceptions import ValidationError
 
+from erudit.models import Collection
+from erudit.models import Journal
 from erudit.models import Organisation
 
+from .abstract_models import AbstractSubscription
 
-class InstitutionalAccount(models.Model):
+
+class JournalAccessSubscription(AbstractSubscription):
+    """ Defines a subscription allowing a user or an organisation to access journals.
+
+    The subscription can associate many Journal instances to the user or the organisation.
+    A subscription for collection of journals or a "full access" subscription can also be specified.
     """
-    An institutional account defines how an institution can access
-    protected content.
-    """
-    institution = models.ForeignKey(Organisation, verbose_name=_('Organisation'))
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, verbose_name=_('Utilisateur'), blank=True, null=True)
+    organisation = models.ForeignKey(
+        Organisation, verbose_name=_('Organisation'), blank=True, null=True)
+
+    # Which Journal instances can be accessed using this subscription?
+    journal = models.ForeignKey(
+        Journal, verbose_name=_('Revue'), blank=True, null=True, related_name='+')
+    collection = models.ForeignKey(
+        Collection, verbose_name=_('Collection'), blank=True, null=True, related_name='+')
+    journals = models.ManyToManyField(Journal, verbose_name=_('Revues'), related_name='+')
+    full_access = models.BooleanField(default=False, verbose_name=_('Accès complet'))
 
     class Meta:
-        verbose_name = _('Compte institutionnel')
-        verbose_name_plural = _('Comptes institutionnel')
-
-    def __str__(self):
-        return str(self.institution)
+        verbose_name = _('Abonnement aux revues')
+        verbose_name_plural = _('Abonnements aux revues')
 
 
 class InstitutionIPAddressRange(models.Model):
-    institutional_account = models.ForeignKey(
-        InstitutionalAccount, verbose_name=_('Compte institutionnel'))
+    subscription = models.ForeignKey(
+        JournalAccessSubscription, verbose_name=_('Abonnement aux revues'))
     ip_start = models.GenericIPAddressField(verbose_name=_('Adresse IP de début'))
     ip_end = models.GenericIPAddressField(verbose_name=_('Adresse IP de fin'))
 
@@ -55,3 +69,24 @@ class InstitutionIPAddressRange(models.Model):
         return reduce(
             lambda ips, ipn: ips + list(ipn),
             ipaddress.summarize_address_range(start, end), [])
+
+
+class JournalManagementSubscription(AbstractSubscription):
+    """ Defines a subscription allowing the members of a journal to manage its subscriptions. """
+    journal = models.ForeignKey(Journal, verbose_name=_('Revue'))
+    plan = models.ForeignKey('JournalManagementPlan', verbose_name=_('Forfait'))
+
+    class Meta:
+        verbose_name = _('Abonnement de gestion de revue')
+        verbose_name_plural = _('Abonnements de gestion de revue')
+
+
+class JournalManagementPlan(models.Model):
+    """ Defines the limits of the possibilities provided by a journal management subscription. """
+    title = models.CharField(max_length=255, verbose_name=_('Titre'), blank=True, null=True)
+    code = models.SlugField(max_length=100, unique=True, verbose_name=_('Code'))
+    max_accounts = models.PositiveSmallIntegerField(verbose_name=_('Maximum de comptes'))
+
+    class Meta:
+        verbose_name = _("Forfait de gestion d'une revue")
+        verbose_name_plural = _("Forfaits de gestion de revues")
