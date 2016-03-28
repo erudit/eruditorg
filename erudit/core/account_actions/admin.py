47fa44ddff1cd236b1fef8968fbbba0776514e7c
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-03-28 10:07:04 -0400

    Improve AccountActionToken admin integration

--- erudit/core/account_actions/admin.py
+++ erudit/core/account_actions/admin.py
@@ -6,7 +6,9 @@ from .models import AccountActionToken
 
 
 class AccountActionTokenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'key', 'email', 'first_name', 'last_name', )
+    list_display = (
+        'id', 'key', 'email', 'first_name', 'last_name', 'expiration_date', 'is_expired',
+        'is_consumed', )
     list_display_links = ('id', 'key', 'email', )
 
 
