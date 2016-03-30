8980d1172fa555a5088fe9efca499d5567a4f503
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-03-30 11:41:18 -0400

    Update the subscription.manage_individual_subscription permission to use JournalManagementSubscription instances

--- erudit/core/subscription/rules.py
+++ erudit/core/subscription/rules.py
@@ -9,11 +9,21 @@ from core.authorization.defaults import AuthorizationConfig as AC
 from core.authorization.predicates import HasAuthorization
 from erudit.rules import is_journal_member
 
+from .models import JournalManagementSubscription
+
+
+@rules.predicate
+def has_journal_management_subscription(user, journal):
+    return JournalManagementSubscription.objects.filter(journal=journal).exists()
+
 
 rules.add_perm(
     'subscription.manage_individual_subscription',
     is_authenticated & (
-        is_superuser | is_staff |
-        is_journal_member & HasAuthorization(AC.can_manage_individual_subscription)
+        is_superuser | is_staff | (
+            is_journal_member &
+            has_journal_management_subscription &
+            HasAuthorization(AC.can_manage_individual_subscription)
+        )
     ),
 )
