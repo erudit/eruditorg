5e1c5c556cbd005626c45e30c76cd441edfe2370
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-03-31 17:15:59 -0400

    Limit user choices when creating authorizations

--- erudit/apps/userspace/journal/authorization/tests/test_forms.py
+++ erudit/apps/userspace/journal/authorization/tests/test_forms.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib.contenttypes.models import ContentType
+
 from core.authorization.defaults import AuthorizationConfig as AC
 from core.authorization.models import Authorization
 from erudit.tests.base import BaseEruditTestCase
@@ -39,3 +41,20 @@ class TestAuthorizationForm(BaseEruditTestCase):
         self.assertEqual(authorization.content_object, self.journal)
         self.assertEqual(
             authorization.authorization_codename, AC.can_manage_issuesubmission.codename)
+
+    def test_do_not_allow_to_create_an_authorization_for_a_user_that_is_already_authorized(self):
+        # Setup
+        ct = ContentType.objects.get(app_label="erudit", model="journal")
+        Authorization.objects.create(
+            content_type=ct,
+            user=self.user,
+            object_id=self.journal.id,
+            authorization_codename=AC.can_manage_issuesubmission.codename)
+        form_data = {
+            'user': self.user.id,
+        }
+        form = AuthorizationForm(
+            form_data, codename=AC.can_manage_issuesubmission.codename, journal=self.journal)
+        # Run & check
+        self.assertFalse(form.is_valid())
+        self.assertTrue('user' in form.errors)
