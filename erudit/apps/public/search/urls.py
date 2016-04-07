0366591c2abf23f0b790dac3e05b960e06fb4aeb
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-04-07 11:42:07 -0400

    Update 'search' compatibility urls

--- erudit/apps/public/search/urls.py
+++ erudit/apps/public/search/urls.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
-# from django.conf.urls import include
+from django.conf.urls import include
 from django.conf.urls import url
 
-# from . import urls_compat
+from . import urls_compat
 from . import views
 
 
 urlpatterns = [
-    # Compatibility URLs
-    # url('^', include(urls_compat.urlpatterns)),
     url(r'^$', views.Search.as_view(), name="search"),
+
+    # Compatibility URLs
+    url('^', include(urls_compat.urlpatterns)),
 ]
--- erudit/apps/public/search/urls_compat.py
+++ erudit/apps/public/search/urls_compat.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
+from django.views.generic import RedirectView
 
 from base.views import DummyView
 
 
 unsupported_patterns = [
-    r'^$',
-    r'^index.html?$',
     r'^(?P<code>[\w-]+)/?$',
 ]
 
-urlpatterns = [url(pattern_re, DummyView.as_view()) for pattern_re in unsupported_patterns]
+urlpatterns = [
+    url(r'^index\.html?$',
+        RedirectView.as_view(pattern_name='public:search:search', permanent=True))
+]
+
+urlpatterns += [url(pattern_re, DummyView.as_view()) for pattern_re in unsupported_patterns]
