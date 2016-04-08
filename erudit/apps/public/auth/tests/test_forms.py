a431e317a4dcf9f6a806e44e036a5a421f7d80dc
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-04-08 14:34:47 -0400

    Fix password reset email

--- erudit/apps/public/auth/tests/test_forms.py
+++ erudit/apps/public/auth/tests/test_forms.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib.auth.tokens import default_token_generator
+from django.core import mail
+
 from core.accounts.factories import AbonnementProfileFactory
 from core.accounts.models import AbonnementProfile
 from erudit.tests.base import BaseEruditTestCase
 
 from ..forms import PasswordChangeForm
+from ..forms import PasswordResetForm
 
 
 class TestPasswordChangeForm(BaseEruditTestCase):
@@ -77,3 +81,53 @@ class TestPasswordChangeForm(BaseEruditTestCase):
         form = PasswordChangeForm(self.user, form_data)
         self.assertFalse(form.is_valid())
         self.assertTrue('old_password' in form.errors)
+
+
+class TestPasswordResetForm(BaseEruditTestCase):
+    def test_can_properly_choose_standard_users_with_usable_passwords(self):
+        # Setup
+        self.user.email = 'foobar@example.com'
+        self.user.save()
+        # Run & check
+        form = PasswordResetForm({})
+        self.assertEqual(list(form.get_users('foobar@example.com')), [self.user, ])
+
+    def test_can_properly_choose_abonnementprofile_users_without_usable_passwords(self):
+        # Setup
+        self.user.set_unusable_password()
+        self.user.email = 'foobar@example.com'
+        self.user.save()
+        profile = AbonnementProfileFactory.create(user=self.user)
+        profile.password = profile.sha1('test-1')
+        profile.save()
+        # Run & check
+        form = PasswordResetForm({})
+        self.assertEqual(list(form.get_users('foobar@example.com')), [self.user, ])
+
+    def test_cannot_choose_users_without_usable_passwords(self):
+        # Setup
+        self.user.set_unusable_password()
+        self.user.email = 'foobar@example.com'
+        self.user.save()
+        # Run & check
+        form = PasswordResetForm({})
+        self.assertFalse(len(list(form.get_users('foobar@example.com'))))
+
+    def test_can_properly_send_the_reset_email(self):
+        # Setup
+        self.user.email = 'foobar@example.com'
+        self.user.save()
+        form_data = {
+            'email': 'foobar@example.com',
+        }
+        # Run & check
+        form = PasswordResetForm(form_data)
+        self.assertTrue(form.is_valid())
+        form.save(
+            domain_override=None,
+            subject_template_name='public/auth/emails/password_reset_subject.txt',
+            email_template_name='public/auth/emails/password_reset_email.html',
+            use_https=False, token_generator=default_token_generator,
+            from_email=None, request=None, html_email_template_name=None)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].to[0], 'foobar@example.com')
