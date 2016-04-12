cce3b72ffc302873fe19c6491a502c811ab62415
David Cormier <david.cormier@erudit.org>
2016-04-12 14:50:32 -0400

    recompile assets

---
+++ erudit/core/authorization/migrations/0002_auto_20160405_0927.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('authorization', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='authorization',
+            name='authorization_codename',
+            field=models.CharField(max_length=100, choices=[('authorization:can_manage_authorizations', 'Autorisations'), ('subscriptions:can_manage_individual_subscription', 'Abonnements'), ('editor:can_manage_issuesubmission', 'Dépôt de fichiers')]),
+        ),
+    ]
