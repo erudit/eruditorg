a366f20771ebce50d363c96074f49821da6ab469
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-04-18 12:12:49 -0400

    Add HasAnyAuthorization predicate helper to authorization core app

--- erudit/core/authorization/predicates.py
+++ erudit/core/authorization/predicates.py
@@ -9,6 +9,35 @@ from .defaults import AuthorizationDef
 from .models import Authorization
 
 
+class AuthorizationChecker(object):
+    def __init__(self, authorization_codenames, foreign_key=None):
+        self.authorization_codenames = authorization_codenames
+        self.foreign_key = foreign_key
+
+    def __call__(self, user, obj=None):
+        if user.is_anonymous():
+            # Anonymous users cannot have authorizations.
+            return False
+
+        user_model = get_user_model()
+        user_groups_related_name = user_model.groups.field.related_query_name()
+
+        # Defines the main Authorization queryset
+        authorization_qs = Authorization.objects \
+            .filter(authorization_codename__in=self.authorization_codenames) \
+            .filter(Q(**{'group__{}'.format(user_groups_related_name): user}) | Q(user=user))
+
+        if obj is not None:
+            if self.foreign_key:
+                obj = getattr(obj, self.foreign_key)
+            app_label = obj._meta.app_label
+            model_name = obj.__class__.__name__.lower()
+            ct = ContentType.objects.get(app_label=app_label, model=model_name)
+            authorization_qs = authorization_qs.filter(content_type=ct, object_id=obj.id)
+
+        return authorization_qs.exists()
+
+
 class HasAuthorization(object):
     """
     Class generator which return a predicate function parametrized by an
@@ -18,28 +47,17 @@ class HasAuthorization(object):
     def __new__(cls, authorization, foreign_key=None):
         authorization_codename = authorization.codename \
             if isinstance(authorization, AuthorizationDef) else authorization
+        return Predicate(AuthorizationChecker([authorization_codename, ], foreign_key))
 
-        def check(user, obj=None):
-            if user.is_anonymous():
-                # Anonymous users cannot have authorizations.
-                return False
 
-            user_model = get_user_model()
-            user_groups_related_name = user_model.groups.field.related_query_name()
+class HasAnyAuthorization(object):
+    """
+    Class generator which return a predicate function parametrized by a list of authorization
+    codenames. The predicate is verified if the user or the group has at least one of these
+    authorizations.
+    """
 
-            # Defines the main Authorization queryset
-            authorization_qs = Authorization.objects \
-                .filter(authorization_codename=authorization_codename) \
-                .filter(Q(**{'group__{}'.format(user_groups_related_name): user}) | Q(user=user))
-
-            if obj is not None:
-                if foreign_key:
-                    obj = getattr(obj, foreign_key)
-                app_label = obj._meta.app_label
-                model_name = obj.__class__.__name__.lower()
-                ct = ContentType.objects.get(app_label=app_label, model=model_name)
-                authorization_qs = authorization_qs.filter(content_type=ct, object_id=obj.id)
-
-            return authorization_qs.exists()
-
-        return Predicate(check)
+    def __new__(cls, authorizations, foreign_key=None):
+        authorization_codenames = [
+            a.codename if isinstance(a, AuthorizationDef) else a for a in authorizations]
+        return Predicate(AuthorizationChecker(authorization_codenames, foreign_key))
