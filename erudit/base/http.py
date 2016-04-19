eda2c1d1478fd3861f38fcca3d190e8b2b288e6e
Morgan Aubert <morgan.aubert@zoho.com>
2016-04-19 13:22:05 -0400

    Add JSON response shortcut objects

---
+++ erudit/base/http.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from django.http import JsonResponse
+
+
+def JsonAckResponse(**kwargs):
+    """ Returns a JsonResponse that acknowledges the success of an operation. """
+    json_dict = {'status': 'ok'}
+    json_dict.update(kwargs)
+    return JsonResponse(json_dict)
+
+
+def JsonErrorResponse(error, **kwargs):
+    """ Returns a JsonResponse that notifies an error that occured during an operation. """
+    json_dict = {'status': 'nok', 'error': error}
+    json_dict.update(kwargs)
+    return JsonResponse(json_dict)
