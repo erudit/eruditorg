5145fe8e939ac8a0ee5e0011a4592893f99b45a5
Morgan Aubert <morgan.aubert@zoho.com>
2016-04-22 15:14:04 -0400

    Start article serializer

--- erudit/erudit/serializers/core.py
+++ erudit/erudit/serializers/core.py
@@ -6,6 +6,24 @@ from .. import models as erudit_models
 
 
 class EruditDocumentSerializer(serializers.ModelSerializer):
+    real_object = serializers.SerializerMethodField()
+
     class Meta:
         model = erudit_models.EruditDocument
-        fields = ['id', 'localidentifier', ]
+        fields = ['localidentifier', 'real_object', ]
+
+    def get_real_object(self, obj):
+        # This method should return a serialized representation of an Érudit document ; it could be
+        # an article, a thesis, a book, ...
+        # However there is a problem: it not currently possible to determine the correct model
+        # associated with an EruditDocument instance. For now we use only Article objects so we
+        # will make the assumption that all Érudit documents are article... But this problem should
+        # be resolved using polymorphism.
+        article = erudit_models.Article.objects.get(id=obj.id)
+        return ArticleSerializer(article).data
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = erudit_models.Article
+        fields = ['title', 'surtitle', 'processing', ]
