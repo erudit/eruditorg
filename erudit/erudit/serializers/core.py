117e097202f115afffa5d4910cbcb478c58aa360
Morgan Aubert <morgan.aubert@zoho.com>
2016-04-25 17:38:42 -0400

    Update core serializers

--- erudit/erudit/serializers/core.py
+++ erudit/erudit/serializers/core.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from django.utils import formats
+from django.utils import translation
 from rest_framework import serializers
 
 from .. import models as erudit_models
@@ -10,7 +12,7 @@ class EruditDocumentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = erudit_models.EruditDocument
-        fields = ['localidentifier', 'real_object', ]
+        fields = ['id', 'localidentifier', 'real_object', ]
 
     def get_real_object(self, obj):
         # This method should return a serialized representation of an Érudit document ; it could be
@@ -19,11 +21,63 @@ class EruditDocumentSerializer(serializers.ModelSerializer):
         # associated with an EruditDocument instance. For now we use only Article objects so we
         # will make the assumption that all Érudit documents are article... But this problem should
         # be resolved using polymorphism.
-        article = erudit_models.Article.objects.get(id=obj.id)
+        article = erudit_models.Article.objects.select_related('issue', 'issue__journal') \
+            .get(id=obj.id)
         return ArticleSerializer(article).data
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    authors = serializers.SerializerMethodField()
+    abstract = serializers.SerializerMethodField()
+    first_page = serializers.SerializerMethodField()
+    last_page = serializers.SerializerMethodField()
+    journal_code = serializers.SerializerMethodField()
+    issue_localidentifier = serializers.SerializerMethodField()
+    issue_title = serializers.SerializerMethodField()
+    issue_number = serializers.SerializerMethodField()
+    issue_published = serializers.SerializerMethodField()
+
     class Meta:
         model = erudit_models.Article
-        fields = ['title', 'surtitle', 'processing', ]
+        fields = [
+            'journal_code', 'issue_localidentifier', 'issue_title', 'issue_number',
+            'issue_published', 'title', 'surtitle', 'processing', 'authors', 'abstract',
+            'first_page', 'last_page',
+        ]
+
+    def get_authors(self, obj):
+        return obj.erudit_object.authors
+
+    def get_abstract(self, obj):
+        abstracts = obj.erudit_object.abstracts
+        lang = translation.get_language()
+        _abstract = list(filter(lambda r: r['lang'] == lang, abstracts))
+
+        abstract = None
+        if len(_abstract):
+            abstract = _abstract[0]['content']
+        elif len(abstracts):
+            abstract = abstracts[0]['content']
+
+        return abstract
+
+    def get_first_page(self, obj):
+        return obj.erudit_object.first_page
+
+    def get_last_page(self, obj):
+        return obj.erudit_object.last_page
+
+    def get_journal_code(self, obj):
+        return obj.issue.journal.code
+
+    def get_issue_localidentifier(self, obj):
+        return obj.issue.localidentifier
+
+    def get_issue_title(self, obj):
+        return obj.issue.title
+
+    def get_issue_number(self, obj):
+        return obj.issue.erudit_object.number
+
+    def get_issue_published(self, obj):
+        return formats.date_format(obj.issue.date_published, 'YEAR_MONTH_FORMAT')
