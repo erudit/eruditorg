73ea5f23af84db5449af4587f843d29b50c7701a
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-05-20 16:36:48 -0400

    Add 'manage_organisation_subscription_ips' rule

--- eruditorg/core/subscription/rules.py
+++ eruditorg/core/subscription/rules.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import datetime as dt
+
 import rules
 from rules.predicates import is_authenticated
 from rules.predicates import is_staff
@@ -17,6 +19,16 @@ def has_journal_management_subscription(user, journal):
     return JournalManagementSubscription.objects.filter(journal=journal).exists()
 
 
+@rules.predicate
+def has_valid_subscription(user, organisation):
+    nowd = dt.datetime.now().date()
+    return user.organisations.filter(id=organisation.id).exists() and \
+        organisation.journalaccesssubscription_set.filter(
+            journalaccesssubscriptionperiod__start__lte=nowd,
+            journalaccesssubscriptionperiod__end__gte=nowd,
+        ).exists()
+
+
 rules.add_perm(
     'subscription.manage_individual_subscription',
     is_authenticated & (
@@ -24,6 +36,17 @@ rules.add_perm(
             is_journal_member &
             has_journal_management_subscription &
             HasAuthorization(AC.can_manage_individual_subscription)
+        )
+    ),
+)
+
+
+rules.add_perm(
+    'subscription.manage_organisation_subscription_ips',
+    is_authenticated & (
+        is_superuser | is_staff | (
+            has_valid_subscription &
+            HasAuthorization(AC.can_manage_organisation_subscription_ips)
         )
     ),
 )
