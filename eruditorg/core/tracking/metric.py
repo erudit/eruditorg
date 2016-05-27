68621dae123fb2fca376feac640a2b52ad59e378
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-05-27 17:02:58 -0400

    Fix metric docstring

--- eruditorg/core/tracking/metric.py
+++ eruditorg/core/tracking/metric.py
@@ -10,7 +10,7 @@ def metric(metric_name, num=1, tags={}, **kwargs):
     can take additional keyword arguments that will be stored into the fields associated with the
     point being created.
 
-    :param metric_name: The first number to add
+    :param metric_name: The name of the metric to increment
     :param num: The number to increment the metric with (it defaults to 1)
     :param tags: The tags to store in the InfluxDB point
     :type metric_name: str
