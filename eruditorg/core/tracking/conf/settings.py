f6b96fb1a06a6039a279cb3301098e8b064d94d6
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-05-30 11:20:17 -0400

    Add a way to deactivate tracking and improve InfluxDB error handling

--- eruditorg/core/tracking/conf/settings.py
+++ eruditorg/core/tracking/conf/settings.py
@@ -8,3 +8,7 @@ INFLUXDB_PORT = getattr(settings, 'TRACKING_INFLUXDB_PORT', 8086)
 INFLUXDB_DBNAME = getattr(settings, 'TRACKING_INFLUXDB_DBNAME', 'erudit-metrics')
 INFLUXDB_USER = getattr(settings, 'TRACKING_INFLUXDB_USER', 'root')
 INFLUXDB_PASSWORD = getattr(settings, 'TRACKING_INFLUXDB_PASSWORD', 'root')
+
+# This setting can be used to deactivate the tracking. It will silent any error that could be logged
+# if the InfluxDB server is not reachable.
+ACTIVATED = getattr(settings, 'TRACKING_ACTIVATED', True)
