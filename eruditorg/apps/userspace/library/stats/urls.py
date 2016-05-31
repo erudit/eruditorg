ca900396407999af182fad57256be673f1efc21c
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-05-31 15:34:20 -0400

    Add views to generate Journal Report 1 and Journal Report 1 GOA CSV Counter reports

---
+++ eruditorg/apps/userspace/library/stats/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+from django.utils.translation import ugettext_lazy as _
+
+from . import views
+
+urlpatterns = [
+    url(_(r'^rapport/jr1/csv/$'),
+        views.CounterJournalReport1CsvView.as_view(), name='jr1-csv-report'),
+    url(_(r'^rapport/jr1-goa/csv/$'),
+        views.CounterJournalReport1GOACsvView.as_view(), name='jr1-goa-csv-report'),
+]
