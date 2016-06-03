aa4bd377cdbaca79a0bc3eb1dcfeca0e5990b86f
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-06-03 10:37:44 -0400

    Add XML support to the library userspace's stats section

--- eruditorg/apps/userspace/library/stats/views.py
+++ eruditorg/apps/userspace/library/stats/views.py
@@ -9,10 +9,12 @@ from django.views.generic import View
 
 from base.viewmixins import LoginRequiredMixin
 from base.viewmixins import MenuItemMixin
+from core.counter.counter import JournalReport1
+from core.counter.counter import JournalReport1GOA
+from core.counter.csv import get_csv_journal_counter_report_rows
+from core.counter.xml import get_xml_journal_counter_report
 
 from ..viewmixins import OrganisationScopePermissionRequiredMixin
-from .counter import JournalReport1
-from .counter import JournalReport1GOA
 
 
 class StatsLandingView(
@@ -84,57 +86,6 @@ class CounterJournalReportView(LoginRequiredMixin, OrganisationScopePermissionRe
 
 
 class CounterJournalReportCsvView(CounterJournalReportView):
-    def get_csv_rows(self, report):
-        """ Returns the rows to write into the Counter CSV report. """
-        rows = []
-
-        # Add the header rows
-        rows.append([report.title, report.subtitle])
-        rows.append([self.current_organisation.name])
-        rows.append([])
-        rows.append(['Period covered by Report'])
-        rows.append(['{start} to {end}'.format(
-            start=report.start.isoformat(), end=report.end.isoformat())])
-        rows.append(['Date run'])
-        rows.append([dt.datetime.now().isoformat()])
-
-        # Add the sub-header row
-        rows.append([
-            'Journal', 'Publisher', 'Platform', 'Journal DOI', 'Proprietary Identifier',
-            'Print ISSN', 'Online ISSN', 'Reporting Period Total', 'Reporting Period HTML',
-            'Reporting Period PDF',
-        ] + [m['period_title'] for m in report.months])
-
-        # Add the "total" row
-        rows.append([
-            'Total for all journals',
-            '',  # Publisher
-            '',  # Platform
-            '',  # Journal DOI
-            '',  # Proprietary Identifier
-            '',  # Print ISSN
-            '',  # Online ISSN
-            report.total['reporting_period_total'],
-            report.total['reporting_period_html'],
-            report.total['reporting_period_pdf'],
-        ] + report.total['months'])
-
-        for jdata in report.journals:
-            rows.append([
-                jdata['journal'].name,
-                jdata['journal'].publishers.first(),
-                jdata['journal'].collection.name,
-                '',  # Journal DOI
-                '',  # Proprietary Identifier
-                jdata['journal'].issn_print,
-                jdata['journal'].issn_web,
-                jdata['reporting_period_total'],
-                jdata['reporting_period_html'],
-                jdata['reporting_period_pdf'],
-            ] + jdata['months'])
-
-        return rows
-
     def render_report(self, report):
         """ Renders the Counter report as a CSV file. """
         # Prepares the response object
@@ -144,7 +95,9 @@ class CounterJournalReportCsvView(CounterJournalReportView):
 
         # Writes the CSV rows
         writer = csv.writer(response)
-        writer.writerows(self.get_csv_rows(report))
+        writer.writerows(
+            get_csv_journal_counter_report_rows(
+                report, organisation_name=self.current_organisation.name))
 
         return response
 
@@ -154,4 +107,20 @@ class CounterJournalReport1CsvView(CounterJournalReportCsvView):
 
 
 class CounterJournalReport1GOACsvView(CounterJournalReportCsvView):
+    report_class = JournalReport1GOA
+
+
+class CounterJournalReportXmlView(CounterJournalReportView):
+    def render_report(self, report):
+        """ Renders the Counter report as a XML file. """
+        return HttpResponse(
+            get_xml_journal_counter_report(report, self.current_organisation.name),
+            content_type='text/xml')
+
+
+class CounterJournalReport1XmlView(CounterJournalReportXmlView):
+    report_class = JournalReport1
+
+
+class CounterJournalReport1GOAXmlView(CounterJournalReportXmlView):
     report_class = JournalReport1GOA
