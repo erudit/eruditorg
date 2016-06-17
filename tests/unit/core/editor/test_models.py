d5578d57cd5580b91fc973b5de5d39b07ea015e9
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-06-17 15:46:31 -0400

    Add a is_draft property to the IssueSubmission model

--- tests/unit/core/editor/test_models.py
+++ tests/unit/core/editor/test_models.py
@@ -1,18 +1,27 @@
 # -* coding: utf-8 -*-
 
-from erudit.test import BaseEruditTestCase
+from base.test.testcases import EruditTestCase
 
 from core.editor.models import IssueSubmission
 from core.editor.test.factories import IssueSubmissionFactory
 
 
-class IssueSubmissionTestCase(BaseEruditTestCase):
+class TestIssueSubmission(EruditTestCase):
 
     def test_version(self):
         issue = IssueSubmissionFactory.create(journal=self.journal)
         new_files_version = issue.save_version()
-        self.assertEqual(issue.files_versions.count(), 2)
-        self.assertEqual(issue.last_files_version, new_files_version)
+        assert issue.files_versions.count() == 2
+        assert issue.last_files_version == new_files_version
+
+    def test_knows_if_it_is_a_draft(self):
+        # Setup
+        issue_1 = IssueSubmissionFactory.create(journal=self.journal)
+        issue_2 = IssueSubmissionFactory.create(journal=self.journal)
+        issue_2.submit()
+        # Run & check
+        assert issue_1.is_draft
+        assert not issue_2.is_draft
 
     def test_knows_if_it_is_submitted(self):
         # Setup
@@ -20,8 +29,8 @@ class IssueSubmissionTestCase(BaseEruditTestCase):
         issue_2 = IssueSubmissionFactory.create(journal=self.journal)
         issue_2.submit()
         # Run & check
-        self.assertFalse(issue_1.is_submitted)
-        self.assertTrue(issue_2.is_submitted)
+        assert not issue_1.is_submitted
+        assert issue_2.is_submitted
 
     def test_knows_if_it_is_validated(self):
         # Setup
@@ -30,8 +39,8 @@ class IssueSubmissionTestCase(BaseEruditTestCase):
         issue_2.submit()
         issue_2.approve()
         # Run & check
-        self.assertFalse(issue_1.is_validated)
-        self.assertTrue(issue_2.is_validated)
+        assert not issue_1.is_validated
+        assert issue_2.is_validated
 
     def test_knows_if_it_is_archived(self):
         # Setup
@@ -41,17 +50,16 @@ class IssueSubmissionTestCase(BaseEruditTestCase):
         issue_2.approve()
         issue_2.archive()
         # Run & check
-        self.assertFalse(issue_1.is_archived)
-        self.assertTrue(issue_2.is_archived)
+        assert not issue_1.is_archived
+        assert issue_2.is_archived
 
 
-class IssueSubmissionWorkflowTestCase(BaseEruditTestCase):
-
+class TestIssueSubmissionWorkflow(EruditTestCase):
     def test_refuse(self):
         issue = IssueSubmissionFactory(journal=self.journal)
         issue.submit()
         issue.refuse()
 
         issues = IssueSubmission.objects.all().order_by('id')
-        self.assertEqual(issues.count(), 1)
-        self.assertEqual(issues.first().files_versions.count(), 2)
+        assert issues.count() == 1
+        assert issues.first().files_versions.count() == 2
