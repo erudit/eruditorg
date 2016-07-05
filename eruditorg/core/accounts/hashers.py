f923f69e7d216f699020a11d4b944fa2eb9e1502
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-07-05 14:17:38 -0400

    Update doc

--- eruditorg/core/accounts/hashers.py
+++ eruditorg/core/accounts/hashers.py
@@ -8,19 +8,25 @@ from django.contrib.auth.hashers import PBKDF2PasswordHasher
 
 
 class PBKDF2WrappedAbonnementsSHA1PasswordHasher(PBKDF2PasswordHasher):
+    """ This hasher wraps existing 'abonnements' SHA1 passwords.
+
+    This password hasher allow to store weak hashes from the 'abonnement' DB (SHA1) using the
+    PBKDF2(AbonnementsSHA1(password)) algorithm.
+    """
     algorithm = 'pbkdf2_wrapped_abonnements_sha1'
 
     def encode_sha1_hash(self, sha1_hash, salt, iterations=None):
+        """ Encodes a SHA1-password that comes from the 'abonnement' DB. """
         return super(PBKDF2WrappedAbonnementsSHA1PasswordHasher, self).encode(
             sha1_hash, salt, iterations)
 
     def encode(self, password, salt, iterations=None, legacy_salt=None):
-        """ Encode using the crypt function from the legacy system. """
+        """ Encodes a password using the Abonnements-SHA1 algorithm from the legacy system. """
         sha1_hash = self.sha1(password, legacy_salt)
         return self.encode_sha1_hash(sha1_hash, salt, iterations)
 
     def sha1(self, password, legacy_salt=None):
-        """ Encode using the crypt function from the legacy system. """
+        """ Encodes using the crypt function from the legacy system. """
         if legacy_salt is None:
             legacy_salt = settings.INDIVIDUAL_SUBSCRIPTION_SALT
         to_sha = password.encode('utf-8') + legacy_salt.encode('utf-8')
