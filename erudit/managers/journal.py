20a014554f3f170b5e6ed6e45c2dd5a350ec21c5
David Cormier (Érudit) <david.cormier@erudit.org>
2016-08-08 14:40:42 -0400

    Add a LegacyJournalManager
    Can be used to work around quirks of the legacy system.

--- erudit/managers/journal.py
+++ erudit/managers/journal.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.db.models import Q
+
+
+class LegacyJournalManager(models.Manager):
+    """ Provides utility methods to work with journals in the legacy
+    databases """
+
+    def get_by_id(self, code):
+        """ Return the journal by id
+
+        The legacy system use a different id for scientific and cultural journals
+        For scientific journals, the identifier is the code (shortname)
+        For cultural journals, the identifier is the fedora localidentifier
+        """
+        return self.get(Q(code=code) | Q(localidentifier=code))
 
 
 class JournalUpcomingManager(models.Manager):
