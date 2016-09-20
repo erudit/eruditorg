0cce6c804027e954e95eac21fb05870838915129
Sophy <sophy.ouch@erudit.org>
2016-09-20 16:09:27 -0400

    Simplify sticky elements (filters, alpha nav bar)

--- eruditorg/static/js/controllers/public/journal/JournalListController.js
+++ eruditorg/static/js/controllers/public/journal/JournalListController.js
@@ -10,7 +10,7 @@ export default {
   		e.preventDefault();
   		e.stopPropagation();
       var target = $(this).attr('href').replace('#', '');
-  		$('html, body').animate( { scrollTop: $('a[name="'+target+'"]').offset().top -190 }, 750);
+  		$('html, body').animate( { scrollTop: $('a[name="'+target+'"]').offset().top - 137 }, 750);
   		return false;
   	});
   	$('#journal_list_per_disciplines .index').on('click', 'a', function(e) {
@@ -24,16 +24,7 @@ export default {
 
   stickyElements: function() {
     function stickyFilterForm(offset) {
-      let form = $('form#filter_form');
-      let footer = $('footer#site-footer');
-      let footerOffsetY = footer.offset().top - footer.height() - 80;
-      let filterFormTopOffset = form.offset().top;
-
-      if (filterFormTopOffset > footerOffsetY) {
-        form.hide();
-      } else if (filterFormTopOffset < footerOffsetY - 100) {
-        form.show();
-      }
+      let form = $('.filters');
 
       if ($(window).scrollTop() >= offset) {
         form.addClass('sticky');
@@ -43,16 +34,7 @@ export default {
     }
 
     function stickyItemsMenu(offset) {
-      let menu = $('#items_menu');
-      let footer = $('footer#site-footer');
-      let footerOffsetY = footer.offset().top - footer.height();
-      let filterFormTopOffset = menu.offset().top;
-
-      if (filterFormTopOffset > footerOffsetY) {
-        menu.hide();
-      } else if (filterFormTopOffset < footerOffsetY - 100) {
-        menu.show();
-      }
+      let menu = $('.list-header');
 
       if ($(window).scrollTop() >= offset) {
         menu.addClass('sticky');
@@ -62,9 +44,10 @@ export default {
     }
 
     $(window).scroll(function () {
-      var origOffsetY = $('#journal_list_per_names').offset().top + 100;
+      var origOffsetY = $('#journal_list_per_names').offset().top + 110;
       stickyFilterForm(origOffsetY);
       stickyItemsMenu(origOffsetY);
     });
   },
+
 };
