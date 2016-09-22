a0dfb7ef969fe13100aaaed8bde8db588faf1675
Sophy <sophy.ouch@erudit.org>
2016-09-22 12:15:15 -0400

    Simplify list content markup and re-name classes

--- eruditorg/static/js/controllers/public/thesis/CollectionListController.js
+++ eruditorg/static/js/controllers/public/thesis/CollectionListController.js
@@ -2,14 +2,14 @@ export default {
   init() {
     $('.thesis-extend').click(function(ev){
       ev.preventDefault();
-      $(this).parents('.thesis').find('.description').slideDown();
+      $(this).parents('.thesis').find('.thesis-description').slideDown();
       $(this).hide();
       $(this).parent().find('.thesis-reduce').show();
     });
 
     $('.thesis-reduce').click(function(ev){
       ev.preventDefault();
-      $(this).parents('.thesis').find('.description').slideUp();
+      $(this).parents('.thesis').find('.thesis-description').slideUp();
       $(this).hide();
       $(this).parent().find('.thesis-extend').show();
     });
