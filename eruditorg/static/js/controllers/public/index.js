b52f01d7291dedc7fb8b276365385ebfaf80c3d6
Sophy <sophy.ouch@erudit.org>
2016-09-29 11:35:18 -0400

    Smooth scroll for all internal links

--- eruditorg/static/js/controllers/public/index.js
+++ eruditorg/static/js/controllers/public/index.js
@@ -4,6 +4,7 @@ import AdvancedSearchController from './search/AdvancedSearchController';
 import ResultsController from './search/ResultsController';
 import ArticleDetailController from './journal/ArticleDetailController';
 import IssueDetailController from './journal/IssueDetailController';
+import JournalDetailController from './journal/JournalDetailController';
 import JournalListController from './journal/JournalListController';
 import CollectionListController from './thesis/CollectionListController';
 import SavedCitationListController from './citations/SavedCitationListController';
@@ -16,6 +17,7 @@ const controllers = {
   'public:search:results': ResultsController,
   'public:journal:article_detail': ArticleDetailController,
   'public:journal:issue_detail': IssueDetailController,
+  'public:journal:journal_detail': JournalDetailController,
   'public:journal:journal_list': JournalListController,
   'public:thesis:collection_list': CollectionListController,
   'public:citations:list': SavedCitationListController,
---
+++ eruditorg/static/js/controllers/public/journal/JournalDetailController.js
@@ -0,0 +1,24 @@
+export default {
+
+  init: function() {
+    this.smoothScroll();
+  },
+
+  smoothScroll: function() {
+
+    $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+      }, 900, 'swing', function () {
+        window.location.hash = target;
+      });
+
+    });
+
+  },
+
+};
