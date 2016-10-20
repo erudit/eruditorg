30b2714d89178a74e9313fc9130b2b32443b0f55
Sophy <sophy.ouch@erudit.org>
2016-10-20 14:54:24 -0400

    Clean up template tags
    Remove unused tags, rename to shorter names, clarify template blocks, use <main> tag, rename classes

--- eruditorg/static/js/controllers/public/HomeController.js
+++ eruditorg/static/js/controllers/public/HomeController.js
@@ -11,20 +11,20 @@ export default {
   layout : function () {
 
   	var window_height 		= $(window).height(),
-  		sticky_nav_height 	= $('#homepage-content .homepage--sticky-nav').outerHeight(),
+  		sticky_nav_height 	= $('#homepage .homepage--sticky-nav').outerHeight(),
   		header_height 		= window_height / 3 >> 0,
   		search_height 		= window_height - header_height - sticky_nav_height;
 
   	$('#homepage-header').css('height', header_height);
-  	$('#homepage-content .search-module').css('height', search_height);
+  	$('#homepage .search-module').css('height', search_height);
   }/*,
 
   sticky_elements : function () {
-  	$('#homepage-content .homepage--sticky-nav').stick_in_parent();
+  	$('#homepage .homepage--sticky-nav').stick_in_parent();
   },
 
   smooth_scroll : function () {
-  	$('#homepage-content .homepage--sticky-nav').on('click', 'a', function(e) {
+  	$('#homepage .homepage--sticky-nav').on('click', 'a', function(e) {
   		if( e ) {
   			e.preventDefault();
   			e.stopPropagation();
@@ -33,7 +33,7 @@ export default {
   		var target = $(this).attr('href').replace('#', '');
   		if( !target ) return false;
 
-		  $('html, body').animate( { scrollTop: $('#homepage-content a[name="'+target+'"]').offset().top }, 750 );
+		  $('html, body').animate( { scrollTop: $('#homepage a[name="'+target+'"]').offset().top }, 750 );
 		  return false;
   	});
   }*/
