8e9fd08242c7ca81b38eef438155cf45e175a956
Sophy <sophy.ouch@erudit.org>
2016-11-08 21:47:45 -0500

    Smooth scroll all anchor links in journal detail

--- eruditorg/static/js/controllers/public/journal/JournalDetailController.js
+++ eruditorg/static/js/controllers/public/journal/JournalDetailController.js
@@ -1,24 +1,25 @@
 export default {
 
   init: function() {
-    this.smoothScroll();
+    this.article = $('#journal_detail');
+    this.sticky_header_height = 0;
+
+    this.smooth_scroll();
   },
 
-  smoothScroll: function() {
+  smooth_scroll : function () {
+    this.article.on('click', 'a[href*="#"]', (e) => {
+      if( e ) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
 
-    $('a[href^="#"]').on('click',function (e) {
-      e.preventDefault();
-      var target = this.hash;
-      var $target = $(target);
+      var target = $(e.currentTarget).attr('href').replace('#', '');
+      if( !target ) return false;
 
-      $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-      }, 900, 'swing', function () {
-        window.location.hash = target;
-      });
-
+      $('html, body').animate( { scrollTop: this.article.find('#'+target).offset().top - this.sticky_header_height - 30 }, 750 );
+      return false;
     });
-
   },
 
 };
