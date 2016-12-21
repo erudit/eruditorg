10e2dbd416fde75d7169e5b5bfaf73dc4473988f
David Cormier <david.cormier@erudit.org>
2016-12-21 15:18:12 -0500

    remove bibliographic_reference from admin

--- erudit/admin/journal.py
+++ erudit/admin/journal.py
@@ -30,7 +30,6 @@ class JournalAdmin(admin.ModelAdmin):
                 ('collection', 'type',),
                 ('code', 'localidentifier',),
                 ('name', 'subtitle',),
-                ('bibliographic_reference',),
                 ('previous_journal', 'next_journal', ),
                 ('issn_print', 'issn_web', ),
                 ('website_url', ),
