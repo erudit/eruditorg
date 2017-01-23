c11f4fe4913b986235f2b8dd64b5bc9742245ace
Oluwa Lawani <oluwa.lawani@umontreal.ca>
2017-01-23 12:30:31 -0600

    Add actions in admin to pre-publish/publish Issues

--- erudit/admin/journal.py
+++ erudit/admin/journal.py
@@ -2,6 +2,7 @@
 
 from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin
+from django.utils.translation import gettext as _
 
 from ..models import Article
 from ..models import ArticleAbstract
@@ -93,6 +94,17 @@ class IssueAdmin(admin.ModelAdmin):
     list_display = ('journal', 'year', 'volume', 'number', 'title', 'localidentifier', )
     search_fields = ('id', 'localidentifier', )
     list_filter = ('is_published', 'journal__collection', )
+    actions = ['make_published', 'make_unpublished', ]
+
+    def make_published(self, request, queryset):
+        rows_updated = queryset.update(is_published=True)
+        self.message_user(request, _('%s numéro marqué comme diffusé.') % rows_updated)
+    make_published.short_description = _("Marquer les numéros sélectionnés comme diffusés")
+
+    def make_unpublished(self, request, queryset):
+        rows_updated = queryset.update(is_published=False)
+        self.message_user(request, _('%s numéro marqué comme pré-diffusé.') % rows_updated)
+    make_unpublished.short_description = _("Marquer les numéros sélectionnés comme pré-diffusés")
 
 
 class ArticleAbstractAdmin(admin.ModelAdmin):
