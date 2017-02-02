e5205ca9d992883474454e5da06b075f20ed5cec
David Cormier <david.cormier@erudit.org>
2017-02-02 11:49:22 -0500

    oai journals do not have a localidentifier
    only journals imported from fedora have a localidentifier

--- erudit/management/commands/import_journals_from_oai.py
+++ erudit/management/commands/import_journals_from_oai.py
@@ -164,7 +164,6 @@ class Command(BaseCommand):
             journal = Journal()
             journal.code = journal_code
         journal.collection = collection
-        journal.localidentifier = journal_set.setSpec.split(':')[-1]
         journal.name = journal_set.title if hasattr(journal_set, 'title') else journal_set.setName
 
         # Determines the URL of the journal
