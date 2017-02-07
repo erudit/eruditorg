38b92eb496d06e05a07bbc6ef48a34366291e841
David Cormier <david.cormier@erudit.org>
2017-02-07 14:12:03 -0500

    remove mandragore backend

--- tests/unit/core/accounts/test_backends.py
+++ tests/unit/core/accounts/test_backends.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 from django.test import TestCase, override_settings
 
 from core.accounts.backends import EmailBackend
-from core.accounts.backends import MandragoreBackend
 
 
 class TestEmailBackend(TestCase):
@@ -65,58 +64,3 @@ class TestEmailBackend(TestCase):
         # Run & check
         self.assertIsNone(
             backend.authenticate(username='david.cormier@erudit.org', password='test_password'))
-
-
-class MandragoreBackendTestCase(TestCase):
-
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username='test_user',
-            email='david.cormier@erudit.org',
-            password='test_password'
-        )
-
-        self.backend = MandragoreBackend()
-
-    def test_invalid_username(self):
-        user = self.backend.authenticate(
-            username='test_wrong_username', password='test_password'
-        )
-        self.assertIsNone(user)
-
-    def test_no_externaldb_in_settings(self):
-        with self.settings(EXTERNAL_DATABASES=None):
-            user = self.backend.authenticate(
-                username='test_user', password='test_password'
-            )
-
-            self.assertIsNone(user)
-
-    def test_no_mandragore_in_externaldb_settings(self):
-        with self.settings(EXTERNAL_DATABASES={}):
-            user = self.backend.authenticate(
-                username='test_user', password='test_password'
-            )
-            self.assertIsNone(user)
-
-    @override_settings(EXTERNAL_DATABASES={'mandragore': 'dummy'})
-    @mock.patch('core.accounts.backends.get_user_from_mandragore', new=mock.MagicMock(
-        return_value=('test_user', '$6$salt$passwd')))
-    def test_authentication_success(self):
-
-        with mock.patch('crypt.crypt', new=mock.MagicMock(return_value='$6$salt$passwd')):
-            user = self.backend.authenticate(
-                username='test_user', password='test_password'
-            )
-
-            self.assertEqual(user, self.user)
-
-    @override_settings(EXTERNAL_DATABASES={'mandragore': 'dummy'})
-    @mock.patch('core.accounts.backends.get_user_from_mandragore', new=mock.MagicMock(
-        return_value=('test_user', '$6$salt$passwd')))
-    def test_wrong_password(self):
-        user = self.backend.authenticate(
-            username='test_user', password='wrong_password'
-        )
-
-        self.assertIsNone(user)
