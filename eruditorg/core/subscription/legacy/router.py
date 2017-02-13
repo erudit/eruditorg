3dacf8d48a37eddac0a1b8b67efb9b22e0ff4f68
David Cormier <david.cormier@erudit.org>
2017-02-13 10:28:20 -0500

    legacy subscription: models are in abonnement

--- eruditorg/core/subscription/legacy/router.py
+++ eruditorg/core/subscription/legacy/router.py
@@ -1,5 +1,5 @@
 class LegacyRouter(object):
     def db_for_read(self, model, **hints):
         if model._meta.app_label == 'legacy':
-            return 'legacy_subscription'
+            return 'abonnement'
         return None
