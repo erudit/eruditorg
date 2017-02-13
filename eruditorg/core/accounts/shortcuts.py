ef43f2251a89a2dd38a497d3bf5929667b57544b
David Cormier <david.cormier@erudit.org>
2017-02-13 14:12:59 -0500

    accounts: get_or_create_legacy_user may return None
    handle cases where legacy databases have users with different emails
    but the _same_ username

--- eruditorg/core/accounts/shortcuts.py
+++ eruditorg/core/accounts/shortcuts.py
@@ -2,12 +2,17 @@
 
 from django.contrib.auth import get_user_model
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def get_or_create_legacy_user(username, email, hashed_password=None):
     """ Gets or creates a user.
 
     This function is aimed to be used in an import script.
     """
+    user = None
     user_model = get_user_model()
     try:
         user = user_model.objects.get(email=email)
@@ -16,7 +21,11 @@ def get_or_create_legacy_user(username, email, hashed_password=None):
         user.set_unusable_password()
         user.save()
     except user_model.DoesNotExist:
-        user = user_model.objects.create(username=username, email=email)
-        user.password = hashed_password
-        user.save()
+        try:
+            user_model.objects.get(username=username)
+            logger.warn("A user with the username {} already exists. Cannot create.")
+        except user_model.DoesNotExist:
+            user = user_model.objects.create(username=username, email=email)
+            user.password = hashed_password
+            user.save()
     return user
