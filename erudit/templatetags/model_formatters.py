ae20b74edd694146c5641c4084769a59dd4e6e78
David Cormier <david.cormier@erudit.org>
2017-04-07 10:04:42 -0400

    add person_list template filter

---
+++ erudit/templatetags/model_formatters.py
@@ -0,0 +1,18 @@
+from django import template
+from django.utils.translation import ugettext_lazy as _
+register = template.Library()
+
+
+@register.filter(name="person_list")
+def person_list(authors):
+    """ Format a list of persons in a string enumeration"""
+    # Create a copy of the list
+    authors = list(authors)
+    last_author = authors.pop()
+    if len(authors) == 0:
+        return last_author
+    return "{} {} {}".format(
+        ", ".join(authors),
+        _("et"),
+        last_author
+    )
