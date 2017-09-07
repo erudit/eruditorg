10576d69ec43c18f91b043fbeaa4ca17bfee0c07
David Cormier <david.cormier@erudit.org>
2017-09-07 13:37:47 -0400

    tests: move request fixtures to base factories

--- tests/unit/apps/public/journal/test_viewmixins.py
+++ tests/unit/apps/public/journal/test_viewmixins.py
@@ -2,7 +2,6 @@
 
 import pytest
 
-from django.contrib.auth.models import AnonymousUser
 from django.http import Http404
 from django.test import RequestFactory
 from django.views.generic import DetailView
@@ -11,32 +10,15 @@ from erudit.test import BaseEruditTestCase
 from erudit.test.factories import ArticleFactory, EmbargoedArticleFactory, NonEmbargoedArticleFactory, OpenAccessArticleFactory
 from erudit.models import Article
 
-from base.test.factories import UserFactory
 from apps.public.journal.viewmixins import ContentAccessCheckMixin
 from apps.public.journal.viewmixins import SingleArticleMixin
 from apps.public.journal.viewmixins import SingleJournalMixin
-from core.subscription.models import UserSubscriptions
 from core.subscription.test.factories import JournalAccessSubscriptionFactory
 from core.subscription.middleware import SubscriptionMiddleware
+from base.test.factories import get_anonymous_request, get_authenticated_request
 
 middleware = SubscriptionMiddleware()
 
-
-@pytest.fixture
-def anonymous_request():
-    request = RequestFactory().get('/')
-    request.user = AnonymousUser()
-    request.subscriptions = UserSubscriptions()
-    request.session = dict()
-    return request
-
-@pytest.fixture
-def authenticated_request():
-    request = RequestFactory().get('/')
-    request.user = UserFactory()
-    request.subscriptions = UserSubscriptions()
-    request.session = dict()
-    return request
 
 @pytest.fixture()
 def single_article_view():
@@ -94,41 +76,42 @@ class TestContentAccessCheckMixin(object):
         super(TestContentAccessCheckMixin, self).setUp()
         self.factory = RequestFactory()
 
-    def test_do_not_grant_access_by_default(self, anonymous_request, single_article_view):
+    def test_do_not_grant_access_by_default(self, single_article_view):
         # Setup
         article = EmbargoedArticleFactory()
 
         view = single_article_view()
         view.object = article
-        view.request = anonymous_request
+        view.request = get_anonymous_request()
 
         # Run # check
         assert not view.content_access_granted
 
-    def test_can_grant_access_to_an_article_if_it_is_in_open_access(self, anonymous_request, single_article_view):
+    def test_can_grant_access_to_an_article_if_it_is_in_open_access(self, single_article_view):
         # Setup
         article = OpenAccessArticleFactory()
         view = single_article_view
         view.object = article
-        view.request = anonymous_request
+        view.request = get_anonymous_request()
 
         # Run # check
         assert view.content_access_granted
 
-    def test_can_grant_access_to_an_article_if_it_is_not_embargoed(self, anonymous_request, single_article_view):
+    def test_can_grant_access_to_an_article_if_it_is_not_embargoed(self, single_article_view):
         # Setup
         article = NonEmbargoedArticleFactory.create()
 
         view = single_article_view()
         view.object = article
-        view.request = anonymous_request
+        view.request = get_anonymous_request()
 
         # Run # check
         assert view.content_access_granted
 
-    def test_can_grant_access_to_an_article_if_it_is_associated_to_an_individual_subscription(self, authenticated_request, single_article_view):
+    def test_can_grant_access_to_an_article_if_it_is_associated_to_an_individual_subscription(self, single_article_view):
         # Setup
         article = EmbargoedArticleFactory()
+        authenticated_request = get_authenticated_request()
 
         JournalAccessSubscriptionFactory.create(
             user=authenticated_request.user,
@@ -145,10 +128,11 @@ class TestContentAccessCheckMixin(object):
         # Run # check
         assert view.content_access_granted
 
-    def test_cannot_grant_access_to_an_article_if_it_is_associated_to_an_individual_subscription_that_is_not_ongoing(self, authenticated_request, single_article_view):  # noqa
+    def test_cannot_grant_access_to_an_article_if_it_is_associated_to_an_individual_subscription_that_is_not_ongoing(self, single_article_view):  # noqa
         # Setup
 
         article = EmbargoedArticleFactory.create()
+        authenticated_request = get_authenticated_request()
 
         JournalAccessSubscriptionFactory.create(user=authenticated_request.user, journal=article.issue.journal)
 
@@ -159,9 +143,10 @@ class TestContentAccessCheckMixin(object):
         # Run # check
         assert not view.content_access_granted
 
-    def test_can_grant_access_to_an_article_if_it_is_associated_to_an_institutional_account(self, anonymous_request, single_article_view):
+    def test_can_grant_access_to_an_article_if_it_is_associated_to_an_institutional_account(self, single_article_view):
         # Setup
         article = EmbargoedArticleFactory.create()
+        anonymous_request = get_anonymous_request()
 
         JournalAccessSubscriptionFactory(
             journal=article.issue.journal,
@@ -182,9 +167,10 @@ class TestContentAccessCheckMixin(object):
         # Run # check
         assert view.content_access_granted
 
-    def test_cannot_grant_access_to_an_article_if_it_is_associated_to_an_institutional_account_that_is_not_not_ongoing(self, anonymous_request, single_article_view):  # noqa
+    def test_cannot_grant_access_to_an_article_if_it_is_associated_to_an_institutional_account_that_is_not_not_ongoing(self, single_article_view):  # noqa
         # Setup
         article = EmbargoedArticleFactory.create()
+        anonymous_request = get_anonymous_request()
 
         JournalAccessSubscriptionFactory.create(
             journal=article.issue.journal,
@@ -203,11 +189,12 @@ class TestContentAccessCheckMixin(object):
         # Run # check
         assert not view.content_access_granted
 
-    def test_second_subscription_is_activated_when_first_doesnt_give_access_to_article(self, authenticated_request, single_article_view):
+    def test_second_subscription_is_activated_when_first_doesnt_give_access_to_article(self, single_article_view):
         """ Test that the adequate subscription is activated """
 
         # Create an embargoed article
         article = EmbargoedArticleFactory()
+        authenticated_request = get_authenticated_request()
 
         # Create an institutional subscription that gives access to this article
         JournalAccessSubscriptionFactory(
@@ -240,9 +227,10 @@ class TestContentAccessCheckMixin(object):
         assert authenticated_request.subscriptions.active_subscription == individual_subscription
         assert authenticated_request.subscriptions._subscriptions.index(individual_subscription) == 1
 
-    def test_first_subscription_is_activated_when_both_are_valid(self, authenticated_request, single_article_view):
+    def test_first_subscription_is_activated_when_both_are_valid(self, single_article_view):
         # Create an embargoed article
         article = EmbargoedArticleFactory()
+        authenticated_request = get_authenticated_request()
 
         # Create an institutional subscription that gives access to this article
         ip_subscription = JournalAccessSubscriptionFactory(
@@ -271,9 +259,10 @@ class TestContentAccessCheckMixin(object):
         assert authenticated_request.subscriptions.active_subscription == ip_subscription
         assert authenticated_request.subscriptions._subscriptions.index(ip_subscription) == 0
 
-    def test_inserts_a_flag_into_the_context(self, anonymous_request, single_article_view):
+    def test_inserts_a_flag_into_the_context(self, single_article_view):
         # Setup
         article = NonEmbargoedArticleFactory.create()
+        anonymous_request = get_anonymous_request()
 
         view = single_article_view()
         view.object = article
