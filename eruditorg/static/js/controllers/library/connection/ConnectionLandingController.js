12bf3b09bf7c901c0dc9a42e6b3a332622a09437
Sophy <sophy.ouch@erudit.org>
2017-10-18 15:48:35 -0400

    Add and style copy-to-clipboard for credentials

---
+++ eruditorg/static/js/controllers/library/connection/ConnectionLandingController.js
@@ -0,0 +1,36 @@
+import '!!script!clipboard.js/clipboard.min';
+
+export default {
+
+  init: function() {
+
+    this.landing = $('#connection');
+    this.clipboard();
+
+  },
+
+  clipboard : function () {
+    this.landing.find('.clipboard-data').on('click', (e) => {
+      if( e ) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+
+      clipboard.copy( $(e.currentTarget).attr('data-clipboard-text') ).then(
+        () => {
+          $(e.currentTarget).addClass('success');
+          setTimeout(() => { $(e.currentTarget).removeClass('success error'); }, 3000);
+        },
+        () => {
+          $(e.currentTarget).addClass('error');
+          setTimeout(() => { $(e.currentTarget).removeClass('success error'); }, 3000);
+        }
+      );
+
+      return false;
+
+    });
+
+  },
+
+};
