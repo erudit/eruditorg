aed42a95cb50af8a40b993eaaf6e0b4798362cfe
David Cormier <david.cormier@erudit.org>
2017-11-27 17:08:03 -0500

    add kbart form
    ref #601 #1510

---
+++ eruditorg/apps/userspace/library/collection/forms.py
@@ -0,0 +1,39 @@
+import datetime as dt
+from django import forms
+from django.utils.translation import gettext as _
+
+current_year = dt.datetime.now().year
+
+COLLECTION_CHOICES = (
+    ('all', _('Tout')),
+    ('erudit', 'Érudit'),
+    ('unb', 'UNB'),
+)
+
+TYPE_CHOICES = (
+    ('all', _('Tout')),
+    ('academic', _('Savantes')),
+    ('cultural', _('Culturelles')),
+)
+
+ACCESS_CHOICES = (
+    ('all', _('Tout')),
+    ('openaccess', _('Libre accès')),
+    ('restriction', _('Sous commercialisation')),
+    ('endPublication', _('Abandon de publication numérique'))
+)
+
+
+class KBARTForm(forms.Form):
+
+    collection = forms.ChoiceField(
+        label=_("Fonds"), widget=forms.Select(), choices=COLLECTION_CHOICES, required=False
+    )
+
+    typeRevue = forms.ChoiceField(
+        label=_("Type"), widget=forms.Select(), choices=TYPE_CHOICES, required=False
+    )
+
+    access = forms.ChoiceField(
+        label=_("Accès"), widget=forms.Select(), choices=ACCESS_CHOICES, required=False
+    )
--- eruditorg/apps/userspace/library/collection/views.py
+++ eruditorg/apps/userspace/library/collection/views.py
@@ -1,17 +1,57 @@
 # -*- coding: utf-8 -*-
+import requests
+from datetime import datetime
+
+from django.views.generic import FormView
+from django.http.response import HttpResponse
+
+from django.conf import settings
 
 from base.viewmixins import MenuItemMixin
 
 from ...generic_apps.authorization.views import AuthorizationUserView as BaseAuthorizationUserView
 
 from ..viewmixins import OrganisationScopePermissionRequiredMixin
+from .forms import KBARTForm
+
+
+ERUDIT_KBART_BACKEND_URL = getattr(
+    settings,
+    "ERUDIT_KBART_BACKEND_URL",
+    "http://kbart-backend.local"
+)
 
 
 class CollectionView(
-        OrganisationScopePermissionRequiredMixin, MenuItemMixin, BaseAuthorizationUserView):
+        OrganisationScopePermissionRequiredMixin, MenuItemMixin, BaseAuthorizationUserView, FormView):
     menu_library = 'collection'
     permission_required = 'subscription.collection'
     template_name = 'userspace/library/collection/landing.html'
+    form_class = KBARTForm
 
     def get_target_instance(self):
         return self.current_organisation
+
+    def form_valid(self, form):
+
+        report_url = ERUDIT_KBART_BACKEND_URL
+        report = requests.get(report_url, params=form.cleaned_data)
+        filename = "Erudit_Global_{type}_{date}_{collection}_{access}".format(
+            type=form.cleaned_data['typeRevue']
+            if form.cleaned_data['typeRevue'] != 'all' else 'AllTitles',
+            date=datetime.now().strftime("%Y-%m-%d"),
+            collection=form.cleaned_data['collection']
+            if form.cleaned_data['collection'] != 'all' else 'Global',
+            access=form.cleaned_data['access']
+            if form.cleaned_data['access'] != 'all' else 'Global',
+        )
+
+        response = HttpResponse(
+            report, content_type="application/txt"
+        )
+
+        response['Content-Disposition'] = 'attachment; filename="{filename}.txt"'.format(
+            filename=filename,
+        )
+
+        return response
