d0d9a4a389cce28798d56d0ebfb0e7d09ce31b38
David Cormier <david.cormier@erudit.org>
2017-12-21 15:54:25 -0500

    utils: add person_list
    format a list of persons

--- erudit/templatetags/model_formatters.py
+++ erudit/templatetags/model_formatters.py
@@ -1,5 +1,6 @@
 from django import template
-from django.utils.translation import ugettext_lazy as _
+from erudit.models import utils
+
 register = template.Library()
 
 
@@ -39,15 +40,4 @@ def format_editor(editor):
 @register.filter(name="person_list")
 def person_list(authors):
     """ Format a list of persons in a string enumeration"""
-    # Create a copy of the list
-    if not authors:
-        return ""
-    authors = list(authors)
-    last_author = authors.pop()
-    if len(authors) == 0:
-        return last_author
-    return "{} {} {}".format(
-        ", ".join(authors),
-        _("et"),
-        last_author
-    )
+    return utils.person_list(authors)
