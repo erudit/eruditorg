bf1607f58648dce6c7b24ce773d72bf67b532585
Virgil Dupras <hsoft@hardcoded.net>
2018-01-09 14:41:42 -0500

    Improve restrictions import process
    We now properly delete and update subscriptions from upstream. Because
    Victor is our *only* source of subscription information, the simplest
    approach was to delete all and repopulate on each import.
    
    The granularity of the import changed a bit. Instead of iterating mainly
    through subscriptions, we iterate through subscribers, and then through
    subscriptions. This allows us to atomically delete subscriptions per
    subscriber. Because we wrap all of this in a transaction, at no moment
    during the import do we end up with an empty subscription list that
    could disturb production operations.
    
    fixes #1459

--- eruditorg/core/subscription/management/commands/import_restrictions.py
+++ eruditorg/core/subscription/management/commands/import_restrictions.py
@@ -69,40 +69,38 @@ class Command(BaseCommand):
                 abonneid=self.organisation_id
             )
 
+        restriction_subscriber_ids = restriction_subscriptions.order_by('abonneid')\
+            .values_list('abonneid', flat=True)\
+            .distinct()
+
         logger.info(
             "import.started",
             import_type="restrictions",
-            to_process=restriction_subscriptions.count()
+            to_process=len(restriction_subscriber_ids)
         )
 
-        for restriction_subscription in restriction_subscriptions:
+        for subscriber_id in restriction_subscriber_ids:
+            # Fetches the subscriber
             try:
-                self._import_restriction_subscription(restriction_subscription)
+                subscriber = Abonne.objects.get(pk=subscriber_id)
+            except Abonne.DoesNotExist:
+                logger.error('Abonne.DoesNotExist', abonne_id=subscriber_id)
+                raise ImportException
+            subscription_qs = restriction_subscriptions.filter(abonneid=subscriber_id)
+            try:
+                self._import_restriction_subscriber(subscriber, subscription_qs)
             except ImportException:
                 pass
         logger.info("import.finished", **created_objects)
 
     @transaction.atomic
-    def _import_restriction_subscription(self, restriction_subscription):
+    def _import_restriction_subscriber(self, restriction_subscriber, subscription_qs):
         logger = structlog.get_logger(__name__)
         logger = logger.bind(
-            subscription_id=restriction_subscription.id
+            subscriber_id=restriction_subscriber.pk
         )
-        # Fetches the subscriber
-        try:
-            restriction_subscriber = Abonne.objects.get(pk=restriction_subscription.abonneid)
-        except Abonne.DoesNotExist:
-            logger.error('Abonne.DoesNotExist', abonne_id=restriction_subscription.abonneid)
-            raise ImportException
 
-        # Fetch the related journal
-        try:
-            restriction_journal = Revue.objects.get(revueid=restriction_subscription.revueid)
-        except Revue.DoesNotExist:
-            logger.error('Revue.DoesNotExist', revue_id=restriction_subscription.revueid)
-            raise ImportException
-
-        # STEP 1: gets or creates the RestrictionProfile instance
+        # gets or creates the RestrictionProfile instance
         # --
 
         try:
@@ -130,10 +128,18 @@ class Command(BaseCommand):
             if created:
                 logger.info("organisation.created", pk=organisation.pk, name=organisation.name)
 
-            restriction_profile = LegacyAccountProfile.objects.create(
-                origin=LegacyAccountProfile.DB_RESTRICTION,
-                legacy_id=str(restriction_subscriber.pk),
-                user=user, organisation=organisation)
+            # Sometime, we get subscriber rows with duplicate names! It seems that most of the
+            # time, it refers to the same entity, so it's not a big deal, but we want to make sure
+            # we're not going to cause an integrity error before creating the profile.
+            try:
+                restriction_profile = LegacyAccountProfile.objects \
+                    .filter(origin=LegacyAccountProfile.DB_RESTRICTION) \
+                    .get(organisation=organisation)
+            except LegacyAccountProfile.DoesNotExist:
+                restriction_profile = LegacyAccountProfile.objects.create(
+                    origin=LegacyAccountProfile.DB_RESTRICTION,
+                    legacy_id=str(restriction_subscriber.pk),
+                    user=user, organisation=organisation)
 
             if restriction_subscriber.icone:
                 f = open(
@@ -159,7 +165,38 @@ class Command(BaseCommand):
                 sushi_requester_id=restriction_subscriber.requesterid
             )
 
-        # STEP 2: gets or creates a JournalAccessSubscription instance
+        # Delete all subscriptions for this subscriber!
+        #
+        # Why can we do this? Because this import script is the *only* source of subscription
+        # information. Because of this, we can happily go ahead with a "delete and repopulate"
+        # approach. If we don't delete our stuff, subscription deletions in Victor won't properly
+        # be imported: subscription will stay here forever.
+
+        JournalAccessSubscription.objects\
+            .filter(organisation=restriction_profile.organisation)\
+            .delete()
+
+        for subscription in subscription_qs.all():
+            self._import_restriction_subscription(
+                subscription, restriction_subscriber, restriction_profile)
+
+    def _import_restriction_subscription(
+            self, restriction_subscription, restriction_subscriber, restriction_profile):
+        logger = structlog.get_logger(__name__)
+        logger = logger.bind(
+            subscriber_id=restriction_subscriber.pk,
+            subscription_id=restriction_subscription.pk
+        )
+
+        #
+        # Fetch the related journal
+        try:
+            restriction_journal = Revue.objects.get(revueid=restriction_subscription.revueid)
+        except Revue.DoesNotExist:
+            logger.error('Revue.DoesNotExist', revue_id=restriction_subscription.revueid)
+            raise ImportException
+
+        # gets or creates a JournalAccessSubscription instance
         # --
 
         try:
@@ -179,7 +216,7 @@ class Command(BaseCommand):
             subscription.journals.add(journal)
             logger.info("subscription.add_journal", journal_pk=journal.pk)
 
-        # STEP 3: creates the subscription period
+        # creates the subscription period
         # --
 
         start_date = dt.date(restriction_subscription.anneeabonnement, 2, 1)
@@ -209,7 +246,7 @@ class Command(BaseCommand):
         else:
             subscription_period.save()
 
-        # STEP 5: create the subscription referer
+        # create the subscription referer
         # --
 
         if restriction_subscriber.referer:
@@ -221,7 +258,7 @@ class Command(BaseCommand):
             if created:
                 logger.info("referer.created", referer=restriction_subscriber.referer)
 
-        # STEP 4: creates the IP whitelist associated with the subscription
+        # creates the IP whitelist associated with the subscription
         # --
 
         restriction_subscriber_ips_set1 = Ipabonne.objects.filter(
