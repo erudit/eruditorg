d306ab912a02659bc9afda344201fc396aca89aa
Virgil Dupras <hsoft@hardcoded.net>
2018-01-24 16:35:34 -0500

    issue detail page: call get_authors(html=True) in liberuditarticle
    Some author display values have markup in them.
    
    ref #1326
    
    Requires liberuditarticle!32

--- eruditorg/apps/public/journal/templateannotations.py
+++ eruditorg/apps/public/journal/templateannotations.py
@@ -1,9 +1,12 @@
+import structlog
+
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 # Injects a `extra` attribute to target models to supply functions
 # for use in templates. Cleaner than templatetags and much cleaner
 # than putting this in models.
+logger = structlog.get_logger(__name__)
 
 
 class IssueAnnotator:
@@ -37,3 +40,29 @@ class IssueAnnotator:
         if self.issue.external_url:
             return False  # external issues are never locked
         return self.issue.embargoed and not self.view.content_access_granted
+
+
+class ArticleAnnotator:
+    @classmethod
+    def annotate_articles(cls, articles):
+        for article in articles:
+            article.helper = cls(article)
+
+    def __init__(self, article):
+        self.article = article
+
+    def get_formatted_authors(self):
+        if self.article.erudit_object:
+            return mark_safe(self.article.erudit_object.get_authors(formatted=True, html=True))
+        else:
+            # we're pretty sure that the code below is useless but we don't dare replace this
+            # with assert article.erudit_object just yet. This is why we have this log below
+            # so that if this branch ever executes, we'll know about it in Sentry.
+            logger.error("Calling get_formatted_authors() with a non-erudit article.")
+            authors = [str(author) for author in self.article.authors.all()]
+            if len(authors) > 1:
+                return ', '.join(authors[:-1]) + ' et ' + authors[-1]
+            elif authors:
+                return authors[0]
+            else:
+                return ''
