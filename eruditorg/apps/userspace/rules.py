3f9c8340bde979e0141b1c0ce5e4d9aa19192ac3
David Cormier <david.cormier@erudit.org>
2018-02-14 10:59:29 -0500

    journal rules: add can_manage_a_journal predicate
    The predicate returns True if a user can manage at least one journal
    in her dashboard. Use this predicate in the journal rules instead of
    verifying every authorization.
    
    ref #1666

--- eruditorg/apps/userspace/rules.py
+++ eruditorg/apps/userspace/rules.py
@@ -4,26 +4,15 @@ import rules
 from rules.predicates import is_authenticated
 from rules.predicates import is_staff
 from rules.predicates import is_superuser
-
-from core.authorization.defaults import AuthorizationConfig as AC
-from core.authorization.predicates import HasAnyAuthorization
-from core.journal.predicates import is_journal_member
-
+from core.journal.predicates import can_manage_a_journal
 
 # This permission assume to use a 'Journal' object to perform the perm check
 rules.add_perm(
     'userspace.access',
     is_authenticated & (
-        is_superuser | is_staff |
-        (
-            is_journal_member &
-            HasAnyAuthorization([
-                AC.can_edit_journal_information,
-                AC.can_manage_authorizations,
-                AC.can_manage_issuesubmission,
-                AC.can_manage_individual_subscription,
-            ])
-        )
+        is_superuser |
+        is_staff |
+        can_manage_a_journal |
     ),
 )
 
