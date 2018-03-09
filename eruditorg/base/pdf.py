85fe37c653a6fb55878af2dde7be1e062d5a7579
David Cormier <david.cormier@erudit.org>
2018-03-09 16:27:05 -0500

    pdf: do not fetch static
    Avoid requesting the static files to the web server. Use the local
    versions
    
    ref #1680

--- eruditorg/base/pdf.py
+++ eruditorg/base/pdf.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
+import os
 import tempfile
 import subprocess
 import io
+import mimetypes
 
 from django.template.loader import get_template
 from weasyprint import HTML
+from weasyprint.urls import default_url_fetcher
+from django.conf import settings
+
+# Make sure mimetypes knows of all the extensions used in the pdf
+mimetypes.add_type("application/vnd.ms-fontobject", ".eot")
+mimetypes.add_type("font/woff", ".woff")
+mimetypes.add_type("font/woff2", ".woff2")
+mimetypes.add_type("font/ttf", ".ttf")
+mimetypes.add_type("image/svg+xml", ".svg")
 
 
 def add_coverpage_to_pdf(coverpage, content):
@@ -49,6 +60,32 @@ def get_pdf_first_page(content):
             return f2.read()
 
 
+def local_url_fetcher(url):
+    """ An URL fetcher that supports reading local files from the disk
+
+    If the file has the "local:" prefix, read it from the disk.
+    Otherwise call the default url fetcher.
+
+    Just like the ``default_url_fetcher``, it is the caller's responsibility
+    to close the ``file_obj`` returned.
+    """
+
+    if url.lower().startswith('local:'):
+        path = os.path.normpath(settings.STATIC_ROOT + url.lstrip('local:'))
+
+        if not path.startswith(settings.STATIC_ROOT):
+            raise ValueError("Path should be a subpath of settings.STATIC_ROOT")
+        file_obj = open(path, "rb")
+        asset = dict(
+            file_obj=file_obj,
+            mime_type=mimetypes.guess_type(path)[0],
+        )
+
+        return asset
+
+    return default_url_fetcher(url)
+
+
 def generate_pdf(template_name, file_object=None, context=None, base_url=None):
     """
     Generates a PDF into the given file object from a specific Django template.
@@ -60,7 +97,7 @@ def generate_pdf(template_name, file_object=None, context=None, base_url=None):
         file_object = io.BytesIO()
 
     try:
-        HTML(string=html, base_url=base_url).write_pdf(file_object)
+        HTML(string=html, base_url=base_url, url_fetcher=local_url_fetcher).write_pdf(file_object)
     except Exception:
         # the 0xAD character is a soft hyphen. If we don't strip it, we *sometimes* get a decoding
         # error at the weasyprint/Cairo level. It's not an encoding problem per se: the 0xAD
@@ -70,6 +107,6 @@ def generate_pdf(template_name, file_object=None, context=None, base_url=None):
         # on whole document rendering so far and we went with this temporary bandaid.
         # ref eruditorg#1640
         html = html.replace('\xad', '')
-        HTML(string=html, base_url=base_url).write_pdf(file_object)
+        HTML(string=html, base_url=base_url, url_fetcher=local_url_fetcher).write_pdf(file_object)
 
     return file_object
