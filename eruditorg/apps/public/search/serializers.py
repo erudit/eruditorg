e4582fb0f2993780d739ad1edb31c7c578e465ac
Virgil Dupras <hsoft@hardcoded.net>
2018-03-16 11:01:27 -0400

    search: un-DRF serializers

--- eruditorg/apps/public/search/serializers.py
+++ eruditorg/apps/public/search/serializers.py
@@ -1,85 +1,58 @@
 from django.utils.translation import ugettext as _
-from rest_framework import serializers
 
 from erudit.templatetags.model_formatters import person_list
 
 
-class GenericSolrDocumentSerializer(serializers.Serializer):
-    localidentifier = serializers.SerializerMethodField()
-    document_type = serializers.SerializerMethodField()
-    numero = serializers.SerializerMethodField()
-    authors = serializers.SerializerMethodField()
-    pages = serializers.SerializerMethodField()
-    title = serializers.SerializerMethodField()
-    volume = serializers.SerializerMethodField()
-    url = serializers.SerializerMethodField()
-    collection = serializers.SerializerMethodField()
-    issn = serializers.SerializerMethodField()
-    publication_date = serializers.SerializerMethodField()
-    year = serializers.SerializerMethodField()
-    series = serializers.SerializerMethodField()
-
-    def get_localidentifier(self, obj):
-        return obj['ID']
-
-    def get_document_type(self, obj):
-        corpus = obj['Corpus_fac']
-        return {
-            'Article': 'article',
-            'Culturel': 'article',
-            'Thèses': 'thesis',
-            'Dépot': 'report',
-            'Livres': 'book',
-            'Actes': 'book',
-            'Rapport': 'report',
-        }.get(corpus, 'generic')
-
-    def get_year(self, obj):
-        if 'Annee' in obj:
-            return obj.get('Annee')[0]
-
-    def get_publication_date(self, obj):
-        return obj.get('AnneePublication')
-
-    def get_issn(self, obj):
-        return obj.get('ISSN')
-
-    def get_collection(self, obj):
-        return obj.get('Fonds_fac')
-
-    def get_authors(self, obj):
-
-        return person_list(obj.get('AuteurNP_fac'))
-
-    def get_volume(self, obj):
-        return obj.get('Volume')
-
-    def get_series(self, obj):
-        collection_title = obj.get('TitreCollection_fac')
-        if collection_title:
-            return collection_title[0]
-        return None
-
-    def get_url(self, obj):
-        if 'URLDocument' in obj:
-            return obj['URLDocument'][0]
-        else:
-            return None
-
-    def get_numero(self, obj):
-        return obj.get('Numero')
-
-    def get_pages(self, obj):
-        if not {'PremierePage', 'DernierePage'}.issubset(set(obj.keys())):
-            return None
-        return _('Pages {firstpage}-{lastpage}'.format(
-            firstpage=obj['PremierePage'],
-            lastpage=obj['DernierePage']
+def serialize_solr_result(result):
+    corpus = result['Corpus_fac']
+    document_type = {
+        'Article': 'article',
+        'Culturel': 'article',
+        'Thèses': 'thesis',
+        'Dépot': 'report',
+        'Livres': 'book',
+        'Actes': 'book',
+        'Rapport': 'report',
+    }.get(corpus, 'generic')
+    collection_title = result.get('TitreCollection_fac')
+    if collection_title:
+        series = collection_title[0]
+    else:
+        series = None
+    if not {'PremierePage', 'DernierePage'}.issubset(set(result.keys())):
+        pages = None
+    else:
+        pages = _('Pages {firstpage}-{lastpage}'.format(
+            firstpage=result['PremierePage'],
+            lastpage=result['DernierePage']
         ))
+    TITLE_ATTRS = ['Titre_fr', 'Titre_en', 'TitreRefBiblio_aff']
+    title = _("(Sans titre)")
+    for attrname in TITLE_ATTRS:
+        if attrname in result:
+            title = result[attrname]
+            break
+    return {
+        'localidentifier': result['ID'],
+        'document_type': document_type,
+        'year': result['Annee'][0] if 'Annee' in result else None,
+        'publication_date': result.get('AnneePublication'),
+        'issn': result.get('ISSN'),
+        'collection': result.get('Fonds_fac'),
+        'authors': person_list(result.get('AuteurNP_fac')),
+        'volume': result.get('Volume'),
+        'series': series,
+        'url': result['URLDocument'][0] if 'URLDocument' in result else None,
+        'numero': result.get('Numero'),
+        'pages': pages,
+        'title': title,
+    }
 
-    def get_title(self, obj):
-        TITLE_ATTRS = ['Titre_fr', 'Titre_en', 'TitreRefBiblio_aff']
-        for attrname in TITLE_ATTRS:
-            if attrname in obj:
-                return obj[attrname]
-        return _("(Sans titre)")
+
+class GenericSolrDocumentSerializer:
+    def __init__(self, objs, **kwargs):
+        self.objs = objs
+
+    @property
+    def data(self):
+        return map(serialize_solr_result, self.objs)
