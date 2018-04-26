1596adc015a3cbd01802650a8853e3431e9e35fd
Virgil Dupras <hsoft@hardcoded.net>
2018-04-26 15:33:32 -0400

    Fix broken tests

--- eruditorg/apps/public/journal/views.py
+++ eruditorg/apps/public/journal/views.py
@@ -293,7 +293,10 @@ class JournalAuthorsListView(SingleJournalMixin, TemplateView):
         context['letters_exists'] = self.letters_exists
         context['latest_issue'] = self.journal.last_issue
         context['meta_info_issue'] = context['latest_issue']
-        context['journal_info'] = self.journal.information
+        try:
+            context['journal_info'] = self.journal.information
+        except ObjectDoesNotExist:
+            pass
         return context
 
 
@@ -355,7 +358,10 @@ class IssueDetailView(
         context = super(IssueDetailView, self).get_context_data(**kwargs)
 
         context['journal'] = self.object.journal
-        context['journal_info'] = self.object.journal.information
+        try:
+            context['journal_info'] = self.object.journal.information
+        except ObjectDoesNotExist:
+            pass
 
         context['meta_info_issue'] = self.object
         guest_editors = self.object.erudit_object.get_redacteurchef(
@@ -548,7 +554,10 @@ class BaseArticleDetailView(
         shouldcache = obj.issue.is_published and obj.id
         context['cache_timeout'] = (7 * 24 * 60 * 60) if shouldcache else 0
 
-        context['journal_info'] = obj.issue.journal.information
+        try:
+            context['journal_info'] = obj.issue.journal.information
+        except ObjectDoesNotExist:
+            pass
 
         return context
 
