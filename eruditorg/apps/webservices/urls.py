96c075bd90259483d12569e361e55cf3f9d60e62
Virgil Dupras <hsoft@hardcoded.net>
2018-05-16 11:37:52 -0400

    Add new "restrictions-retro" webservice
    ref #1791

--- eruditorg/apps/webservices/urls.py
+++ eruditorg/apps/webservices/urls.py
@@ -1,9 +1,10 @@
-# -*- coding: utf-8 -*-
-
 from django.conf.urls import include
 from django.conf.urls import url
+
+from .views.restrictions import RetroRestrictionsView
 
 
 urlpatterns = [
     url(r'^sushi/', include('apps.webservices.sushi.urls', namespace='sushi')),
+    url(r'^restrictions-retro/$', RetroRestrictionsView.as_view(), name='restrictions_retro'),
 ]
---
+++ eruditorg/apps/webservices/views/restrictions.py
@@ -0,0 +1,30 @@
+import datetime as dt
+
+from lxml import etree
+from lxml.builder import E
+
+from django.http import HttpResponse
+from django.views.generic import View
+
+from erudit.models import Journal
+
+
+class RetroRestrictionsView(View):
+    http_method_names = ['get', ]
+
+    def get(self, request):
+        def get_revue_elem(journal):
+            min_year = dt.date.today().year + 1
+            for issue in journal.published_issues:
+                if issue.embargoed:
+                    min_year = min(min_year, issue.date_published.year)
+            embargoed_years = range(min_year, dt.date.today().year + 1)
+            return E.revue(
+                E.years(';'.join(map(str, embargoed_years))),
+                identifier=journal.legacy_code)
+
+        journals = Journal.objects.filter(collection__code='erudit', open_access=False)
+        root = E.revues(
+            *map(get_revue_elem, journals.all())
+        )
+        return HttpResponse(etree.tostring(root), content_type='text/xml')
