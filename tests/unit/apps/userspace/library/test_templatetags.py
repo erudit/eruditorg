68bf98e169531c5f13d2625c908f6e678f490690
Virgil Dupras <hsoft@hardcoded.net>
2018-05-17 09:27:05 -0400

    Decouple tests/unit/apps/userspace/* from BaseEruditTestCase

--- tests/unit/apps/userspace/library/test_templatetags.py
+++ tests/unit/apps/userspace/library/test_templatetags.py
@@ -1,28 +1,23 @@
-# -*- coding: utf-8 -*-
-
+import pytest
 from django.core.urlresolvers import resolve
 from django.core.urlresolvers import reverse
 from django.test import RequestFactory
 
-from erudit.test import BaseEruditTestCase
 from erudit.test.factories import OrganisationFactory
 
 from apps.userspace.library.templatetags.userspace_library_tags import library_url
 
+pytestmark = pytest.mark.django_db
 
-class TestLibraryUrlSimpleTag(BaseEruditTestCase):
-    def test_can_resolve_the_current_url_for_another_organisation(self):
-        # Setup
-        org_1 = OrganisationFactory.create()
-        org_2 = OrganisationFactory.create()
-        factory = RequestFactory()
-        base_url = reverse(
-            'userspace:library:home', kwargs={'organisation_pk': org_1.pk})
-        request = factory.get(base_url)
-        request.resolver_match = resolve(base_url)
-        # Run
-        url = library_url({'request': request}, org_2)
-        # Check
-        self.assertEqual(
-            url,
-            reverse('userspace:library:home', kwargs={'organisation_pk': org_2.pk}))
+
+def test_can_resolve_the_current_url_for_another_organisation():
+    org_1 = OrganisationFactory.create()
+    org_2 = OrganisationFactory.create()
+    factory = RequestFactory()
+    base_url = reverse(
+        'userspace:library:home', kwargs={'organisation_pk': org_1.pk})
+    request = factory.get(base_url)
+    request.resolver_match = resolve(base_url)
+    url = library_url({'request': request}, org_2)
+    EXPECTED = reverse('userspace:library:home', kwargs={'organisation_pk': org_2.pk})
+    assert url == EXPECTED
