8ffb6ff5a04f7bf6bfc7ab2ac3cc3fe488724745
David Cormier <david.cormier@erudit.org>
2018-05-18 16:30:18 -0400

    call clean & save on article, not Command

--- eruditorg/erudit/management/commands/import_journals_from_fedora.py
+++ eruditorg/erudit/management/commands/import_journals_from_fedora.py
@@ -634,8 +634,8 @@ class Command(BaseCommand):
         """ Imports an article using the EruditArticle v3 specification. """
         article.publication_allowed = self._get_is_publication_allowed(issue_article_node)
         article.sync_with_erudit_object(article_erudit_object)
-        self.clean()
-        self.save()
+        article.clean()
+        article.save()
 
         if self.test_xslt:
             try:
