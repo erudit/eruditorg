b3105b9b7f144534f74efb2e5c5d5afea4536b50
David Cormier <david.cormier@erudit.org>
2018-07-24 17:31:12 -0400

    move BookAdmin to admin

--- eruditorg/apps/public/book/admin.py
+++ eruditorg/apps/public/book/admin.py
@@ -1,5 +1,50 @@
 from django.contrib import admin
-from .models import Book, BookCollection, BookAdmin
+from .models import Book, BookCollection
+
+
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {
+            'fields': (
+                ('type', 'collection',),
+            )
+        }),
+        (None, {
+            'fields': (
+                ('title', 'subtitle',),
+            )
+        }),
+        (None, {
+            'fields': (
+                ('year',),
+            )
+        }),
+        (None, {
+            'fields': (
+                ('publisher', 'publisher_url',),
+            )
+        }),
+        (None, {
+            'fields': (
+                ('authors', 'contribution',),
+            )
+        }),
+        (None, {
+            'fields': (
+                ('isbn', 'digital_isbn',),
+            )
+        }),
+        (None, {
+            'fields': (
+                ('cover',),
+            )
+        }),
+        (None, {
+            'fields': (
+                ('copyright',),
+            )
+        }),
+    ]
 
 admin.site.register(BookCollection)
 admin.site.register(Book, BookAdmin)
