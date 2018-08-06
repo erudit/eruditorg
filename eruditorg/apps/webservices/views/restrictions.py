85dda983c5c9f5aad76ae2682b672b8091c95f5b
aziz <aziz.bouhouch@erudit.org>
2018-08-06 11:19:04 -0400

    embargo: Retourner la liste des numéros par revue avec toutes les informations de l embargo.
    ref erudit/rapports/kbart#34

--- eruditorg/apps/webservices/views/restrictions.py
+++ eruditorg/apps/webservices/views/restrictions.py
@@ -4,6 +4,7 @@ from lxml import etree
 from lxml.builder import E
 
 from django.http import HttpResponse
+from django.db.models import Q
 from django.views.generic import View
 
 from erudit.models import Journal
@@ -13,7 +14,8 @@ class RestrictionsView(View):
     http_method_names = ['get', ]
 
     def get(self, request):
-        def get_revue_elem(journal):
+
+        def get_journal_elem(journal):
             date_embargo_begins = journal.date_embargo_begins
             if not date_embargo_begins:
                 return E.journal()
@@ -41,6 +43,65 @@ class RestrictionsView(View):
         journals = Journal.objects.filter(
             collection__is_main_collection=True, open_access=False, active=True)
         root = E.journals(
-            *map(get_revue_elem, journals.all())
+            *map(get_journal_elem, journals.all())
         )
+        return HttpResponse(etree.tostring(root), content_type='text/xml')
+
+
+class RestrictionsByJournalView(View):
+    http_method_names = ['get', ]
+
+    def get(self, request, journal_code=None):
+
+        def get_journal_elem(journal):
+
+            embargoed_issues = []
+            whitelisted_issues = []
+            for issue in journal.published_issues:
+                if issue.embargoed:
+                    embargoed_issues.append(issue.localidentifier)
+                if issue.force_free_access:
+                    whitelisted_issues.append(issue.localidentifier)
+            issues_elements = E.issues(
+                *[E.issue(
+                    E.number(str(x.number)),
+                    E.volume(str(x.volume)),
+                    E.year(str(x.year)),
+                    localidentifier=x.localidentifier,
+                    embargoed=str(x.embargoed),
+                    whitelisted=str(issue.force_free_access)
+                ) for x in journal.published_issues],
+                count=str(len(journal.published_issues)),
+                embargoed_count=str(len(embargoed_issues)),
+                whitelisted_count=str(len(whitelisted_issues))
+            )
+
+            if journal.open_access:
+                journal_element = E.journal(
+                    issues_elements,
+                    code=journal.code,
+                    localidentifier=journal.localidentifier,
+                    embargoed=str(not journal.open_access)
+                )
+            else:
+                journal_element = E.journal(
+                    E.embargo_date(journal.date_embargo_begins.strftime('%Y-%m-%d')),
+                    E.embargo_duration(
+                        str(journal.embargo_in_months),
+                        unit='month'
+                    ),
+                    issues_elements,
+                    code=journal.code,
+                    localidentifier=journal.localidentifier,
+                    embargoed=str(not journal.open_access)
+                )
+
+            return journal_element
+
+        journal = Journal.objects.filter(
+            Q(code=journal_code) | Q(localidentifier=journal_code),
+            collection__is_main_collection=True,
+            active=True
+        )
+        root = get_journal_elem(*journal)
         return HttpResponse(etree.tostring(root), content_type='text/xml')
--- tests/functional/apps/webservices/test_views.py
+++ tests/functional/apps/webservices/test_views.py
@@ -4,8 +4,8 @@ import pytest
 from lxml import etree
 from django.core.urlresolvers import reverse
 from django.test import Client
-
-from erudit.test.factories import IssueFactory
+from erudit.test.factories import IssueFactory, EmbargoedIssueFactory
+from django.test import RequestFactory
 
 pytestmark = pytest.mark.django_db
 
@@ -33,3 +33,40 @@ class TestRestrictionsView:
         elem = root[0].find('embargoed_issues')
         assert len(elem[:]) == 1
         assert elem[0].attrib['localidentifier'] == issue.localidentifier
+
+
+class TestRestrictionsByJournalView:
+
+    def test_that_journal_and_all_issues_works(self):
+        issue = EmbargoedIssueFactory(journal__collection__code='erudit')
+        url = reverse('webservices:restrictionsByJournal',  kwargs={'journal_code':issue.journal.localidentifier})
+        response = Client().get(url)
+        root = etree.fromstring(response.content.decode())
+
+        # Check the attributes of the journal
+        assert root.tag == 'journal'
+        assert root.attrib['code'] == issue.journal.code
+        assert root.attrib['localidentifier'] == issue.journal.localidentifier
+        assert root.attrib['embargoed'] == 'True'
+
+        # Check the presence and the values of other elements
+        for child in root:
+            assert child.tag in ('embargo_date', 'embargo_duration', 'issues')
+
+        embargo_date = issue.journal.date_embargo_begins.strftime('%Y-%m-%d')
+        embargo_duration = str(issue.journal.embargo_in_months)
+        assert root.find('embargo_date').text == embargo_date
+        assert root.find('embargo_duration').text == embargo_duration
+
+        # Check the attributes of the issues
+        issues_element = root.find('issues')
+        assert issues_element.attrib['count'] == str(len(issue.journal.published_issues))
+        assert issues_element.attrib['embargoed_count'] == str(len(issue.journal.published_issues))
+        whitelisted_issues=issue.journal.published_issues.filter(force_free_access=True)
+        assert issues_element.attrib['whitelisted_count'] == str(len(whitelisted_issues))
+
+        # Check the attributes of the last issue (index = 0)
+        issue_element = issues_element[0]
+        assert issue_element.attrib['embargoed'] == str(issue.embargoed)
+        assert issue_element.attrib['whitelisted'] == str(issue.force_free_access)
+        assert issue_element.attrib['localidentifier'] == issue.localidentifier
