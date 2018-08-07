cd17f1964ac8d9d8178b005eadd2d0a0a5fae669
Bérenger Enselme <benselme@gmail.com>
2018-08-07 15:52:03 -0400

    Linting + requirements for book imports

--- eruditorg/apps/public/book/management/commands/import_books.py
+++ eruditorg/apps/public/book/management/commands/import_books.py
@@ -1,14 +1,15 @@
 import re
 import os
-from lxml import html, etree
+from lxml import html
+# noinspection PyProtectedMember
 from bs4 import UnicodeDammit
 
 from django.core.files.storage import default_storage
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from apps.public.book.models import BookCollection, Book
 
 
-to_import = ('ACFASSudbury','aidelf', 'artefact', 'sqrsf', 'artsVisuels', 'CEFAN', 'npqs',)
+to_import = ('ACFASSudbury', 'aidelf', 'artefact', 'sqrsf', 'artsVisuels', 'CEFAN', 'npqs',)
 
 # TODO fix book import
 
@@ -126,8 +127,8 @@ class Command(BaseCommand):
             editeur = root.find('.//h1[@class="editeur"]')
             if editeur is not None:
                 book.publisher = _get_text(editeur)
-            book.type = 'li'
-            copyright = root.find('.//p[@class="droits"]')
+            book.type = 'li' if book_notice is not None else 'ac'
+            copyright_ = root.find('.//p[@class="droits"]')
             cover = root.find('.//div[@class="couverture"]/img')
             if cover is not None:
                 src = cover.get('src')
@@ -139,23 +140,19 @@ class Command(BaseCommand):
                     bla = default_storage.save('book_cover', open(cover_path, 'rb'))
                     book.cover = bla
                     print("saved the cover: {}".format(cover_path))
-                except:
+                except FileNotFoundError:
                     print("didnt save the cover: {}".format(cover_path))
-            if copyright is not None:
-                book.copyright = _get_text(copyright)
-            try:
-                book.save()
-            except:
-                import ipdb; ipdb.set_trace()
-        pass
+            if copyright_ is not None:
+                book.copyright = _get_text(copyright_)
+            book.save()
 
     def handle(self, *args, **options):
         BookCollection.objects.all().delete()
         Book.objects.all().delete()
 
+        # noinspection PyAttributeOutsideInit
         self.directory = options.get('livre_directory')[0]
-        self.default_collection = BookCollection(name="[Hors collection]")
-        self.default_collection.save()
+        default_collection = BookCollection.objects.create(name="[Hors collection]")
 
         with open('{directory}/livre/index.xml'.format(directory=self.directory), 'rb') as index:
             root = get_unicode_root(index)
@@ -166,16 +163,16 @@ class Command(BaseCommand):
                 if book_id_match:
                     collection = book_id_match.group(1)
                     # collections have an index.xml file in their directory
-                    path = "{directory}/livre/{collection}/index.xml".format(directory=self.directory, collection=collection)
+                    path = "{directory}/livre/{collection}/index.xml".format(
+                        directory=self.directory, collection=collection)
                     if os.path.exists(path) and collection in to_import:
                         self.import_collection(collection, notice, path)
                     else:
-                        book_path = notice.find('.//div[@class="format"]/a').get('href').replace('html', 'xml').replace(
-                            'htm', 'xml').lstrip(" ")
+                        book_path = notice.find('.//div[@class="format"]/a').get('href')
+                        book_path = book_path.replace('html', 'xml').replace('htm', 'xml')
                         book_fs_path = "{directory}/{path}".format(directory=self.directory,
-                                                                   path=book_path)
-                        self.import_book(self.default_collection, book_fs_path, notice)
+                                                                   path=book_path.lstrip(" "))
+                        self.import_book(default_collection, book_fs_path, notice)
                 else:
                     print(notice)
                     continue
-                    self.import_book(self.default_collection, notice)
