8b353bd95113f4064f04948868ca2f723c111752
David Cormier <david.cormier@erudit.org>
2018-09-28 14:44:57 -0400

    Remove SessionAuthenticationMiddleware
    It has been removed from Django 2.0
    
    Ref:
    https://docs.djangoproject.com/en/2.0/releases/2.0/#backwards-incompatible-2-0

--- eruditorg/base/settings/base.py
+++ eruditorg/base/settings/base.py
@@ -120,7 +120,6 @@ MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.sites.middleware.CurrentSiteMiddleware',
