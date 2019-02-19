e8d60901df5dd3891a3fdd87eb3eaf36a6527655
David Cormier <david.cormier@erudit.org>
2019-02-19 11:42:09 -0500

    Parametrize test_permissions.TestViews

--- tests/functional/apps/userspace/journal/editor/test_permissions.py
+++ tests/functional/apps/userspace/journal/editor/test_permissions.py
@@ -12,91 +12,42 @@ from core.editor.test.factories import IssueSubmissionFactory
 pytestmark = pytest.mark.django_db
 
 
+@pytest.mark.parametrize('permissions,code', (
+    ([AC.can_manage_issuesubmission.codename], 200),
+    ([], 302)
+))
+@pytest.mark.parametrize('view,use_issue_submission_args', (
+    ('userspace:journal:editor:issues', False),
+    ('userspace:journal:editor:add', False),
+    ('userspace:journal:editor:update', True),
+))
 class TestViews:
-    def test_issuesubmission_restricted(self):
-        user_non_granted = UserFactory()
-        journal = JournalFactory()
-        client = Client(logged_user=user_non_granted)
-        url = reverse('userspace:journal:editor:issues', args=(journal.pk, ))
-        response = client.get(url)
-        assert response.status_code == 403
 
-    def test_issuesubmission_granted(self):
+    def test_userspace_journal_permissions(self, permissions, code, view, use_issue_submission_args):
         user_granted = UserFactory()
         journal = JournalFactory()
         journal.members.add(user_granted)
-
         client = Client(logged_user=user_granted)
-        url = reverse('userspace:journal:editor:issues', kwargs={'journal_pk': journal.pk})
+
+        for permission in permissions:
+            ct = ContentType.objects.get(app_label="erudit", model="journal")
+            Authorization.objects.create(
+                content_type=ct,
+                user=user_granted,
+                object_id=journal.id,
+                authorization_codename=permission)
+
+        if use_issue_submission_args:
+            issue = IssueSubmissionFactory(journal=journal)
+            args = (issue.journal.pk, issue.pk, )
+            kwargs = {}
+        else:
+            args = ()
+            kwargs = {'journal_pk': journal.pk}
+
+        url = reverse(view, args=args, kwargs=kwargs)
         response = client.get(url)
-        assert response.status_code == 302
-
-        ct = ContentType.objects.get(app_label="erudit", model="journal")
-        Authorization.objects.create(
-            content_type=ct,
-            user=user_granted,
-            object_id=journal.id,
-            authorization_codename=AC.can_manage_issuesubmission.codename)
-        response = client.get(url)
-        assert response.status_code == 200
-
-    def test_issuesubmission_add_restricted(self):
-        user_non_granted = UserFactory()
-        journal = JournalFactory()
-        client = Client(logged_user=user_non_granted)
-        url = reverse('userspace:journal:editor:add', args=(journal.pk, ))
-        response = client.get(url)
-        assert response.status_code == 403
-
-    def test_issuesubmission_add_granted(self):
-        user_granted = UserFactory()
-        journal = JournalFactory()
-        journal.members.add(user_granted)
-
-        client = Client(logged_user=user_granted)
-        url = reverse('userspace:journal:editor:add', kwargs={'journal_pk': journal.pk})
-        response = client.get(url)
-        assert response.status_code == 302
-
-        ct = ContentType.objects.get(app_label="erudit", model="journal")
-        Authorization.objects.create(
-            content_type=ct,
-            user=user_granted,
-            object_id=journal.id,
-            authorization_codename=AC.can_manage_issuesubmission.codename)
-        response = client.get(url)
-        assert response.status_code == 200
-
-    def test_issuesubmission_update_restricted(self):
-        user_non_granted = UserFactory()
-        issue = IssueSubmissionFactory()
-
-        client = Client(logged_user=user_non_granted)
-        url = reverse('userspace:journal:editor:update', args=(issue.journal.pk, issue.pk, ))
-        response = client.get(url)
-        assert response.status_code == 403
-
-    def test_issuesubmission_update_granted(self):
-        user_granted = UserFactory()
-        journal = JournalFactory()
-        journal.members.add(user_granted)
-        issue = IssueSubmissionFactory(journal=journal)
-
-        client = Client(logged_user=user_granted)
-        url = reverse('userspace:journal:editor:update', kwargs={
-            'journal_pk': journal.pk, 'pk': issue.pk})
-        response = client.get(url)
-        assert response.status_code == 302
-
-        ct = ContentType.objects.get(app_label="erudit", model="journal")
-        Authorization.objects.create(
-            content_type=ct,
-            user=user_granted,
-            object_id=journal.id,
-            authorization_codename=AC.can_manage_issuesubmission.codename)
-        response = client.get(url)
-        assert response.status_code == 200
-
+        assert response.status_code == code
 
 class TestRules:
 
