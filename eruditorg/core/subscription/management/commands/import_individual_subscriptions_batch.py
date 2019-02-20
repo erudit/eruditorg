3c36e9fa8eb74f60eaaf3f8353f7f98b79638a51
David Cormier <david.cormier@erudit.org>
2019-02-20 11:27:17 -0500

    Fix plan.subscribe_email call in import batch
    Ref: #2043

--- eruditorg/core/subscription/management/commands/import_individual_subscriptions_batch.py
+++ eruditorg/core/subscription/management/commands/import_individual_subscriptions_batch.py
@@ -78,7 +78,7 @@ class Command(BaseCommand):
                         self.stdout.write("Journal {} has no plan.".format(journal_shortname))
                         raise
 
-                    plan.subscribe_email(email, firstname=first_name, lastname=last_name)
+                    plan.subscribe_email(email, first_name=first_name, last_name=last_name)
                 else:
                     user, _ = get_user_model().objects.get_or_create(
                         email=email,
