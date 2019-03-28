0066603215bf59d3c53c6a0953892dddd6bef7aa
Maxime Turcotte <maxime.turcotte@erudit.org>
2019-03-28 15:33:31 -0400

    Add missing migration

--- eruditorg/erudit/models/journal.py
+++ eruditorg/erudit/models/journal.py
@@ -99,7 +99,7 @@ class Journal(FedoraMixin, FedoraDated):
 
     type = models.ForeignKey(
         JournalType, null=False, blank=False, verbose_name=_('Type'),
-        on_delete=models.CASCADE
+        default=JournalType.CODE_SCIENTIFIC, on_delete=models.CASCADE
     )
     """ The type of the journal """
 
