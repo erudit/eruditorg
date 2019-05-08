0bfa9894e0a8f7e59cf57d7298f85110412521ab
David Cormier <david.cormier@erudit.org>
2019-05-08 11:47:55 -0400

    Remove DrupalPasswordHasher

--- eruditorg/base/settings/base.py
+++ eruditorg/base/settings/base.py
@@ -347,7 +347,6 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.SHA1PasswordHasher',
     'django.contrib.auth.hashers.MD5PasswordHasher',
     'django.contrib.auth.hashers.CryptPasswordHasher',
-    'core.accounts.hashers.DrupalPasswordHasher',
 ]
 
 # External systems
