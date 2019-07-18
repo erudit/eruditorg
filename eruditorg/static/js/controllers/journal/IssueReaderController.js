a5e4ba6b0092c1f346b70b31d792cb7524890d5e
Maxime Turcotte <maxime.turcotte@erudit.org>
2019-07-18 16:11:15 -0400

    Customize the book reader navigation bar and translate the strings
    ref #2081

--- eruditorg/static/js/controllers/journal/IssueReaderController.js
+++ eruditorg/static/js/controllers/journal/IssueReaderController.js
@@ -1,15 +1,9 @@
-import '!!script!bookreader/BookReader/jquery-ui-1.12.0.min.js';
-import '!!script!bookreader/BookReader/jquery.browser.min.js';
-import '!!script!bookreader/BookReader/dragscrollable-br.js';
-import '!!script!bookreader/BookReader/jquery.colorbox-min.js';
-import '!!script!bookreader/BookReader/jquery.bt.min.js';
-import '!!script!bookreader/BookReader/BookReader.js';
-import '!!script!bookreader/BookReader/plugins/plugin.url.js';
+import IssueReader from "../../modules/IssueReader";
 
 
 export default {
   init: function() {
-    var body = $("body")
+    var body = $("body");
     var options = {
       getNumLeafs: function() {
         return body.data("num-leafs");
@@ -21,16 +15,15 @@ export default {
         return body.data("page-height");
       },
       getPageURI: function(index, reduce, rotate) {
-        return body.data("book-url") + gettext('feuilletage/') + (index + 1).toString() + '.jpg' + body.data("ticket");
+        return body.data("book-url") + gettext("feuilletage/") + (index + 1).toString() + ".jpg" + body.data("ticket");
       },
       bookUrl: body.data("book-url") + body.data("ticket"),
       bookUrlText: gettext("Retour au sommaire"),
-      bookUrlTitle: gettext("Retour au sommaire"),
       imagesBaseURL: body.data("images-base-url"),
-      showLogo: false,
+      el: "#issue-reader",
       protected: true,
     };
-    var br = new BookReader(options);
+    var br = new IssueReader(options);
     br.init();
   },
 };
