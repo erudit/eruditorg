e8ffb2a2363d1f94166c9da9082fa6d82b905c56
Maxime Turcotte <maxime.turcotte@erudit.org>
2019-09-25 11:45:43 -0400

    Parametrize TestIssueSubmissionStatus and add test for NEEDS_CORRECTIONS status
    ref #2371

--- tests/functional/apps/userspace/journal/editor/test_status.py
+++ tests/functional/apps/userspace/journal/editor/test_status.py
@@ -19,41 +19,22 @@ class TestIssueSubmissionStatus(BaseEditorTestCase):
             if not elem.attrib['type'] == 'hidden'
         ])
 
-    def test_a_draft_submission_is_editable(self):
-        draft_submission = IssueSubmission.objects.create(
+    @pytest.mark.parametrize('status, expected_is_disabled', (
+        (IssueSubmission.DRAFT, False),
+        (IssueSubmission.SUBMITTED, False),
+        (IssueSubmission.NEEDS_CORRECTIONS, False),
+        (IssueSubmission.VALID, True),
+    ))
+    def test_issue_submission_is_editable_by_status(self, status, expected_is_disabled):
+        issue_submission = IssueSubmission.objects.create(
             journal=self.journal,
             volume="2",
             contact=self.user,
-            status=IssueSubmission.DRAFT
+            status=status,
         )
-        result = self.client.get(
-            reverse('userspace:journal:editor:update', kwargs={
-                'journal_pk': self.journal.pk, 'pk': draft_submission.pk})
-        )
-        assert not self.is_disabled(result)
-
-    def test_a_submitted_submission_is_editable(self):
-        submitted_submission = IssueSubmission.objects.create(
-            journal=self.journal,
-            volume="2",
-            contact=self.user,
-            status=IssueSubmission.SUBMITTED
-        )
-        result = self.client.get(
-            reverse('userspace:journal:editor:update', kwargs={
-                'journal_pk': self.journal.pk, 'pk': submitted_submission.pk})
-        )
-        assert not self.is_disabled(result)
-
-    def test_a_valid_submission_is_not_editable(self):
-        valid_submission = IssueSubmission.objects.create(
-            journal=self.journal,
-            volume="2",
-            contact=self.user,
-            status=IssueSubmission.VALID
-        )
-        result = self.client.get(
-            reverse('userspace:journal:editor:update', kwargs={
-                'journal_pk': self.journal.pk, 'pk': valid_submission.pk})
-        )
-        assert self.is_disabled(result)
+        url = reverse('userspace:journal:editor:update', kwargs={
+            'journal_pk': self.journal.pk,
+            'pk': issue_submission.pk,
+        })
+        result = self.client.get(url)
+        assert self.is_disabled(result) == expected_is_disabled
