d083d01da7d54d002117bfc1a8f28073ca38c948
Maxime Turcotte <maxime.turcotte@erudit.org>
2019-11-12 12:27:02 -0500

    Remove BOOKS flag and retro fallback in templates
    ref #2449

--- eruditorg/apps/public/book/urls.py
+++ eruditorg/apps/public/book/urls.py
@@ -1,20 +1,15 @@
 from django.conf.urls import url
 from django.utils.translation import ugettext_lazy as _
 from . import views
-from waffle.decorators import waffle_flag
 
 app_name = "book"
 
 urlpatterns = [
-    url(r'^$',
-        waffle_flag('BOOKS')(
-            views.BookListView.as_view()
-        ), name='home'),
+    url(r'^$', views.BookListView.as_view(), name='home'),
     url(_(r'^(?P<collection_slug>[-\w]+)/(?P<slug>[-\w]+)/$'),
-        waffle_flag('BOOKS')(views.BookDetailView.as_view()),
-        name='book_detail'),
+        views.BookDetailView.as_view(), name='book_detail'),
     url(_(r'^(?P<collection_slug>[-\w]+)/(?P<slug>[-\w]+)/(?P<chapter_id>[a-z0-9]+)/$'),
-        waffle_flag('BOOKS')(views.ChapterDetailView.as_view()), name='chapter_detail'),
+        views.ChapterDetailView.as_view(), name='chapter_detail'),
     url(_(r'^(?P<collection_slug>[-\w]+)/(?P<slug>[-\w]+)/(?P<chapter_id>[a-z0-9]+)\.pdf$'),
-        waffle_flag('BOOKS')(views.chapter_pdf_view), name='chapter_pdf'),
+        views.chapter_pdf_view, name='chapter_pdf'),
 ]
