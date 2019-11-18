4237671360509560c144094c20e57f4f1ea6f634
Maxime Turcotte <maxime.turcotte@erudit.org>
2019-11-18 09:41:38 -0500

    Display the list of baskets in the template
    ref #2470

--- eruditorg/apps/userspace/library/collection/views.py
+++ eruditorg/apps/userspace/library/collection/views.py
@@ -1,3 +1,7 @@
+import datetime
+import json
+import requests
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
@@ -16,3 +20,15 @@ class CollectionView(
 
     def get_target_instance(self):
         return self.current_organisation
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if settings.ABONNEMENTS_BASKETS_BACKEND_URL is None:
+            return context
+        response = requests.get(
+            settings.ABONNEMENTS_BASKETS_BACKEND_URL + str(datetime.datetime.now().year),
+        )
+        if response.status_code == 200:
+            context['baskets'] = json.loads(response.content.decode())
+            context['kbart_2014_backend_url'] = settings.KBART_2014_BACKEND_URL
+        return context
