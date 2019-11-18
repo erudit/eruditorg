ded11b4c4d2af16ce16e87f2d2688b1dc717db01
Maxime Turcotte <maxime.turcotte@erudit.org>
2019-11-18 10:53:51 -0500

    Add a proxy view to be able to download files from the KBART 2014 backend
    ref #2470

--- eruditorg/apps/userspace/library/collection/urls.py
+++ eruditorg/apps/userspace/library/collection/urls.py
@@ -8,4 +8,5 @@ app_name = "collection"
 
 urlpatterns = [
     url(r'^$', views.CollectionView.as_view(), name='landing'),
+    url(r'^kbart2014/$', views.Kbart2014FileDownloadView.as_view(), name='kbart2014_download'),
 ]
--- eruditorg/apps/userspace/library/collection/views.py
+++ eruditorg/apps/userspace/library/collection/views.py
@@ -1,9 +1,12 @@
 import datetime
 import json
 import requests
+import urllib
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.http import HttpResponse, HttpResponseNotFound
+from django.views.generic import TemplateView, View
+from django.urls import reverse
 
 from django.conf import settings
 
@@ -30,5 +33,27 @@ class CollectionView(
         )
         if response.status_code == 200:
             context['baskets'] = json.loads(response.content.decode())
-            context['kbart_2014_backend_url'] = settings.KBART_2014_BACKEND_URL
+            context['kbart2014_download_url'] = reverse(
+                'userspace:library:collection:kbart2014_download',
+                kwargs={'organisation_pk': kwargs.get('organisation_pk')},
+            )
         return context
+
+
+class Kbart2014FileDownloadView(LoginRequiredMixin, OrganisationScopePermissionRequiredMixin, View):
+    """ Proxy view to download files from the KBART 2014 backend. """
+    permission_required = 'library.has_access_to_dashboard'
+
+    def get(self, request, *args, **kwargs):
+        if settings.KBART_2014_BACKEND_URL is None:
+            return HttpResponseNotFound
+        response = requests.get(
+            settings.KBART_2014_BACKEND_URL + '?{}'.format(urllib.parse.urlencode(request.GET)),
+        )
+        if response.status_code == 200:
+            new_response = HttpResponse(response.content)
+            new_response['Content-Type'] = response.headers.get('content-type')
+            new_response['Content-Disposition'] = response.headers.get('content-disposition')
+            return new_response
+        else:
+            return HttpResponseNotFound
