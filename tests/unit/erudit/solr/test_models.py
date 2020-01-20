26fc7f2625e72b77e64e7ca39fd75a7d274c2e90
Maxime Turcotte <maxime.turcotte@erudit.org>
2020-01-20 14:32:16 -0500

    Search results: Do not link to issue if we cannot generate issue URL
    Some old UNB articles are indexed in Solr with erroneous issue localidentifiers. Until the data
    problem is solved in Solr, we should not try to link to the issue if we don't know the URL.
    
    ref #2517

--- tests/unit/erudit/solr/test_models.py
+++ tests/unit/erudit/solr/test_models.py
@@ -1,9 +1,34 @@
 import pysolr
+import pytest
 
-from erudit.solr.models import SolrDocument
+from erudit.solr.models import Article, SolrDocument
+from erudit.test.factories import IssueFactory
 
 
 class TestSolrDocument:
     def test_localidentfier_doesnt_include_unb_prefix(self):
         assert SolrDocument({"ID": "unb:123", "Corpus_fac": "Article"}).localidentifier == "123"
         assert SolrDocument({"ID": "123", "Corpus_fac": "Article"}).localidentifier == "123"
+
+
+@pytest.mark.django_db
+class TestArticle:
+    @pytest.mark.parametrize("issue_exist, external_url, expected_url", (
+        (False, None, None),
+        (True, None, "/fr/revues/journal/2020-issue/"),
+        (True, "https://www.exemple.com", "https://www.exemple.com"),
+    ))
+    def test_issue_url(self, issue_exist, external_url, expected_url):
+        if issue_exist:
+            IssueFactory(
+                journal__code="journal",
+                localidentifier="issue",
+                year="2020",
+                external_url=external_url,
+            )
+        article = Article({
+            "ID": "article",
+            "NumeroID": "issue",
+            "RevueID": "journal",
+        })
+        assert article.issue_url == expected_url
