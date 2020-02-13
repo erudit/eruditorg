a675ea4cee9dc7cb92d130bc661f80b427564b54
Maxime Turcotte <maxime.turcotte@erudit.org>
2020-02-13 08:45:34 -0500

    Revert "Merge branch '2512-redis-sets' into 'master'"
    This reverts merge request !696

--- eruditorg/erudit/cache/__init__.py
+++
@@ -1,9 +0,0 @@
-from .client import EruditCacheClient
-
-
-def cache_set(cache, key, value, timeout, localidentifiers=[]):
-    # Pass `localidentifiers` to our cache client only if we are using EruditCacheClient.
-    if hasattr(cache, 'client') and isinstance(cache.client, EruditCacheClient):
-        cache.client.set(key, value, timeout, localidentifiers=localidentifiers)
-    else:
-        cache.set(key, value, timeout)
