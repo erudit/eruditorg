d8e44bb93d82626377bfb14463b488558b4f1f6f
Maxime Turcotte <maxime.turcotte@erudit.org>
2020-06-04 10:34:54 -0400

    Lighthouse: Add URLs to audits

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,6 +3,13 @@ module.exports = {
     collect: {
       url: [
         "https://test.erudit.org/fr/",
+        "https://test.erudit.org/fr/revues/",
+        "https://test.erudit.org/fr/revues/images/",
+        "https://test.erudit.org/fr/revues/images/2017-n182-images03064/",
+        "https://test.erudit.org/fr/revues/images/2017-n182-images03064/85562ac/",
+        "https://test.erudit.org/fr/revues/ae/",
+        "https://test.erudit.org/fr/revues/ae/2017-v93-n4-ae04492/",
+        "https://test.erudit.org/fr/revues/ae/2017-v93-n4-ae04492/1058591ar/",
       ],
       settings: {
         onlyCategories: [
