1b77c0a2f4fe98f307b603304c76c5f2dc7e0b1c
Maxime Turcotte <maxime.turcotte@erudit.org>
2020-06-04 12:33:11 -0400

    Lighthouse: Use prochain-test.erudit.org as it's the one which is deployed first

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,14 +2,15 @@ module.exports = {
   ci: {
     collect: {
       url: [
-        "https://test.erudit.org/fr/",
-        "https://test.erudit.org/fr/revues/",
-        "https://test.erudit.org/fr/revues/images/",
-        "https://test.erudit.org/fr/revues/images/2017-n182-images03064/",
-        "https://test.erudit.org/fr/revues/images/2017-n182-images03064/85562ac/",
-        "https://test.erudit.org/fr/revues/ae/",
-        "https://test.erudit.org/fr/revues/ae/2017-v93-n4-ae04492/",
-        "https://test.erudit.org/fr/revues/ae/2017-v93-n4-ae04492/1058591ar/",
+        "https://prochain-test.erudit.org/fr/",
+        "https://prochain-test.erudit.org/fr/revues/?sorting=name",
+        "https://prochain-test.erudit.org/fr/revues/?sorting=disciplines",
+        "https://prochain-test.erudit.org/fr/revues/images/",
+        "https://prochain-test.erudit.org/fr/revues/images/2017-n182-images03064/",
+        "https://prochain-test.erudit.org/fr/revues/images/2017-n182-images03064/85562ac/",
+        "https://prochain-test.erudit.org/fr/revues/ae/",
+        "https://prochain-test.erudit.org/fr/revues/ae/2017-v93-n4-ae04492/",
+        "https://prochain-test.erudit.org/fr/revues/ae/2017-v93-n4-ae04492/1058591ar/",
       ],
       settings: {
         onlyCategories: [
