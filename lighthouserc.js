9181ff4b3f4576348b4eeb73450c70c2456004ca
Maxime Turcotte <maxime.turcotte@erudit.org>
2020-06-04 13:32:13 -0400

    Lighthouse: Add some more URLs to audit

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,11 +6,15 @@ module.exports = {
         "https://prochain-test.erudit.org/fr/revues/?sorting=name",
         "https://prochain-test.erudit.org/fr/revues/?sorting=disciplines",
         "https://prochain-test.erudit.org/fr/revues/images/",
+        "https://prochain-test.erudit.org/fr/revues/images/auteurs/",
         "https://prochain-test.erudit.org/fr/revues/images/2017-n182-images03064/",
         "https://prochain-test.erudit.org/fr/revues/images/2017-n182-images03064/85562ac/",
         "https://prochain-test.erudit.org/fr/revues/ae/",
+        "https://prochain-test.erudit.org/fr/revues/ae/auteurs/",
         "https://prochain-test.erudit.org/fr/revues/ae/2017-v93-n4-ae04492/",
         "https://prochain-test.erudit.org/fr/revues/ae/2017-v93-n4-ae04492/1058591ar/",
+        "https://prochain-test.erudit.org/fr/recherche/avancee/",
+        "https://prochain-test.erudit.org/fr/recherche/?basic_search_term=environnement",
       ],
       settings: {
         onlyCategories: [
