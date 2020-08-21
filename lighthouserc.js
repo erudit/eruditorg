83b7397cc5ad11a62b033f85f2dcd1ae45f97389
Maxime Turcotte <maxime.turcotte@erudit.org>
2020-08-21 07:58:27 -0400

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,21 +2,22 @@ module.exports = {
   ci: {
     collect: {
       url: [
-        "https://prochain-test.erudit.org/fr/",
-        "https://prochain-test.erudit.org/fr/revues/?sorting=name",
-        "https://prochain-test.erudit.org/fr/revues/?sorting=disciplines",
-        "https://prochain-test.erudit.org/fr/revues/images/",
-        "https://prochain-test.erudit.org/fr/revues/images/auteurs/",
-        "https://prochain-test.erudit.org/fr/revues/images/2017-n182-images03064/",
-        "https://prochain-test.erudit.org/fr/revues/images/2017-n182-images03064/85562ac/",
-        "https://prochain-test.erudit.org/fr/revues/ae/",
-        "https://prochain-test.erudit.org/fr/revues/ae/auteurs/",
-        "https://prochain-test.erudit.org/fr/revues/ae/2017-v93-n4-ae04492/",
-        "https://prochain-test.erudit.org/fr/revues/ae/2017-v93-n4-ae04492/1058591ar/",
-        "https://prochain-test.erudit.org/fr/recherche/avancee/",
-        "https://prochain-test.erudit.org/fr/recherche/?basic_search_term=environnement",
+        "https://www.erudit.org/fr/",
+        "https://www.erudit.org/fr/revues/?sorting=name",
+        "https://www.erudit.org/fr/revues/?sorting=disciplines",
+        "https://www.erudit.org/fr/revues/images/",
+        "https://www.erudit.org/fr/revues/images/auteurs/",
+        "https://www.erudit.org/fr/revues/images/2017-n182-images03064/",
+        "https://www.erudit.org/fr/revues/images/2017-n182-images03064/85562ac/",
+        "https://www.erudit.org/fr/revues/ae/",
+        "https://www.erudit.org/fr/revues/ae/auteurs/",
+        "https://www.erudit.org/fr/revues/ae/2017-v93-n4-ae04492/",
+        "https://www.erudit.org/fr/revues/ae/2017-v93-n4-ae04492/1058591ar/",
+        "https://www.erudit.org/fr/recherche/avancee/",
+        "https://www.erudit.org/fr/recherche/?basic_search_term=environnement",
       ],
       settings: {
+        chromeFlags: "--no-sandbox --headless",
         onlyCategories: [
           "performance",
           "accessibility",
@@ -24,6 +25,10 @@ module.exports = {
           "seo",
         ],
       },
+      numberOfRuns: 3,
+    },
+    upload: {
+      target: "temporary-public-storage",
     },
   },
 };
