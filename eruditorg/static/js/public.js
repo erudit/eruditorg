168973e927c91bac2d5e6bb7d7f3f440bb141d12
Maxime Turcotte <maxime.turcotte@erudit.org>
2020-08-27 08:01:24 -0400

    JavaScript: Move controllers' index.js to public.js & userspace.js
    So it will be easier to split those files.
    
    ref #2660

--- eruditorg/static/js/public.js
+++ eruditorg/static/js/public.js
@@ -8,7 +8,29 @@ import '!!script-loader!lazysizes/plugins/aspectratio/ls.aspectratio.min';
 
 import modules from './modules';
 import DOMRouter from './core/DOMRouter';
-import controllers from './controllers/public';
+
+import LoginController from './controllers/public/LoginController';
+import AdvancedSearchController from './controllers/public/search/AdvancedSearchController';
+import ResultsController from './controllers/public/search/ResultsController';
+import ArticleDetailController from './controllers/public/journal/ArticleDetailController';
+import IssueDetailController from './controllers/public/journal/IssueDetailController';
+import JournalListController from './controllers/public/journal/JournalListController';
+import JournalDetailController from './controllers/public/journal/JournalDetailController';
+import SavedCitationListController from './controllers/public/citations/SavedCitationListController';
+
+const controllers = {
+  'public:login': LoginController,
+  'public:search:advanced-search': AdvancedSearchController,
+  'public:search:results': ResultsController,
+  'public:journal:article_detail': ArticleDetailController,
+  'public:journal:issue_detail': IssueDetailController,
+  'public:journal:journal_list': JournalListController,
+  'public:journal:journal_detail': JournalDetailController,
+  'public:citations:list': SavedCitationListController,
+};
+
+export default controllers;
+
 
 // Defines the router and initializes it!
 let router = new DOMRouter(controllers);
--- eruditorg/static/js/userspace.js
+++ eruditorg/static/js/userspace.js
@@ -7,7 +7,18 @@ import '!!script-loader!inline-svg/dist/inlineSVG.min';
 import modules from './modules';
 import Select2 from 'select2/dist/js/select2.full.min';
 import DOMRouter from './core/DOMRouter';
-import controllers from './controllers/userspace';
+
+import FormController from './controllers/userspace/editor/FormController';
+import ConnectionLandingController from './controllers/userspace/library/connection/ConnectionLandingController';
+import {JournalInformationFormController} from './controllers/userspace/journalinformation/FormController';
+
+const controllers = {
+  'userspace:editor:form': FormController,
+  'userspace:library:connection:landing': ConnectionLandingController,
+  'userspace:journalinformation:update': new JournalInformationFormController(),
+};
+
+export default controllers;
 
 // Defines the router and initializes it!
 let router = new DOMRouter(controllers);
