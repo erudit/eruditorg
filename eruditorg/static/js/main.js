b2aa1f629607049d8eb4f4b22785ffb0a75b05fe
Maxime Turcotte <maxime.turcotte@erudit.org>
2020-09-01 16:21:09 -0400

    CSS & JS: Stop fetching cookieconsent from cdnjs and add it to our npm dependencies
    It should be faster to get it from our own CDN
    
    ref #2667

--- eruditorg/static/js/main.js
+++ eruditorg/static/js/main.js
@@ -3,6 +3,7 @@ import '@babel/polyfill';
 import '!!script-loader!jquery/dist/jquery.min';
 import '!!script-loader!akkordion/dist/akkordion.min';
 import '!!script-loader!bootstrap-sass/assets/javascripts/bootstrap.min';
+import '!!script-loader!cookieconsent/build/cookieconsent.min';
 import '!!script-loader!inline-svg/dist/inlineSVG.min';
 import '!!script-loader!jquery-validation/dist/jquery.validate.min';
 import '!!script-loader!jquery-validation/dist/localization/messages_fr';
