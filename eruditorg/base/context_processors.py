bbfb6806cde44691874debfe8a6fddf584562a57
Maxime Turcotte <maxime.turcotte@erudit.org>
2020-09-15 14:00:59 -0400

    Remove the FOREVER_TTL settings and set default TTL to 30 days
    We don't want None TTL in our Redis since we use volatile-lru as our maxmemory policy, so if redis
    is full of keys with no TTL, it will crash.
    
    ref #2544

--- eruditorg/base/context_processors.py
+++ eruditorg/base/context_processors.py
@@ -41,5 +41,4 @@ def cache_constants(request):
         'NEVER_TTL': settings.NEVER_TTL,        # Do not cache
         'SHORT_TTL': settings.SHORT_TTL,        # Cache for 1 hour
         'LONG_TTL': settings.LONG_TTL,          # Cache for 1 day
-        'FOREVER_TTL': settings.FOREVER_TTL,    # Cache forever
     }
