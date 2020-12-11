b0cfb3615cf9cc5ec0971bc54a7f1b0c8ab0e396
benselme <benselme@gmail.com>
2020-12-11 14:43:15 -0500

    Rework Counter reports app
    * Remove lots of unused files and functions
    * Simplify forms class structure
    * Clarify forms initialization and defaults
    * Add a field for period type (yearly or monthly) and   javascript
      to show/hide the relevant controls for each option
    * Upgrade Counter R5 lib and use its functions to compute
      the available range of dates available for reports
    
    ref #2697

--- eruditorg/apps/userspace/library/stats/forms.py
+++ eruditorg/apps/userspace/library/stats/forms.py
@@ -1,9 +1,8 @@
+import calendar
 import datetime as dt
 import typing
 from django import forms
 from django.utils.translation import gettext_lazy as _
-
-current_year = dt.datetime.now().year
 
 FORMAT_CHOICES = [
     ("csv", "CSV"),
@@ -12,7 +11,6 @@ FORMAT_CHOICES = [
 ]
 
 MONTH_CHOICES = [
-    ("", ""),
     (1, _("janvier")),
     (2, _("février")),
     (3, _("mars")),
@@ -28,165 +26,230 @@ MONTH_CHOICES = [
 ]
 
 
+class DatesRange(typing.NamedTuple):
+    first_day: dt.date
+    last_day: dt.date
+
+    @property
+    def first_year(self):
+        return self.first_day.year
+
+    @property
+    def first_month(self):
+        return self.first_day.month
+
+    @property
+    def last_year(self):
+        return self.last_day.year
+
+    @property
+    def last_month(self):
+        return self.last_day.month
+
+
+def get_month_period(cleaned_data: dict) -> DatesRange:
+    """Compute first and last date for a months-spanning period stored in cleaned_data, starting at
+    the first day of the start month to the last day of the end month. This function expects
+    cleaned_data to contain the expected fields with valid data in them.
+
+    """
+
+    # those fields are required and coerced to int so the cast is safe
+    month_start = typing.cast(int, cleaned_data["month_start"])
+    month_end = typing.cast(int, cleaned_data["month_end"])
+    year_start = typing.cast(int, cleaned_data["year_start"])
+    year_end = typing.cast(int, cleaned_data["year_end"])
+    last_day_of_end_month = calendar.monthrange(year_end, month_end)[1]
+    end_date = dt.date(year_end, month_end, last_day_of_end_month)
+    return DatesRange(dt.date(year_start, month_start, 1), end_date)
+
+
 class CounterReportForm(forms.Form):
-    month_start = forms.TypedChoiceField(choices=MONTH_CHOICES, required=False, coerce=int)
-    month_end = forms.TypedChoiceField(choices=MONTH_CHOICES, required=False, coerce=int)
-
-    def __init__(self, form_info, end_year, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.form_info = form_info
-        self.release = form_info.counter_release
-
-        year_choices = [("", "")] + [(str(y), str(y)) for y in range(end_year, 2008, -1)]
-
-        year_period_choices = [("", "")] + [(str(y), str(y)) for y in range(end_year, 2010, -1)]
-
-        self.fields["year"] = forms.TypedChoiceField(
-            choices=year_choices, required=False, coerce=int
-        )
-        self.fields["year_start"] = forms.TypedChoiceField(
-            choices=year_period_choices, required=False, coerce=int
-        )
-        self.fields["year_end"] = forms.TypedChoiceField(
-            choices=year_period_choices, required=False, coerce=int
-        )
-
-    def clean(self):
-        cleaned_data = super().clean()
-
-        year = cleaned_data.get("year", None)
-        month_start = cleaned_data.get("month_start", None)
-        month_end = cleaned_data.get("month_end", None)
-        year_start = cleaned_data.get("year_start", None)
-        year_end = cleaned_data.get("year_end", None)
-
-        if not year and not all([month_start, month_end, year_start, year_end]):
-            self.add_error("year", _("Vous devez spécifier l'année ou la période"))
-
-        if all([month_start, month_end, year_start, year_end]):
-            dstart = dt.date(int(year_start), int(month_start), 1)
-            dend = dt.date(int(year_end), int(month_end), 1)
-
-            if dstart >= dend:
-                self.add_error(
-                    "year_start", _("La date de début doit être inférieure à la date de fin")
-                )
-
-            if dend - dt.timedelta(weeks=104) > dstart:
-                self.add_error("year_start", _("La période doit être de deux ans maximum"))
-
-        return cleaned_data
-
-    def get_report_period(self) -> typing.Tuple[dt.date, dt.date]:
-        # TODO: incohérence: la date de fin est-elle incluse dans le rapport ou non ?
-        #    dans tous les cas soit l'année, soit le dernier mois sont faux.
-        cleaned_data = self.cleaned_data
-        year = cleaned_data.get("year", None)
-        if year:
-            return dt.date(year, 1, 1), dt.date(year, 12, 31)
-
-        month_start = cleaned_data["month_start"]
-        month_end = cleaned_data["month_end"]
-        year_start = cleaned_data["year_start"]
-        year_end = cleaned_data["year_end"]
-
-        return dt.date(year_start, month_start, 1), dt.date(year_end, month_end, 1)
-
-
-class CounterR4Form(CounterReportForm):
-    format = forms.ChoiceField(choices=FORMAT_CHOICES, required=False)
-
-
-class CounterJR1Form(CounterR4Form):
-    prefix = "counter_jr1"
-    report_type = forms.CharField(initial="counter-jr1", widget=forms.HiddenInput(), required=False)
-
-
-class CounterJR1GOAForm(CounterR4Form):
-    prefix = "counter_jr1_goa"
-    report_type = forms.CharField(initial="counter-jr1-goa", widget=forms.HiddenInput())
-
-
-class CounterR5TRJ1Form(CounterReportForm):
-    prefix = "counter_r5_trj1"
-    report_code = "TR_J1"
-
-
-class CounterR5TRJ3Form(CounterReportForm):
-    prefix = "counter_r5_trj3"
-    report_code = "TR_J3"
-
-
-class CounterR5IRA1Form(CounterReportForm):
-    prefix = "counter_r5_ira1"
-    report_code = "IR_A1"
-
-
-class StatsFormInfo(typing.NamedTuple):
     code: str
-    form_class: typing.Type[CounterReportForm]
     tab_label: str
     title: str
     description: str
     counter_release: str
 
-    @property
-    def submit_name(self):
-        return "submit-{}".format(self.code)
+    period_type = forms.ChoiceField(
+        choices=[
+            ("monthly", _("Période mensuelle")),
+            ("annual", _("Période annuelle")),
+        ],
+        initial="monthly",
+        widget=forms.RadioSelect,
+        required=True,
+    )
+    month_start = forms.TypedChoiceField(choices=MONTH_CHOICES, required=True, coerce=int)
+    month_end = forms.TypedChoiceField(choices=MONTH_CHOICES, required=True, coerce=int)
+    year_start = forms.TypedChoiceField(choices=(), required=True, coerce=int)
+    year_end = forms.TypedChoiceField(choices=(), required=True, coerce=int)
+    year = forms.TypedChoiceField(choices=(), required=True, coerce=int)
+
+    def __init__(self, available_range: DatesRange, *args, **kwargs):
+        """Base form for both Counter R4 and R5 reports.
+
+        To simplify validation all fields are required, and all have initial values set in __init__
+        according to the available range.
+
+        :param available_range: The range of dates for which data is available (inclusive)
+        """
+
+        self.available_range = available_range
+        end_year = available_range.last_year
+        last_month = available_range.last_month
+        # the default range for reports is the latest available month or year
+        initial = kwargs["initial"] = kwargs.get("initial", {})
+        initial.update(
+            {
+                "year": end_year,
+                "year_start": end_year,
+                "month_start": last_month,
+                "year_end": end_year,
+                "month_end": last_month,
+            }
+        )
+
+        super().__init__(*args, **kwargs)
+
+        first_year = available_range.first_year
+        available_years = list(range(end_year, first_year - 1, -1))
+        year_period_choices = [(str(y), str(y)) for y in available_years]
+        self.fields["year_start"].choices = year_period_choices
+        self.fields["year_end"].choices = year_period_choices
+        self.fields["year"].choices = year_period_choices
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if not self.errors and cleaned_data["period_type"] == "monthly":
+            start_date, end_date = get_month_period(cleaned_data)
+            if start_date >= end_date:
+                raise forms.ValidationError(
+                    _("La date de début doit être inférieure à la date de fin"),
+                    code="end_before_start",
+                )
+
+            if end_date - start_date > dt.timedelta(weeks=105):
+                raise forms.ValidationError(
+                    _("La période doit être de deux ans maximum"), code="too_long"
+                )
+
+        return cleaned_data
+
+    def get_report_period(self) -> DatesRange:
+        """Compute the report period as begin and end dates from what was entered in the form. It's
+        expected that the form data has been cleaned and validated before calling this method.
+
+        The period selected can be a whole year or a range of months. In any case, everything is
+        inclusive: first month and last month are included in the range, and the dates returned
+        are also inclusive. That means that in the case of a month range, the end date is the last
+        day of the selected end month (as required by the R5 spec and R5 library).
+
+        """
+
+        cleaned_data = self.cleaned_data
+        if cleaned_data["period_type"] == "annual":
+            year = cleaned_data["year"]
+            if year == self.available_range.last_year:
+                return get_month_period(
+                    {
+                        "year_start": year,
+                        "month_start": 1,
+                        "year_end": year,
+                        "month_end": self.available_range.last_month,
+                    }
+                )
+            elif year == self.available_range.first_year:
+                return get_month_period(
+                    {
+                        "year_start": year,
+                        "month_start": self.available_range.first_month,
+                        "year_end": year,
+                        "month_end": 12,
+                    }
+                )
+            else:
+                return DatesRange(dt.date(year, 1, 1), dt.date(year, 12, 31))
+
+        return get_month_period(cleaned_data)
+
+    @classmethod
+    def submit_name(cls):
+        return f"submit-{cls.code}"
 
 
-STATS_FORMS_INFO = [
-    StatsFormInfo(
-        code="counter-jr1",
-        form_class=CounterJR1Form,
-        tab_label=_("JR1"),
-        title=_("Journal Report 1"),
-        description=_(
-            "Nombre de requêtes réussies d’articles en texte intégral par mois et par " "revue"
-        ),
-        counter_release="R4",
-    ),
-    StatsFormInfo(
-        code="counter-jr1-goa",
-        form_class=CounterJR1GOAForm,
-        tab_label=_("JR1_GOA"),
-        title=_("Journal Report 1 GOA"),
-        description=_(
-            "Nombre de requêtes réussies d’articles en libre accès en texte intégral par "
-            "mois et par revue"
-        ),
-        counter_release="R4",
-    ),
-    StatsFormInfo(
-        code="counter-r5-trj1",
-        form_class=CounterR5TRJ1Form,
-        tab_label=_("TR_J1"),
-        title=_("Journal Requests (Excluding OA_Gold)"),
-        description=_(
-            "Nombre de requêtes réussies d’articles en accès restreint en texte intégral "
-            "par mois et par revue"
-        ),
-        counter_release="R5",
-    ),
-    StatsFormInfo(
-        code="counter-r5-trj3",
-        form_class=CounterR5TRJ3Form,
-        tab_label=_("TR_J3"),
-        title=_("Journal Usage by Access Type"),
-        description=_(
-            "Nombre de requêtes réussies d’articles en texte intégral "
-            "par mois, par revue et par type d'accès"
-        ),
-        counter_release="R5",
-    ),
-    StatsFormInfo(
-        code="counter-r5-ira1",
-        form_class=CounterR5IRA1Form,
-        tab_label=_("IR_A1"),
-        title=_("Journal Article Requests"),
-        description=_(
-            "Nombre de requêtes réussies d’articles en texte intégral " "par mois et par article"
-        ),
-        counter_release="R5",
-    ),
+class CounterR4Form(CounterReportForm):
+    format = forms.ChoiceField(choices=FORMAT_CHOICES, required=True)
+
+
+class CounterJR1Form(CounterR4Form):
+    code = "counter-jr1"
+    tab_label = _("JR1")
+    title = _("Journal Report 1")
+    description = _(
+        "Nombre de requêtes réussies d’articles en texte intégral par mois et par revue"
+    )
+    counter_release = "R4"
+    prefix = "counter_jr1"
+    is_gold_open_access = False
+
+
+class CounterJR1GOAForm(CounterR4Form):
+    code = "counter-jr1-goa"
+    tab_label = _("JR1_GOA")
+    title = _("Journal Report 1 GOA")
+    description = _(
+        "Nombre de requêtes réussies d’articles en libre accès en texte intégral par "
+        "mois et par revue"
+    )
+    counter_release = "R4"
+    prefix = "counter_jr1_goa"
+    is_gold_open_access = True
+
+
+class CounterR5Form(CounterReportForm):
+    counter_release = "R5"
+
+
+class CounterR5TRJ1Form(CounterR5Form):
+    code = "counter-r5-trj1"
+    tab_label = _("TR_J1")
+    title = _("Journal Requests (Excluding OA_Gold)")
+    description = _(
+        "Nombre de requêtes réussies d’articles en accès restreint en texte intégral "
+        "par mois et par revue"
+    )
+    prefix = "counter_r5_trj1"
+    report_code = "TR_J1"
+
+
+class CounterR5TRJ3Form(CounterR5Form):
+    code = "counter-r5-trj3"
+    tab_label = _("TR_J3")
+    title = _("Journal Usage by Access Type")
+    description = _(
+        "Nombre de requêtes réussies d’articles en texte intégral "
+        "par mois, par revue et par type d'accès"
+    )
+    prefix = "counter_r5_trj3"
+    report_code = "TR_J3"
+
+
+class CounterR5IRA1Form(CounterR5Form):
+    code = "counter-r5-ira1"
+    tab_label = _("IR_A1")
+    title = _("Journal Article Requests")
+    description = _(
+        "Nombre de requêtes réussies d’articles en texte intégral par mois et par article"
+    )
+    prefix = "counter_r5_ira1"
+    report_code = "IR_A1"
+
+
+REPORT_FORMS = [
+    CounterJR1Form,
+    CounterJR1GOAForm,
+    CounterR5TRJ1Form,
+    CounterR5TRJ3Form,
+    CounterR5IRA1Form,
 ]
--- eruditorg/apps/userspace/library/stats/views.py
+++ eruditorg/apps/userspace/library/stats/views.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 import datetime as dt
-import typing
+from functools import singledispatch
+from typing import Type, Tuple, Optional
 from io import StringIO
 
 import requests
@@ -10,7 +10,7 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 from apps.userspace.library.shortcuts import get_last_year_of_subscription
-from .settings import ERUDIT_COUNTER_BACKEND_URL
+from erudit.models import Organisation
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from base.viewmixins import MenuItemMixin
@@ -20,34 +20,112 @@ from apps.userspace.library.viewmixins import OrganisationScopePermissionRequire
 
 from .forms import (
     CounterReportForm,
-    StatsFormInfo,
-    STATS_FORMS_INFO,
-    CounterJR1Form,
-    CounterJR1GOAForm,
+    REPORT_FORMS,
+    CounterR4Form,
+    CounterR5Form,
+    DatesRange,
 )
 
-from counter_r5.generate import CounterR5Report
+from counter_r5 import CounterR5Report, CounterR5Config
 
 
-def compute_end_year(current_year: int, last_year_of_subscription: typing.Optional[int]) -> int:
-    if last_year_of_subscription is None:
-        return current_year
-    elif last_year_of_subscription <= current_year:
-        return last_year_of_subscription
-
-    return current_year
+def compute_r4_end_month(
+    today: dt.date,
+    last_year_of_subscription: Optional[int],
+) -> dt.date:
+    # first day of the previous month
+    last_complete_month = (today.replace(day=1) - dt.timedelta(days=1)).replace(day=1)
+    if last_year_of_subscription is None or last_year_of_subscription > last_complete_month.year:
+        return last_complete_month
+    else:
+        return dt.date(last_year_of_subscription, 12, 1)
 
 
 def get_stats_form(
-    form_info: StatsFormInfo, request_data: dict, end_year: int
-) -> typing.Tuple[CounterReportForm, bool]:
-    is_submitted = form_info.submit_name in request_data
+    form_class: Type[CounterReportForm],
+    request_data: dict,
+    available_range: DatesRange,
+) -> Tuple[CounterReportForm, bool]:
+    """Create a counter report request form and initialize it if it has been submitted. """
+
+    # There is one form for each type for counter report, but only one can be submitted at a time.
+    # If this form was submitted then the submit button name will be in the HTTP GET request's data.
+    is_submitted = form_class.submit_name() in request_data
     kwargs = {
         "data": request_data if is_submitted else None,
-        "form_info": form_info,
-        "end_year": end_year,
     }
-    return form_info.form_class(**kwargs), is_submitted
+    return form_class(available_range, **kwargs), is_submitted
+
+
+# noinspection PyUnusedLocal
+@singledispatch
+def get_report_response(form: CounterReportForm, organisation: Organisation) -> HttpResponse:
+    """Generate a HTTP Response containing the Counter report data requested in the submitted
+    form for the current organization.
+
+    """
+    pass
+
+
+@get_report_response.register(CounterR4Form)
+def get_r4_report_response(form: CounterR4Form, organisation: Organisation) -> HttpResponse:
+    dstart, dend = form.get_report_period()
+    report_arguments = {
+        "format": form.cleaned_data["format"],
+        "id": organisation.account_id,
+        "beginPeriod": dstart.strftime("%Y-%m-%d"),
+        "endPeriod": dend.strftime("%Y-%m-%d"),
+        "isGoldOpenAccess": form.is_gold_open_access,
+    }
+    # by default, the format is CSV
+    report_format = report_arguments["format"]
+    url = settings.ERUDIT_COUNTER_BACKEND_URL
+    if report_format == "xml":
+        url = f"{url}/counterXML.php"
+    elif report_format == "html":
+        url = f"{url}/counterHTML.php"
+    report = requests.get(url, params=report_arguments)
+    filename = "{id}-{beginPeriod}-{endPeriod}".format(**report_arguments)
+    response = HttpResponse(report, content_type=f"application/{report_format}")
+    response["Content-Disposition"] = f'attachment; filename="{filename}.{report_format}"'
+    return response
+
+
+@get_report_response.register(CounterR5Form)
+def get_r5_report_response(form: CounterR5Form, organisation: Organisation) -> HttpResponse:
+    begin_date, end_date = form.get_report_period()
+    organisation_id = organisation.account_id
+    config = get_counter_r5_config()
+    report = CounterR5Report(
+        config=config,
+        report_type=form.report_code,
+        customer_id=organisation.account_id,
+        customer_name=organisation.name,
+        begin_date=begin_date,
+        end_date=end_date,
+    )
+
+    f = StringIO()
+    report.write_csv(f)
+    response = HttpResponse(f.getvalue(), content_type="application/csv")
+    filename = (
+        f"{organisation_id}-{begin_date.strftime('%Y-%m-%d')}-{end_date.strftime('%Y-%m-%d')}"
+    )
+    response["Content-Disposition"] = f'attachment; filename="{filename}.csv"'
+    return response
+
+
+def get_counter_r5_config():
+    return CounterR5Config(
+        first_available_month=dt.datetime.strptime(
+            settings.COUNTER_R5_FIRST_AVAILABLE_MONTH, "%Y-%m-%d"
+        ).date(),
+        available_after=settings.COUNTER_R5_AVAILABLE_AFTER,
+        es_index=settings.ELASTICSEARCH_STATS_INDEX,
+        es_host=settings.ELASTICSEARCH_STATS_HOST,
+        es_port=settings.ELASTICSEARCH_STATS_PORT,
+        global_filters={"audit.is_legitimate": True},
+    )
 
 
 class StatsLandingView(
@@ -57,167 +135,39 @@ class StatsLandingView(
     menu_library = "stats"
     permission_required = "library.has_access_to_dashboard"
     template_name = "userspace/library/stats/landing.html"
-    report_base_url = ERUDIT_COUNTER_BACKEND_URL
-    counter_jr1_form = CounterJR1Form
-    counter_jr1goa_form = CounterJR1GOAForm
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         submitted_form = context.get("submitted_form", None)
         if submitted_form and submitted_form.is_valid():
-            # Get the report
-            if submitted_form.release == "R4":
-                report = self.get_r4_report(submitted_form)
-            else:
-                report = self.get_r5_report(submitted_form)
-            return report
+            return get_report_response(submitted_form, self.current_organisation)
 
         return super().get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(StatsLandingView, self).get_context_data(**kwargs)
-        current_year = dt.datetime.now().year
+        today = dt.date.today()
         last_year_of_subscription = get_last_year_of_subscription(self.current_organisation)
-        end_year = compute_end_year(current_year, last_year_of_subscription)
-        forms = []
-        for form_info in STATS_FORMS_INFO:
-            form, is_submitted = get_stats_form(form_info, self.request.GET, end_year)
-            forms.append(form)
+        r5_config = get_counter_r5_config()
+
+        available_ranges = {
+            "R4": DatesRange(
+                dt.date(2010, 1, 1), compute_r4_end_month(today, last_year_of_subscription)
+            ),
+            "R5": DatesRange(
+                r5_config.first_available_month, r5_config.get_last_available_month(today)
+            ),
+        }
+        context["forms"] = {
+            "R4": [],
+            "R5": [],
+        }
+        for form_class in REPORT_FORMS:
+            available_range = available_ranges[form_class.counter_release]
+            form, is_submitted = get_stats_form(form_class, self.request.GET, available_range)
+            context["forms"][form.counter_release].append(form)
             if is_submitted:
                 context["submitted_form"] = form
-        submitted_form = context.get("submitted_form", None)
-        submitted_release = submitted_form.release if submitted_form else None
-        releases = {}
-        for release in ("R4", "R5"):
-            release_forms = [f for f in forms if f.release == release]
-            releases[release] = {
-                "forms": release_forms,
-                "active_form": submitted_form if submitted_release == release else release_forms[0],
-            }
-        context["releases"] = releases
         context["section_aside"] = True
         context["library_statistics_site_messages"] = SiteMessage.objects.library_statistics()
         return context
-
-    def get_r4_report(self, form):
-        dstart, dend = form.get_report_period()
-        # report_arguments = form_data
-        report_arguments = {
-            "format": form.cleaned_data["format"],
-            "id": self.get_organisation_id(),
-            "beginPeriod": dstart.strftime("%Y-%m-%d"),
-            "endPeriod": dend.strftime("%Y-%m-%d"),
-        }
-
-        if form.cleaned_data["report_type"] == "counter-jr1-goa":
-            report_arguments["isGoldOpenAccess"] = True
-
-        report_format = report_arguments["format"]
-        url = self.report_base_url
-        if report_format == "xml":
-            url = "{}/counterXML.php".format(url)
-        elif report_format == "html":
-            url = "{}/counterHTML.php".format(url)
-        report = requests.get(url, params=report_arguments)
-        filename = "{id}-{beginPeriod}-{endPeriod}".format(**report_arguments)
-        response = HttpResponse(report, content_type="application/{}".format(report_format))
-        response["Content-Disposition"] = 'attachment; filename="{filename}.{format}"'.format(
-            filename=filename, format=report_format
-        )
-        return response
-
-    def get_requested_report(self):
-        report_type = None
-        if self.request.GET.get("{}-report_type".format(self.counter_jr1_form.prefix)):
-            report_type = "counter-jr1"
-        elif self.request.GET.get("{}-report_type".format(self.counter_jr1goa_form.prefix)):
-            report_type = "counter-jr1-goa"
-        return report_type
-
-    def get_report(self, form_data):
-        dstart, dend = self.get_report_period(form_data)
-        # report_arguments = form_data
-        report_arguments = {}
-        report_arguments["format"] = form_data["format"]
-        report_arguments["id"] = self.current_organisation.account_id
-        report_arguments["beginPeriod"] = dstart.strftime("%Y-%m-%d")
-        report_arguments["endPeriod"] = dend.strftime("%Y-%m-%d")
-        if form_data.get("report_type") == "counter-jr1-goa":
-            report_arguments["isGoldOpenAccess"] = True
-
-        format = report_arguments["format"]
-        report = requests.get(self._get_report_url(format), params=report_arguments)
-        filename = "{id}-{beginPeriod}-{endPeriod}".format(**report_arguments)
-        response = HttpResponse(
-            report, content_type="application/{format}".format(**report_arguments)
-        )
-        response["Content-Disposition"] = 'attachment; filename="{filename}.{format}"'.format(
-            filename=filename, format=format
-        )
-        return response
-
-    def get_report_period(self, form_data):
-        """ Returns a tuple (start date, end date) containing the period of the report. """
-        now_dt = dt.datetime.now()
-        year = form_data.get("year", None)
-        month_start = form_data.get("month_start", None)
-        month_end = form_data.get("month_end", None)
-        year_start = form_data.get("year_start", None)
-        year_end = form_data.get("year_end", None)
-
-        # First handles the case where a precise period has been specified
-        try:
-            dstart_str = "{year}-{month}-01".format(year=year_start, month=month_start)
-            dstart = dt.datetime.strptime(dstart_str, "%Y-%m-%d").date()
-            dend_str = "{year}-{month}-01".format(year=year_end, month=month_end)
-            dend = dt.datetime.strptime(dend_str, "%Y-%m-%d").date()
-        except (ValueError, TypeError):
-            dstart, dend = None, None
-        else:
-            return dstart, dend
-
-        # Then handles
-        try:
-            year = int(year)
-            assert year <= now_dt.year
-        except (ValueError, TypeError, AssertionError):
-            dstart, dend = None, None
-        else:
-            return dt.date(year, 1, 1), dt.date(year, 12, 31)
-
-        # We cannot determine the period to consider so we use the current year
-        return dt.date(now_dt.year, 1, 1), dt.date(now_dt.year, 12, 31)
-
-    def _get_report_url(self, format):
-        if format == "xml":
-            return "{}/counterXML.php".format(self.report_base_url)
-        if format == "html":
-            return "{}/counterHTML.php".format(self.report_base_url)
-        return self.report_base_url
-
-    def get_organisation_id(self):
-        return self.current_organisation.account_id
-
-    def get_r5_report(self, form):
-        begin_date, end_date = form.get_report_period()
-        organisation_id = self.get_organisation_id()
-        report = CounterR5Report(
-            report_type=form.report_code,
-            customer_id=organisation_id,
-            customer_name=self.current_organisation.name,
-            es_index=settings.ELASTICSEARCH_STATS_INDEX,
-            es_host=settings.ELASTICSEARCH_STATS_HOST,
-            es_port=settings.ELASTICSEARCH_STATS_PORT,
-            begin_date=begin_date,
-            end_date=end_date,
-            global_filters={"audit.is_legitimate": True},
-        )
-
-        f = StringIO()
-        report.write_csv(f)
-        response = HttpResponse(f.getvalue(), content_type="application/csv")
-        filename = "{}-{}-{}".format(
-            organisation_id, begin_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d")
-        )
-        response["Content-Disposition"] = f'attachment; filename="{filename}.csv"'
-        return response
