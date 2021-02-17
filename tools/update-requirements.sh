c459fb4b56e1351944b2f0bb3d326c1a12127f05
benselme <benselme@gmail.com>
2021-02-17 10:39:50 -0500

    Manage dev and test requirements with pip-tools

--- tools/update-requirements.sh
+++ tools/update-requirements.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 pip-compile --no-emit-index-url -U requirements.in -o requirements.txt
+pip-compile --no-emit-index-url -U requirements-test.in -o requirements-test.txt
+pip-compile --no-emit-index-url -U requirements-dev.in -o requirements-dev.txt
