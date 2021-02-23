1cda22376cc2a2c7d1db157a7a8e823ce3d30837
Maxime Turcotte <maxime.turcotte@erudit.org>
2021-02-23 11:34:21 -0500

    Run black on eruditorg/apps/userspace/library/stats
    ref #2735

--- eruditorg/apps/userspace/library/stats/common/views.py
+++ eruditorg/apps/userspace/library/stats/common/views.py
@@ -10,21 +10,20 @@ from apps.userspace.library.viewmixins import OrganisationScopePermissionRequire
 
 
 class StatsLandingView(
-        LoginRequiredMixin, MenuItemMixin, OrganisationScopePermissionRequiredMixin, TemplateView):
-    menu_library = 'stats'
-    permission_required = 'subscription.access_library_stats'
-    template_name = 'userspace/library/stats/landing.html'
+    LoginRequiredMixin, MenuItemMixin, OrganisationScopePermissionRequiredMixin, TemplateView
+):
+    menu_library = "stats"
+    permission_required = "subscription.access_library_stats"
+    template_name = "userspace/library/stats/landing.html"
 
     def get_context_data(self, **kwargs):
         context = super(StatsLandingView, self).get_context_data(**kwargs)
         current_year = dt.datetime.now().year
-        context['years'] = range(current_year, current_year - 20, -1)
+        context["years"] = range(current_year, current_year - 20, -1)
         months = []
         for month in range(1, 13):
             month_date = dt.date(current_year, month, 1)
 
-            months.append(
-                (month, _(month_date.strftime("%B")))
-            )
-        context['months'] = months
+            months.append((month, _(month_date.strftime("%B"))))
+        context["months"] = months
         return context
--- eruditorg/apps/userspace/library/stats/settings.py
+++ eruditorg/apps/userspace/library/stats/settings.py
@@ -1,5 +1,5 @@
 from django.conf import settings
 
 ERUDIT_COUNTER_BACKEND_URL = getattr(
-    settings, 'ERUDIT_COUNTER_BACKEND_URL', "http://counter-backend.localhost"
+    settings, "ERUDIT_COUNTER_BACKEND_URL", "http://counter-backend.localhost"
 )
