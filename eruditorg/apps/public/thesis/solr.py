0456d5a70403a34915e5221c991768a504f04ec0
Maxime Turcotte <maxime.turcotte@erudit.org>
2021-02-23 12:46:59 -0500

    Run black on eruditorg/apps/public/thesis
    ref #2735

--- eruditorg/apps/public/thesis/solr.py
+++ eruditorg/apps/public/thesis/solr.py
@@ -14,63 +14,63 @@ def get_client():
 
 def get_thesis_count():
     client = get_client()
-    query = 'Corpus_fac:Thèses'
+    query = "Corpus_fac:Thèses"
     args = {
-        'q': query,
-        'rows': '0',
-        'facet.limit': '0',
+        "q": query,
+        "rows": "0",
+        "facet.limit": "0",
     }
     solr_results = client.search(**args)
     return solr_results.hits
 
 
-RepositorySummary = namedtuple('RepositorySummary', 'count solr_dicts by_year by_author')
+RepositorySummary = namedtuple("RepositorySummary", "count solr_dicts by_year by_author")
 
 
 def get_repository_summary(repository_name):
-    cache_key = 'get_repository_summary-{}'.format(slugify(repository_name))
+    cache_key = "get_repository_summary-{}".format(slugify(repository_name))
     cached_result = cache.get(cache_key)
     if cached_result is not None:
         return cached_result
     client = get_client()
     query = 'Corpus_fac:Thèses Editeur:"{}"'.format(repository_name)
     args = {
-        'q': query,
-        'rows': '3',
-        'sort': 'DateAjoutErudit desc',
-        'facet.field': ['AnneePublication', 'AuteurNP_fac'],
-        'facet.limit': '99999',  # all authors
+        "q": query,
+        "rows": "3",
+        "sort": "DateAjoutErudit desc",
+        "facet.field": ["AnneePublication", "AuteurNP_fac"],
+        "facet.limit": "99999",  # all authors
     }
     solr_results = client.search(**args)
     result = RepositorySummary(
         solr_results.hits,
         list(solr_results.docs),
-        list(pairify(solr_results.facets['facet_fields']['AnneePublication'])),
-        list(pairify(solr_results.facets['facet_fields']['AuteurNP_fac'])),
+        list(pairify(solr_results.facets["facet_fields"]["AnneePublication"])),
+        list(pairify(solr_results.facets["facet_fields"]["AuteurNP_fac"])),
     )
     cache.set(cache_key, result)
     return result
 
 
-Theses = namedtuple('Theses', 'count solr_dicts')
+Theses = namedtuple("Theses", "count solr_dicts")
 
 
 def get_theses(repository_name, rows=50, page=1, sort=None, year=None, author_letter=None):
     if sort is None:
-        sort = ['DateAjoutErudit desc']
+        sort = ["DateAjoutErudit desc"]
     client = get_client()
     query = 'Corpus_fac:Thèses Editeur:"{}"'.format(repository_name)
     if year:
-        query += ' AnneePublication:{}'.format(year)
+        query += " AnneePublication:{}".format(year)
     if author_letter:
-        query += ' Auteur_tri:{}*'.format(author_letter)
+        query += " Auteur_tri:{}*".format(author_letter)
     start = (page - 1) * rows
     args = {
-        'q': query,
-        'rows': str(rows),
-        'start': start,
-        'sort': sort,
-        'facet.limit': '0',
+        "q": query,
+        "rows": str(rows),
+        "start": start,
+        "sort": sort,
+        "facet.limit": "0",
     }
     solr_results = client.search(**args)
     result = Theses(
--- eruditorg/apps/public/thesis/urls.py
+++ eruditorg/apps/public/thesis/urls.py
@@ -6,42 +6,35 @@ from . import views
 app_name = "thesis"
 
 urlpatterns = [
+    url(r"^$", views.ThesisHomeView.as_view(), name="home"),
     url(
-        r'^$',
-        views.ThesisHomeView.as_view(),
-        name='home'
-    ),
-    url(
-        _(r'^(?P<collection_code>[\w-]+)/$'),
+        _(r"^(?P<collection_code>[\w-]+)/$"),
         views.ThesisCollectionHomeView.as_view(),
-        name='collection_home'
+        name="collection_home",
     ),
-
     url(
-        _(r'^(?P<collection_code>[\w-]+)/(?P<publication_year>[\d-]+)/$'),
+        _(r"^(?P<collection_code>[\w-]+)/(?P<publication_year>[\d-]+)/$"),
         views.ThesisPublicationYearListView.as_view(),
-        name='collection_list_per_year'
+        name="collection_list_per_year",
     ),
     url(
-        _(r'^(?P<collection_code>[\w-]+)/(?P<author_letter>[\w\'-])/$'),
+        _(r"^(?P<collection_code>[\w-]+)/(?P<author_letter>[\w\'-])/$"),
         views.ThesisPublicationAuthorNameListView.as_view(),
-        name='collection_list_per_author_name'
+        name="collection_list_per_author_name",
     ),
-
     url(
-        _(r'^citation/(?P<solr_id>.+)\.enw$'),
+        _(r"^citation/(?P<solr_id>.+)\.enw$"),
         views.ThesisEnwCitationView.as_view(),
-        name='thesis_citation_enw'
+        name="thesis_citation_enw",
     ),
     url(
-        _(r'^citation/(?P<solr_id>.+)\.ris$'),
+        _(r"^citation/(?P<solr_id>.+)\.ris$"),
         views.ThesisRisCitationView.as_view(),
-        name='thesis_citation_ris'
+        name="thesis_citation_ris",
     ),
     url(
-        _(r'^citation/(?P<solr_id>.+)\.bib$'),
+        _(r"^citation/(?P<solr_id>.+)\.bib$"),
         views.ThesisBibCitationView.as_view(),
-        name='thesis_citation_bib'
+        name="thesis_citation_bib",
     ),
-
 ]
