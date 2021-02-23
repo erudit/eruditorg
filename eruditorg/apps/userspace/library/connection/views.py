0fe2fbd999c517a8fc9904cb0c7c5b2af4762c1e
Maxime Turcotte <maxime.turcotte@erudit.org>
2021-02-23 11:33:54 -0500

    Run black on eruditorg/apps/userspace/library/connection
    ref #2735

--- eruditorg/apps/userspace/library/connection/views.py
+++ eruditorg/apps/userspace/library/connection/views.py
@@ -9,17 +9,18 @@ from ..viewmixins import OrganisationScopePermissionRequiredMixin
 
 
 class ConnectionLandingView(
-        LoginRequiredMixin, MenuItemMixin, OrganisationScopePermissionRequiredMixin, TemplateView):
-    menu_library = 'connection'
-    permission_required = 'library.has_access_to_dashboard'
-    template_name = 'userspace/library/connection/landing.html'
+    LoginRequiredMixin, MenuItemMixin, OrganisationScopePermissionRequiredMixin, TemplateView
+):
+    menu_library = "connection"
+    permission_required = "library.has_access_to_dashboard"
+    template_name = "userspace/library/connection/landing.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['sushi_requestor_id'] = self.current_organisation.sushi_requester_id
-        context['id_client'] = self.current_organisation.account_id
-        context['sushi_url'] = getattr(settings, "SUSHI_URL", "")
-        context['z3950_host'] = getattr(settings, "Z3950_HOST", "")
-        context['z3950_port'] = getattr(settings, "Z3950_PORT", "")
-        context['z3950_database'] = getattr(settings, "Z3950_DATABASE", "")
+        context["sushi_requestor_id"] = self.current_organisation.sushi_requester_id
+        context["id_client"] = self.current_organisation.account_id
+        context["sushi_url"] = getattr(settings, "SUSHI_URL", "")
+        context["z3950_host"] = getattr(settings, "Z3950_HOST", "")
+        context["z3950_port"] = getattr(settings, "Z3950_PORT", "")
+        context["z3950_database"] = getattr(settings, "Z3950_DATABASE", "")
         return context
