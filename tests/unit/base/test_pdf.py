e6a730134f16558a3f07236b42bdc42fc6d4eb34
Maxime Turcotte <maxime.turcotte@erudit.org>
2021-03-01 11:48:39 -0500

    pikepdf: use context managers to open/close PDFs
    ref #2724

--- tests/unit/base/test_pdf.py
+++ tests/unit/base/test_pdf.py
@@ -1,12 +1,11 @@
 import fitz
-import io
 
 from base.pdf import get_pdf_first_page
 
 
 def test_get_pdf_first_page():
     with open("./tests/fixtures/pdf/044308ar.pdf", "rb") as pdf:
-        content = io.BytesIO(pdf.read())
+        content = pdf.read()
         # Original PDF has 4 pages.
         assert len(fitz.Document(stream=content, filetype="pdf")) == 4
         pdf_first_page = get_pdf_first_page(content)
