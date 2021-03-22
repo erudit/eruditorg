a5d816706ac3668ffb8db1a1b6294ece2f90dec5
Maxime Turcotte <maxime.turcotte@erudit.org>
2021-03-22 19:04:51 -0400

    Remove unused JS function
    ref #2697

--- eruditorg/static/js/controllers/userspace/library/stats/StatsLandingController.js
+++ eruditorg/static/js/controllers/userspace/library/stats/StatsLandingController.js
@@ -1,7 +1,6 @@
 export default {
     init: function() {
         this.togglePeriodFieldsets();
-        // this.setAvailableMonths();
     },
 
     togglePeriodFieldsets: function() {
@@ -18,46 +17,4 @@ export default {
             });
         });
     },
-/*    setAvailableMonths: function() {
-        $('form.counter').each(function() {
-            // Helper function to enable or disable month options based on the selected year and
-            // the last available month.
-            var toggleAvailableMonths = function($form_group) {
-                // Get the year field.
-                var $year_field = $form_group.find('select[name*="year"]');
-                // Get the month field.
-                var $month_field = $form_group.find('select[name*="month"]');
-                // Get the value of the selected year.
-                var selected_year = $year_field.children("option:selected").val();
-                // Get the value of the last available month.
-                var end_month = $form_group.closest('form').attr('data-end-month');
-
-                // If the selected year is the same as the current year, disable all months that
-                // come after the last available month.
-                if (parseInt(selected_year) === (new Date).getFullYear()) {
-                    for (i = 12; i > end_month; i--) {
-                        $month_field.children('option[value="' + i + '"]').prop('disabled', true);
-                    }
-                // Counter R5 exception, monthly reports are only available since April 2017.
-                } else if (parseInt(selected_year) === 2017) {
-                    for (i = 3; i > 0; i--) {
-                        $month_field.children('option[value="' + i + '"]').prop('disabled', true);
-                    }
-                // If the selected year is lower than the current year, enable all months.
-                } else if (parseInt(selected_year) < (new Date).getFullYear()) {
-                    $month_field.children('option').prop('disabled', false);
-                }
-            };
-
-            // Set the available months on init.
-            $(this).find('fieldset[id$="monthly"] select[name*="year"]').each(function() {
-                toggleAvailableMonths($(this).closest('.form-group'));
-            });
-
-            // Set the available months on change.
-            $(this).find('fieldset[id$="monthly"] select[name*="year"]').on('change', function() {
-                toggleAvailableMonths($(this).closest('.form-group'));
-            });
-        });
-    },*/
 }
