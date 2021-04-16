cfc6f24ea62d43bebcf9e3ca74af521c55ec8a74
Maxime Turcotte <maxime.turcotte@erudit.org>
2021-04-16 08:54:17 -0400

    For action needed issues & eminent archival issues, send an email to all production teams
    For each issue submission, we need to find the production teams groups associated with the issue's
    journal and send an email to all of them.
    
    ref #2762

--- eruditorg/core/editor/tasks.py
+++ eruditorg/core/editor/tasks.py
@@ -1,16 +1,15 @@
-import structlog
+from django.contrib.auth.models import Group
 from itertools import groupby
 from operator import attrgetter
 
 from core.email import Email
 
-from .models import IssueSubmission, ProductionTeam
+from .models import IssueSubmission
 from .apps import EMAIL_TAG
+from .utils import get_production_teams_groups
 
-logger = structlog.getLogger(__name__)
 
-
-def _handle_issue_submission_archival_and_files_deletion():
+def _handle_issue_submission_archival_and_files_deletion() -> None:
     # First, get issue submissions ready for archival and whose files must be deleted.
     for issue in IssueSubmission.objects.ready_for_archival():
         for fversion in issue.files_versions.all():
@@ -18,73 +17,107 @@ def _handle_issue_submission_archival_and_files_deletion():
         issue.archived = True
         issue.save()
 
-    # Get production team emails.
-    emails = ProductionTeam.emails()
-    if emails is None:
-        return
-
     # Now get issue submissions that will be archived eminently. The production team must be
     # informed that their files will be deleted in five days.
     issues = IssueSubmission.objects.eminent_archival()
-    if not issues.exists():
+    if not issues.count():
         return
 
-    # Group issues by journal.
-    eminent_archival_issues = {
-        journal_name: list(issues)
-        for journal_name, issues in groupby(
-            issues,
-            attrgetter("journal.name"),
-        )
+    # Get all production teams groups and their emails.
+    production_teams_groups = {
+        production_team_group.id: {
+            "emails": list(production_team_group.user_set.values_list("email", flat=True)),
+            "issue_submissions": [],
+        }
+        for production_team_group in Group.objects.filter(productionteam__isnull=False)
     }
 
-    # Send the email.
-    email = Email(
-        list(emails),
-        html_template="emails/editor/issue_files_deletion_content.html",
-        subject_template="emails/editor/issue_files_deletion_subject.html",
-        extra_context={"issue_submissions": eminent_archival_issues},
-        tag=EMAIL_TAG,
-    )
-    email.send()
+    # Group issues by production team group.
+    for issue in issues:
+        for production_team_group in get_production_teams_groups(issue.journal):
+            production_teams_groups[production_team_group.id]["issue_submissions"].append(issue)
+
+    # Now prepare and send an email to each production team group with eminent archival issues.
+    for production_team_group in production_teams_groups.values():
+        # Group issues by journal.
+        eminent_archival_issues = {
+            journal_name: list(issues)
+            for journal_name, issues in groupby(
+                production_team_group["issue_submissions"],
+                attrgetter("journal.name"),
+            )
+        }
+
+        # Do not send email if there is no emails or no issue submissions.
+        if not production_team_group["emails"] or not production_team_group["issue_submissions"]:
+            continue
+
+        # Send the email.
+        email = Email(
+            production_team_group["emails"],
+            html_template="emails/editor/issue_files_deletion_content.html",
+            subject_template="emails/editor/issue_files_deletion_subject.html",
+            extra_context={"issue_submissions": eminent_archival_issues},
+            tag=EMAIL_TAG,
+        )
+        email.send()
 
 
-def _handle_issue_submission_action_needed():
-    # Get production team emails.
-    emails = ProductionTeam.emails()
-    if emails is None:
-        return
-
+def _handle_issue_submission_action_needed() -> None:
     # Get action needed issue submissions.
     issues = IssueSubmission.objects.action_needed()
     if not issues.exists():
         return
 
-    # Group issues by status and by journal.
-    submitted = {
-        journal_name: list(issues)
-        for journal_name, issues in groupby(
-            issues.filter(status=IssueSubmission.SUBMITTED),
-            attrgetter("journal.name"),
-        )
-    }
-    needs_corrections = {
-        journal_name: list(issues)
-        for journal_name, issues in groupby(
-            issues.filter(status=IssueSubmission.NEEDS_CORRECTIONS),
-            attrgetter("journal.name"),
-        )
+    # Get all production teams groups and their emails.
+    production_teams_groups = {
+        production_team_group.id: {
+            "emails": list(production_team_group.user_set.values_list("email", flat=True)),
+            IssueSubmission.SUBMITTED: [],
+            IssueSubmission.NEEDS_CORRECTIONS: [],
+        }
+        for production_team_group in Group.objects.filter(productionteam__isnull=False)
     }
 
-    # Send the email.
-    email = Email(
-        list(emails),
-        html_template="emails/editor/issue_submission_action_needed_content.html",
-        subject_template="emails/editor/issue_submission_action_needed_subject.html",
-        extra_context={
-            "submitted": submitted,
-            "needs_corrections": needs_corrections,
-        },
-        tag=EMAIL_TAG,
-    )
-    email.send()
+    # Group issues by production team group.
+    for issue in issues:
+        for production_team_group in get_production_teams_groups(issue.journal):
+            production_teams_groups[production_team_group.id][issue.status].append(issue)
+
+    # Now prepare and send an email to each production team group with action needed issues.
+    for production_team_group in production_teams_groups.values():
+        # Group issues by status and by journal.
+        needs_review = {
+            journal_name: list(issues)
+            for journal_name, issues in groupby(
+                production_team_group[IssueSubmission.SUBMITTED],
+                attrgetter("journal.name"),
+            )
+        }
+        needs_corrections = {
+            journal_name: list(issues)
+            for journal_name, issues in groupby(
+                production_team_group[IssueSubmission.NEEDS_CORRECTIONS],
+                attrgetter("journal.name"),
+            )
+        }
+
+        # Do not send email if there is no emails or no issue submissions.
+        if not production_team_group["emails"] or not (
+            production_team_group[IssueSubmission.SUBMITTED]
+            + production_team_group[IssueSubmission.NEEDS_CORRECTIONS]
+        ):
+            continue
+
+        # Send the email.
+        email = Email(
+            production_team_group["emails"],
+            html_template="emails/editor/issue_submission_action_needed_content.html",
+            subject_template="emails/editor/issue_submission_action_needed_subject.html",
+            extra_context={
+                "submitted": needs_review,
+                "needs_corrections": needs_corrections,
+            },
+            tag=EMAIL_TAG,
+        )
+        email.send()
--- tests/unit/core/editor/test_tasks.py
+++ tests/unit/core/editor/test_tasks.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import pytest
 
 from django.contrib.auth.models import Group
 from django.core import mail
@@ -15,92 +16,79 @@ from core.editor.test.factories import ProductionTeamFactory
 
 
 class TestHandleIssueSubmissionFilesRemoval(BaseEditorTestCase):
-    def test_is_able_to_remove_files_from_approved_issue_submissions_after_90_days(self):
-        # Setup
+    @pytest.fixture(autouse=True)
+    def setup_issue_submission(self):
+        # Add a file to the issue submission, submit it and approve it.
         rfile = ResumableFile.objects.create(path="dummy/path.png", filesize=42, uploadsize=42)
         self.issue_submission.last_files_version.submissions.add(rfile)
         self.issue_submission.submit()
         self.issue_submission.approve()
+
+    @pytest.fixture
+    def eminent_archival_issue(self):
+        # Change the issue submission's modified date so that its archival is eminent.
+        self.issue_submission._meta.get_field("date_modified").auto_now = False
+        self.issue_submission.date_modified = tz.now() - dt.timedelta(
+            days=editor_settings.ARCHIVAL_DAY_OFFSET - 5
+        )
+        self.issue_submission.save()
+        self.issue_submission._meta.get_field("date_modified").auto_now = True
+
+    def test_is_able_to_remove_files_from_approved_issue_submissions_after_90_days(self):
         self.issue_submission._meta.get_field("date_modified").auto_now = False
         self.issue_submission.date_modified = tz.now() - dt.timedelta(
             days=editor_settings.ARCHIVAL_DAY_OFFSET
         )
         self.issue_submission.save()
         self.issue_submission._meta.get_field("date_modified").auto_now = True
-        # Run
         _handle_issue_submission_archival_and_files_deletion()
-        # Check
         assert not ResumableFile.objects.count()
         self.issue_submission.refresh_from_db()
         assert self.issue_submission.archived
 
-    def test_sends_an_email_to_notify_the_production_team_5_days_before_removal(self):
-        # Setup
-        group = Group.objects.create(name="Production team")
-        ProductionTeamFactory.create(group=group)
-        self.user.groups.add(group)
-        rfile = ResumableFile.objects.create(path="dummy/path.png", filesize=42, uploadsize=42)
-        self.issue_submission.last_files_version.submissions.add(rfile)
-        self.issue_submission.submit()
-        self.issue_submission.approve()
-        self.issue_submission._meta.get_field("date_modified").auto_now = False
-        self.issue_submission.date_modified = tz.now() - dt.timedelta(
-            days=editor_settings.ARCHIVAL_DAY_OFFSET - 5
-        )
-        self.issue_submission.save()
-        self.issue_submission._meta.get_field("date_modified").auto_now = True
-        # Run
+    def test_sends_an_email_to_notify_the_production_team_5_days_before_removal(
+        self, eminent_archival_issue
+    ):
         _handle_issue_submission_archival_and_files_deletion()
-        # Check
         assert ResumableFile.objects.count() == 1
         assert len(mail.outbox) == 1
         assert mail.outbox[0].to[0] == self.user.email
 
-    def test_do_not_send_an_email_if_the_production_team_group_does_not_exist(self):
-        # Setup
-        rfile = ResumableFile.objects.create(path="dummy/path.png", filesize=42, uploadsize=42)
-        self.issue_submission.last_files_version.submissions.add(rfile)
-        self.issue_submission.submit()
-        self.issue_submission.approve()
-        self.issue_submission._meta.get_field("date_modified").auto_now = False
-        self.issue_submission.date_modified = tz.now() - dt.timedelta(
-            days=editor_settings.ARCHIVAL_DAY_OFFSET - 5
-        )
-        self.issue_submission.save()
-        self.issue_submission._meta.get_field("date_modified").auto_now = True
-        # Run
-        _handle_issue_submission_archival_and_files_deletion()
-        # Check
-        assert ResumableFile.objects.count() == 1
-        assert not len(mail.outbox)
+    def test_send_an_email_to_main_production_team_if_the_journal_has_no_production_team(
+        self, eminent_archival_issue
+    ):
+        # Remove the journal from the production team.
+        self.production_team.journals.remove(self.journal)
 
-    def test_do_not_send_an_email_if_the_production_team_is_empty(self):
-        # Setup
-        group = Group.objects.create(name="Production team")
-        ProductionTeamFactory.create(group=group)
-        rfile = ResumableFile.objects.create(path="dummy/path.png", filesize=42, uploadsize=42)
-        self.issue_submission.last_files_version.submissions.add(rfile)
-        self.issue_submission.submit()
-        self.issue_submission.approve()
-        self.issue_submission._meta.get_field("date_modified").auto_now = False
-        self.issue_submission.date_modified = tz.now() - dt.timedelta(
-            days=editor_settings.ARCHIVAL_DAY_OFFSET - 5
-        )
-        self.issue_submission.save()
-        self.issue_submission._meta.get_field("date_modified").auto_now = True
-        # Run
         _handle_issue_submission_archival_and_files_deletion()
-        # Check
+        assert ResumableFile.objects.count() == 1
+        assert len(mail.outbox) == 1
+
+    def test_send_an_email_to_each_production_team_if_the_journal_has_multiple_production_teams(
+        self, eminent_archival_issue
+    ):
+        # Associate a second production team to the journal.
+        group_2 = Group.objects.create(name="Other production team")
+        production_team_2 = ProductionTeamFactory(group=group_2, identifier="other-production-team")
+        production_team_2.journals.add(self.journal)
+        self.user.groups.add(group_2)
+
+        _handle_issue_submission_archival_and_files_deletion()
+        assert ResumableFile.objects.count() == 1
+        assert len(mail.outbox) == 2
+
+    def test_do_not_send_an_email_if_the_production_team_is_empty(self, eminent_archival_issue):
+        # Remove the user from the production team so it's empty.
+        self.user.groups.remove(self.group)
+        _handle_issue_submission_archival_and_files_deletion()
         assert ResumableFile.objects.count() == 1
         assert not len(mail.outbox)
 
 
 class TestHandleActionNeededIssueSubmissions(BaseEditorTestCase):
-    def test_email_production_team_about_action_needed_needs_review_issue_submissions(self):
-        # Setup
-        group = Group.objects.create(name="Production team")
-        ProductionTeamFactory.create(group=group)
-        self.user.groups.add(group)
+    @pytest.fixture
+    def action_needed_issues(self):
+        # Change the issue submission's modified date so that an action is needed.
         self.issue_submission.submit()
         self.issue_submission._meta.get_field("date_modified").auto_now = False
         self.issue_submission.date_modified = tz.now() - dt.timedelta(
@@ -108,57 +96,48 @@ class TestHandleActionNeededIssueSubmissions(BaseEditorTestCase):
         )
         self.issue_submission.save()
         self.issue_submission._meta.get_field("date_modified").auto_now = True
-        # Run
+
+    def test_email_production_team_about_action_needed_needs_review_issue_submissions(
+        self, action_needed_issues
+    ):
         _handle_issue_submission_action_needed()
-        # Check
         assert len(mail.outbox) == 1
         assert mail.outbox[0].to[0] == self.user.email
 
-    def test_email_production_team_about_action_needed_needs_corrections_issue_submissions(self):
-        # Setup
-        group = Group.objects.create(name="Production team")
-        ProductionTeamFactory.create(group=group)
-        self.user.groups.add(group)
-        self.issue_submission.submit()
+    def test_email_production_team_about_action_needed_needs_corrections_issue_submissions(
+        self, action_needed_issues
+    ):
+        # Refuse the issue submission.
         self.issue_submission.refuse()
-        self.issue_submission._meta.get_field("date_modified").auto_now = False
-        self.issue_submission.date_modified = tz.now() - dt.timedelta(
-            days=editor_settings.ACTION_NEEDED_DAY_OFFSET
-        )
-        self.issue_submission.save()
-        self.issue_submission._meta.get_field("date_modified").auto_now = True
-        # Run
+
         _handle_issue_submission_action_needed()
-        # Check
         assert len(mail.outbox) == 1
         assert mail.outbox[0].to[0] == self.user.email
 
-    def test_do_not_send_an_email_if_the_production_team_group_does_not_exist(self):
-        # Setup
-        self.issue_submission.submit()
-        self.issue_submission._meta.get_field("date_modified").auto_now = False
-        self.issue_submission.date_modified = tz.now() - dt.timedelta(
-            days=editor_settings.ACTION_NEEDED_DAY_OFFSET
-        )
-        self.issue_submission.save()
-        self.issue_submission._meta.get_field("date_modified").auto_now = True
-        # Run
-        _handle_issue_submission_action_needed()
-        # Check
-        assert not len(mail.outbox)
+    def test_send_an_email_to_main_production_team_if_the_journal_has_no_production_team(
+        self, action_needed_issues
+    ):
+        # Remove the journal from the production team.
+        self.production_team.journals.remove(self.journal)
 
-    def test_do_not_send_an_email_if_the_production_team_is_empty(self):
-        # Setup
-        group = Group.objects.create(name="Production team")
-        ProductionTeamFactory.create(group=group)
-        self.issue_submission.submit()
-        self.issue_submission._meta.get_field("date_modified").auto_now = False
-        self.issue_submission.date_modified = tz.now() - dt.timedelta(
-            days=editor_settings.ACTION_NEEDED_DAY_OFFSET
-        )
-        self.issue_submission.save()
-        self.issue_submission._meta.get_field("date_modified").auto_now = True
-        # Run
         _handle_issue_submission_action_needed()
-        # Check
+        assert len(mail.outbox) == 1
+
+    def test_send_an_email_to_each_production_team_if_the_journal_has_multiple_production_teams(
+        self, action_needed_issues
+    ):
+        # Associate a second production team to the journal.
+        group_2 = Group.objects.create(name="Other production team")
+        production_team_2 = ProductionTeamFactory(group=group_2, identifier="other-production-team")
+        production_team_2.journals.add(self.journal)
+        self.user.groups.add(group_2)
+
+        _handle_issue_submission_action_needed()
+        assert len(mail.outbox) == 2
+
+    def test_do_not_send_an_email_if_the_production_team_is_empty(self, action_needed_issues):
+        # Remove the user from the production team so it's empty.
+        self.user.groups.remove(self.group)
+
+        _handle_issue_submission_action_needed()
         assert not len(mail.outbox)
