da2cef2360b5af4a3cadaa37bea74e4ae46abef8
David Cormier <david.cormier@erudit.org>
2021-05-25 15:13:32 -0400

    makemessages.sh: use --no-wrap
    It seems like we are experiencing inconsistent wrapping behaviour. Set
    --no-wrap and see if it helps.

--- tools/makemessages.sh
+++ tools/makemessages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python manage.py makemessages -e txt,py,html,xsl
-python manage.py makemessages -d djangojs -i build -i build_dev
+python manage.py makemessages -e txt,py,html,xsl --no-wrap
+python manage.py makemessages -d djangojs -i build -i build_dev --no-wrap
